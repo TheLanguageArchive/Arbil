1b69d21bb51e2504e9e3abd39fb6feccea463a97
Peter Withers <Peter.Withers@mpi.nl>
2009-04-29 12:23:44 +0000

    Separated the main context menu into a separate class for use by other components. Made sure guihelper is used as a single instance. Made sure treehelper is used as a single instance. Prevented error when showing the table cell menu for the first time. Made a sample help page look pretty. Updated help text and organised help structure. Reduced Vector usage. Set table title to imdi name when in single imd view. Updated node icons when saved from the file menu. Added drag drop to move session and corpus branch between different corpus branches. Added shift tab to go up a row in the single node table view. Added a cache to store the image thumbnails in memory so that scrolling through images is faster. Added selection listeners so that the table and images share a common selection and are scrolled to when the selection changes. Added drag drop of sessions and corpus branches to and from corpus branches. Improved the speed of reloading of the ImdiTable data.

--- LinorgBugCatcher.java
+++ LinorgBugCatcher.java
@@ -1,7 +1,3 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package mpi.linorg;
 
 import java.awt.Robot;
@@ -16,7 +12,7 @@ import javax.imageio.ImageIO;
 /**
  * Document   : LinorgBugCatcher
  * Created on : Dec 17, 2008, 10:35:56 AM
- * @author petwit
+ * @author Peter.Withers@mpi.nl
  */
 public class LinorgBugCatcher {
 
