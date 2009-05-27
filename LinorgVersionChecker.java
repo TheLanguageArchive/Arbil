3f0630dff06540f32fe28fc2bbe0d632344a8f6a
Peter Withers <Peter.Withers@mpi.nl>
2009-05-27 13:45:48 +0000

    Added help text. Updated the application version checker to warn the user when there is a more recent version on the server. Captured the last key when starting to edit a table cell and inserted it into the cell (missing first char) (this may not work on all systems and must be checked) Prevented drag drop moving nodes from outside the cache to inside the cache. Started to implement drag drop to import from the local file system.

---
+++ LinorgVersionChecker.java
@@ -0,0 +1,72 @@
+package mpi.linorg;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import javax.swing.SwingUtilities;
+
+/**
+ * Document   : LinorgVersionChecker
+ * Created on : Mar 11, 2009, 10:13:10 AM
+ * @author Peter.Withers@mpi.nl
+ */
+public class LinorgVersionChecker {
+
+    LinorgFrame parentComponent;
+
+    private boolean isLatestVersion() {
+        try {
+            LinorgVersion linorgVersion = new LinorgVersion();
+            int daysTillExpire = 1;
+            String currentVersionTxt = "arbil-" + linorgVersion.currentMajor + "-" + linorgVersion.currentMinor + "-current.txt";
+            String cachePath = GuiHelper.linorgSessionStorage.updateCache("http://www.mpi.nl/tg/j2se/jnlp/linorg/" + currentVersionTxt, daysTillExpire);
+            BufferedReader bufferedReader = new BufferedReader(new FileReader(cachePath));
+            String serverVersionString = bufferedReader.readLine();
+//            String localVersionString = "linorg" + linorgVersion.currentRevision + ".jar"; // the server string has the full jar file name
+//            System.out.println("currentRevision: " + localVersionString);
+            System.out.println("currentRevision: " + linorgVersion.currentRevision);
+            System.out.println("serverVersionString: " + serverVersionString);
+            // to keep it simple only exact version matches will be considered correct
+            return (linorgVersion.currentRevision.equals(serverVersionString));
+        } catch (Exception ex) {
+            GuiHelper.linorgBugCatcher.logError(ex);
+        }
+        return true;
+    }
+
+    private boolean doUpdate() {
+        // download then check that the new jar is correctly signed
+        // then move the old version
+        // then move the new version to the old location
+        return true;
+    }
+
+    private void restartApplication() {
+        //parentComponent.performCleanExit();
+        //System.//spawn new updated applcation 
+        System.exit(0);
+    }
+
+    public void checkForUpdate(final LinorgFrame parentComponentLocal) {
+        parentComponent = parentComponentLocal;
+        //if (last check date not today)
+        SwingUtilities.invokeLater(new Runnable() {
+
+            public void run() {
+                if (!isLatestVersion()) {
+                    LinorgWindowManager.getSingleInstance().addMessageDialogToQueue("There is a new version available.\nPlease go to the website and update via the download link.", null);
+//                    switch (JOptionPane.showConfirmDialog(parentComponent, "There is a new version available\nDo you want to update now?", "Arbil", JOptionPane.YES_NO_CANCEL_OPTION, JOptionPane.PLAIN_MESSAGE)) {
+//                        case JOptionPane.NO_OPTION:
+//                            break;
+//                        case JOptionPane.YES_OPTION:
+//                            if (doUpdate()) {
+//                                restartApplication();
+//                            }
+//                            break;
+//                        default:
+//                            return;
+//                    }
+                }
+            }
+        });
+    }
+}
