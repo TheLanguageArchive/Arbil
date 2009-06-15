b8979c20af04eb36bd0fdcbf3ccc31bc11e89c84
Peter Withers <Peter.Withers@mpi.nl>
2009-06-15 14:44:31 +0000

    Added sortable text to table cells containing child nodes. Added sorts for both the child nodes in the cell and for the column. Updated cell highlighting to use the cells rendered text. Prevented icons getting forgotten when they should have been updated. Enabled the "id" field to be used as the display name when the other options are not available. Corrected the loading of directory nodes.

---
+++ ImdiTableCellRenderer.java
@@ -0,0 +1,85 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mpi.linorg;
+
+import java.awt.Color;
+import java.util.Arrays;
+import java.util.Comparator;
+import javax.swing.Icon;
+import javax.swing.table.DefaultTableCellRenderer;
+
+/**
+ *
+ * @author petwit
+ */
+public class ImdiTableCellRenderer extends DefaultTableCellRenderer {
+
+    Object cellObject;
+
+    public ImdiTableCellRenderer() {
+    }
+
+    // this is used when getting a string value for the object
+    public ImdiTableCellRenderer(Object cellObject) {
+        setValue(cellObject);
+    }
+
+    @Override
+    protected void setValue(Object value) {
+        cellObject = value;
+        super.setValue(value);
+        if (cellObject instanceof ImdiField[]) {
+            int greyTone = 150;
+            super.setForeground(new Color(greyTone, greyTone, greyTone));
+        }
+        if (cellObject instanceof String && cellObject.equals("")) {
+            super.setBackground(new Color(230, 230, 230)/*Color.lightGray*/);
+        }
+        if (cellObject instanceof ImdiField) {
+            if (((ImdiField) cellObject).fieldNeedsSaveToDisk) {
+                super.setForeground(Color.blue);
+            }
+        }
+    }
+
+    @Override
+    public Icon getIcon() {
+        if (cellObject instanceof ImdiTreeObject) {
+            return (((ImdiTreeObject) cellObject).getIcon());
+        } else if (cellObject instanceof ImdiTreeObject[]) {
+            return (ImdiIcons.getSingleInstance().getIconForImdi((ImdiTreeObject[]) cellObject));
+        } else if (cellObject instanceof ImdiField[]) {
+            return null;
+        } else {
+            return (null);
+        }
+    }
+
+    @Override
+    public String getText() {
+        if (cellObject instanceof ImdiTreeObject) {
+            return (((ImdiTreeObject) cellObject).toString());
+        } else if (cellObject instanceof ImdiTreeObject[]) {
+            String cellText = "";
+            Arrays.sort((ImdiTreeObject[]) cellObject, new Comparator() {
+
+                public int compare(Object o1, Object o2) {
+                    String value1 = o1.toString();
+                    String value2 = o2.toString();
+                    return value1.compareToIgnoreCase(value2);
+                }
+            });
+            for (ImdiTreeObject currentImdiTreeObject : (ImdiTreeObject[]) cellObject) {
+                cellText = cellText + "[" + currentImdiTreeObject.toString() + "],";
+            }
+            cellText = cellText.substring(0, cellText.length() - 1);
+            return (cellText);
+        } else if (cellObject instanceof ImdiField[]) {
+            return "<multiple values>";
+        } else {
+            return super.getText();
+        }
+    }
+}
