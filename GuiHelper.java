91c9eda79bdda04f8e9e3bd9f163b9c36f1b7b6b
Peter Withers <Peter.Withers@mpi.nl>
2009-07-10 12:45:21 +0000

    Moved the tree mouse drag event code into the imditree class. Moved the tree expand and collapse event code into the imditree class. Moved the tree value changed event code into the imditree class. Added an optional xsl file to the templates directory. Moved the menu bar from the main class into a class of its own. Moved the preview split panel into a separate class. Moved the external application launch code to a common location. Added the pretty XSL and dependent files. Added a menu item to open the pretty formatted imdi file in an external browser. Set the XSL output files to be deleted on application exit. Added the pretty xsl and dependent files to the jar via the ant file. Added the XSL as a component of the templates. Added documentation for the Templates and XSL usage. Added the current template directory as a property that is set via the templates menu.

--- GuiHelper.java
+++ GuiHelper.java
@@ -47,7 +47,6 @@ public class GuiHelper {
         //throw new UnsupportedOperationException("Not supported yet.");
         }
     };
-    
     static private GuiHelper singleInstance = null;
 
     static synchronized public GuiHelper getSingleInstance() {
@@ -210,9 +209,10 @@ public class GuiHelper {
             String nodeName = ((ImdiTreeObject) (userObject)).toString();
             if (formatXml) {
                 try {
+                    // 1. Instantiate a TransformerFactory.
                     javax.xml.transform.TransformerFactory tFactory = javax.xml.transform.TransformerFactory.newInstance();
                     // 2. Use the TransformerFactory to process the stylesheet Source and generate a Transformer.
-                    URL xslUrl = this.getClass().getResource("/mpi/linorg/resources/xsl/IMDI_3_0_TO_WEB.xsl");
+                    URL xslUrl = this.getClass().getResource("/mpi/linorg/resources/xsl/imdi-viewer.xsl");
                     File tempHtmlFile;
                     File xslFile = null;
                     if (imdiSchema.selectedTemplateDirectory != null) {
@@ -226,26 +226,26 @@ public class GuiHelper {
                         // copy any dependent files from the jar
                         String[] dependentFiles = {"imdi-viewer-open.gif", "imdi-viewer-closed.gif", "imdi-viewer.js", "additTooltip.js", "additPopup.js", "imdi-viewer.css", "additTooltip.css"};
                         tempHtmlFile = File.createTempFile(nodeFile.getName(), ".html");
-                    tempHtmlFile.deleteOnExit();
-                    for (String dependantFileString : dependentFiles) {
-                        File tempDependantFile = new File(tempHtmlFile.getParent() + File.separatorChar + dependantFileString);
-                        tempDependantFile.deleteOnExit();
+                        tempHtmlFile.deleteOnExit();
+                        for (String dependantFileString : dependentFiles) {
+                            File tempDependantFile = new File(tempHtmlFile.getParent() + File.separatorChar + dependantFileString);
+                            tempDependantFile.deleteOnExit();
 //                        File tempDependantFile = File.createTempFile(dependantFileString, "");
-                        FileOutputStream outFile = new FileOutputStream(tempDependantFile);
-                        //InputStream inputStream = this.getClass().getResourceAsStream("html/imdi-viewer/" + dependantFileString);
-                        InputStream inputStream = this.getClass().getResourceAsStream("/mpi/linorg/resources/xsl/" + dependantFileString);
-                        int bufferLength = 1024 * 4;
-                        byte[] buffer = new byte[bufferLength]; // make htis 1024*4 or something and read chunks not the whole file
-                        int bytesread = 0;
-                        while (bytesread >= 0) {
-                            bytesread = inputStream.read(buffer);
-                            if (bytesread == -1) {
-                                break;
+                            FileOutputStream outFile = new FileOutputStream(tempDependantFile);
+                            //InputStream inputStream = this.getClass().getResourceAsStream("html/imdi-viewer/" + dependantFileString);
+                            InputStream inputStream = this.getClass().getResourceAsStream("/mpi/linorg/resources/xsl/" + dependantFileString);
+                            int bufferLength = 1024 * 4;
+                            byte[] buffer = new byte[bufferLength]; // make htis 1024*4 or something and read chunks not the whole file
+                            int bytesread = 0;
+                            while (bytesread >= 0) {
+                                bytesread = inputStream.read(buffer);
+                                if (bytesread == -1) {
+                                    break;
+                                }
+                                outFile.write(buffer, 0, bytesread);
                             }
-                            outFile.write(buffer, 0, bytesread);
+                            outFile.close();
                         }
-                        outFile.close();
-                    }
                     }
                     javax.xml.transform.Transformer transformer = tFactory.newTransformer(new javax.xml.transform.stream.StreamSource(xslUrl.toString()));
                     // 3. Use the Transformer to transform an XML Source and send the output to a Result object.
@@ -272,6 +272,7 @@ public class GuiHelper {
         }
     }
     // TODO: this could be merged witht the add row function
+
     public AbstractTableModel getImdiTableModel(Hashtable rowNodes) {
         ImdiTableModel searchTableModel = new ImdiTableModel();
         searchTableModel.setShowIcons(true);
