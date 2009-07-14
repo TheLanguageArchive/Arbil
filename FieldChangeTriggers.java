3d7b5233b30786d87dc500152fd5a026f220e875
Peter Withers <Peter.Withers@mpi.nl>
2009-07-14 11:19:05 +0000

    Moved the external application launch code to a common location. Added the current template directory as a property that is set via the templates menu. Updated the corpusstructure and imdiapi jar versions. Updated the code that sets the tree selection when a table row is selected or a node is added. Created a class to handle field triggers that set the value of sibling fileds when the registered fields value is changed. Added LexiconResource as a subnode in the session type (this will require more work).

---
+++ FieldChangeTriggers.java
@@ -0,0 +1,52 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mpi.linorg;
+
+/**
+ * FieldChangeTriggers.java
+ * Created on Jul 13, 2009, 4:15:02 PM
+ * @author Peter.Withers@mpi.nl
+ */
+public class FieldChangeTriggers {
+    // the following strings need to be read from a template file or a vocaulary etc
+
+    String[][] triggersArray = {
+//        TODO: read this array from a file int the teplates directory
+        {".METATRANSCRIPT.Session.MDGroup.Content.Languages.Language(x).Name", ".METATRANSCRIPT.Session.MDGroup.Content.Languages.Language(x).Id", "description"},
+        {".METATRANSCRIPT.Session.MDGroup.Actors.Actor(x).Languages.Language(x).Name", ".METATRANSCRIPT.Session.MDGroup.Actors.Actor(x).Languages.Language(x).Id", "description"}
+    };
+
+    public void actOnChange(ImdiField changedImdiField) {
+        String fieldPath = changedImdiField.getFullXmlPath();
+        fieldPath = fieldPath.replaceAll("\\(\\d\\)", "(x)");
+        System.out.println("fieldPath: " + fieldPath);
+        for (String[] currentTrigger : triggersArray) {
+            if (fieldPath.equals(currentTrigger[0])) {
+                // we now have the path for two fields:
+                // .METATRANSCRIPT.Session.MDGroup.Actors.Actor(x).Languages.Language(x).Id
+                // .METATRANSCRIPT.Session.MDGroup.Actors.Actor(2).Languages.Language(3).Name
+                // and we need to put the (\d) back into the (x)
+                String originalFieldPath = changedImdiField.getFullXmlPath();
+                int lastBracketPos = originalFieldPath.lastIndexOf(")");
+                String targetFieldPath = originalFieldPath.substring(0, lastBracketPos) + currentTrigger[1].substring(lastBracketPos);
+                ImdiField[] targetField = changedImdiField.getSiblingField(targetFieldPath);
+                ImdiVocabularies.VocabularyItem vocabItem = changedImdiField.getVocabulary().findVocabularyItem(changedImdiField.fieldValue);
+                String valueForTargetField = null;
+                if (currentTrigger[2].equals("description")) {
+                    valueForTargetField = vocabItem.descriptionString;
+                } else if (currentTrigger[2].equals("Value")) {
+                    valueForTargetField = vocabItem.languageName;
+                } else if (currentTrigger[2].equals("Code")) {
+                    valueForTargetField = vocabItem.languageCode;
+                } else if (currentTrigger[2].equals("FollowUp")) {
+                    valueForTargetField = vocabItem.followUpVocabulary;
+                }
+                if (valueForTargetField != null) {
+                    targetField[0].setFieldValue(valueForTargetField, true);
+                }
+            }
+        }
+    }
+}
