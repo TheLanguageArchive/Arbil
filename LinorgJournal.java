858c3253f982b807db0c9a4788dcc22e763cde8e
Peter Withers <Peter.Withers@mpi.nl>
2009-07-29 09:55:16 +0000

    Added a class to handle the various templates. When the imdi file is read the Originator attribute is used to select the template for the individual file. Added a template class to provide customised representation and behavior of the IMDI data objects. Moved some relevant code into the template objects. Updated the add menu to be populated from the current template of the clicked node. Added the new catalogue template. Added a dialogue for selection of the storage directory. Added catalogue template sub nodes. Made the sessionstorage object a gettable single instance. Added a  show working directory menu action (could be changed to just a checkbox and menu actions). Moved the triggers into the template object. Added an icon update for non imdi nodes in a corpus branch so that description link nodes display correctly. Updated the templates to hold also a text name that is used in the add menu. Added a few sub nodes for the catalogue files. Enabled more feed back when the XSL imports classes without warning that don't exist.

--- LinorgJournal.java
+++ LinorgJournal.java
@@ -12,7 +12,7 @@ public class LinorgJournal {
     public boolean saveJournalEntry(String imdiUrl, String imdiNodePath, String oldValue, String newValue, String eventType) {
         boolean returnValue = false;
         try {
-            FileWriter journalFile = new FileWriter(GuiHelper.linorgSessionStorage.storageDirectory + "linorgjornal.log", true);
+            FileWriter journalFile = new FileWriter(LinorgSessionStorage.getSingleInstance().storageDirectory + "linorgjornal.log", true);
             System.out.println("Journal: " + imdiUrl + "," + imdiNodePath + "," + oldValue + "," + newValue);
             journalFile.append("\""+imdiUrl + imdiNodePath + "\",\"" + oldValue + "\",\"" + newValue + "\",\"" + eventType + "\"\n");
             journalFile.close();
