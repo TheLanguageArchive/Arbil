74c4b99adad00efa1e82df896c18c9c10eec4133
Peter Withers <Peter.Withers@mpi.nl>
2009-08-25 12:18:48 +0000

    Tidied the favourites class removing variable names relating to the previous useage as templates. Updated the ant file to get the required jar files from the trunk. Modified the favourites to uses a separate directory and to copy the files when adding as a favourite. Added tree model for the favourites and updated the tree handling code to include the favourites tree when populating nodes and sorting etc..

--- LinorgFavourites.java
+++ LinorgFavourites.java
@@ -1,6 +1,8 @@
 package mpi.linorg;
 
-import java.util.Arrays;
+import java.io.File;
+import java.io.IOException;
+import java.net.MalformedURLException;
 import java.util.Enumeration;
 import java.util.Hashtable;
 import java.util.Vector;
@@ -16,7 +18,7 @@ public class LinorgFavourites {
     static private LinorgFavourites singleInstance = null;
 
     static synchronized public LinorgFavourites getSingleInstance() {
-        System.out.println("LinorgTemplates getSingleInstance");
+        System.out.println("LinorgFavourites getSingleInstance");
         if (singleInstance == null) {
             singleInstance = new LinorgFavourites();
         }
@@ -24,10 +26,10 @@ public class LinorgFavourites {
     }
 
     private LinorgFavourites() {
-        loadSelectedTemplates();
+        loadSelectedFavourites();
     }
 
-    private void loadSelectedTemplates() {
+    private void loadSelectedFavourites() {
         Vector<String> userFavouritesStrings;
         try {
             userFavouritesStrings = (Vector<String>) LinorgSessionStorage.getSingleInstance().loadObject("selectedFavourites");
@@ -36,47 +38,77 @@ public class LinorgFavourites {
             userFavouritesStrings = new Vector<String>();
         }
         userFavourites = new Hashtable<String, ImdiTreeObject>();
-        // loop templates and load the imdi objects then set the template flags for each
-        for (Enumeration<String> templatesEnum = userFavouritesStrings.elements(); templatesEnum.hasMoreElements();) {
-            ImdiTreeObject currentImdiObject = GuiHelper.imdiLoader.getImdiObject(null, templatesEnum.nextElement());
-            currentImdiObject.setTemplateStatus(true);
+        // loop favourites and load the imdi objects then set the favourite flags for each
+        for (Enumeration<String> favouritesEnum = userFavouritesStrings.elements(); favouritesEnum.hasMoreElements();) {
+            ImdiTreeObject currentImdiObject = GuiHelper.imdiLoader.getImdiObject(null, favouritesEnum.nextElement());
+            currentImdiObject.setFavouriteStatus(true);
             userFavourites.put(currentImdiObject.getUrlString(), currentImdiObject);
         }
     }
 
     public void toggleFavouritesList(ImdiTreeObject[] imdiObjectArray, boolean setAsTempate) {
-        System.out.println("toggleTemplateList: " + setAsTempate);
+        System.out.println("toggleFavouriteList: " + setAsTempate);
         for (ImdiTreeObject currentImdiObject : imdiObjectArray) {
             if (currentImdiObject.getFields().size() == 0) {
                 // note: the way that favourites are shown in a table will not show meta nodes but their child nodes instead
                 setAsTempate = false;
             }
             if (setAsTempate) {
-                addAsTemplate(currentImdiObject.getUrlString());
+                addAsFavourite(currentImdiObject.getUrlString());
             } else {
                 removeFromFavourites(currentImdiObject.getUrlString());
+                currentImdiObject.setFavouriteStatus(false);
             }
-            currentImdiObject.setTemplateStatus(setAsTempate);
         }
     }
 
-    private void addAsTemplate(String imdiUrlString) {
-        if (!userFavourites.containsKey(imdiUrlString)) {
-            userFavourites.put(imdiUrlString, GuiHelper.imdiLoader.getImdiObject(null, imdiUrlString));
-            saveSelectedTemplates();
-            loadSelectedTemplates();
+    private void addAsFavourite(String imdiUrlString) {
+        try {
+            String[] urlParts = imdiUrlString.split("#");
+            File copiedFile = copyToFavouritesDirectory(urlParts[0]);
+            String favouriteUrlString = copiedFile.getCanonicalPath();
+            if (urlParts.length > 1) {
+                favouriteUrlString = favouriteUrlString + "#" + urlParts[1];
+            }
+            if (!userFavourites.containsKey(favouriteUrlString)) {
+                ImdiTreeObject favouriteImdiObject = GuiHelper.imdiLoader.getImdiObject(null, favouriteUrlString);
+                userFavourites.put(favouriteUrlString, favouriteImdiObject);
+                saveSelectedFavourites();
+                loadSelectedFavourites();
+                favouriteImdiObject.setFavouriteStatus(true);
+            }
+
+        } catch (Exception ex) {
+            GuiHelper.linorgBugCatcher.logError(ex);
         }
+    }
+
+    private File copyToFavouritesDirectory(String imdiUrlString) throws MalformedURLException, IOException {
+        mpi.util.OurURL inUrlLocal = new mpi.util.OurURL(imdiUrlString);
+        File destinationFile = File.createTempFile("fav-", ".imdi", LinorgSessionStorage.getSingleInstance().getFavouritesDir());
+        mpi.util.OurURL destinationUrl = new mpi.util.OurURL(destinationFile.toURL());
+
+        org.w3c.dom.Document nodDom = ImdiTreeObject.api.loadIMDIDocument(inUrlLocal, false);
+        mpi.imdi.api.IMDILink[] links = ImdiTreeObject.api.getIMDILinks(nodDom, inUrlLocal, mpi.imdi.api.WSNodeType.UNKNOWN);
+        if (links != null) {
+            for (mpi.imdi.api.IMDILink currentLink : links) {
+                ImdiTreeObject.api.changeIMDILink(nodDom, destinationUrl, currentLink);
+            }
+        }
+        boolean removeIdAttributes = false;
+        ImdiTreeObject.api.writeDOM(nodDom, destinationFile, removeIdAttributes);
+        return destinationFile;
     }
 
     private void removeFromFavourites(String imdiUrlString) {
         while (userFavourites.containsKey(imdiUrlString)) {
             userFavourites.remove(imdiUrlString);
         }
-        saveSelectedTemplates();
-        loadSelectedTemplates();
+        saveSelectedFavourites();
+        loadSelectedFavourites();
     }
 
-    public void saveSelectedTemplates() {
+    public void saveSelectedFavourites() {
         try {
             LinorgSessionStorage.getSingleInstance().saveObject(new Vector(userFavourites.keySet()), "selectedFavourites");
         } catch (Exception ex) {
@@ -84,42 +116,46 @@ public class LinorgFavourites {
         }
     }
 
+    public Vector getArrayOfFavourites() {
+        return new Vector(userFavourites.keySet());
+    }
+
     public ImdiTreeObject[] listAllFavourites() {
         return userFavourites.values().toArray(new ImdiTreeObject[userFavourites.size()]);
     }
 
     public Enumeration listFavouritesFor(Object targetNodeUserObject) {
-        System.out.println("listTemplatesFor: " + targetNodeUserObject);
-        Vector<String[]> validTemplates = new Vector<String[]>();
+        System.out.println("listFavouritesFor: " + targetNodeUserObject);
+        Vector<String[]> validFavourites = new Vector<String[]>();
         if (targetNodeUserObject instanceof ImdiTreeObject) {
             ImdiTreeObject targetImdiObject = (ImdiTreeObject) targetNodeUserObject;
             boolean targetIsCorpus = targetImdiObject.isCorpus();
             boolean targetIsSession = targetImdiObject.isSession();
             boolean targetIsImdiChild = targetImdiObject.isImdiChild();
             for (Enumeration<ImdiTreeObject> imdiObjectEnum = userFavourites.elements(); imdiObjectEnum.hasMoreElements();) {
-                ImdiTreeObject currentTemplateObject = imdiObjectEnum.nextElement();
-                boolean addThisTemplate = false;
-                if (targetIsCorpus && !currentTemplateObject.isImdiChild()) {
-                    addThisTemplate = true;
-                } else if (targetIsSession && currentTemplateObject.isImdiChild()) {
-                    addThisTemplate = GuiHelper.imdiSchema.nodeCanExistInNode(targetImdiObject, currentTemplateObject);
-                } else if (targetIsImdiChild && currentTemplateObject.isImdiChild()) {
-                    addThisTemplate = GuiHelper.imdiSchema.nodeCanExistInNode(targetImdiObject, currentTemplateObject);
+                ImdiTreeObject currentFavouritesObject = imdiObjectEnum.nextElement();
+                boolean addThisFavourites = false;
+                if (targetIsCorpus && !currentFavouritesObject.isImdiChild()) {
+                    addThisFavourites = true;
+                } else if (targetIsSession && currentFavouritesObject.isImdiChild()) {
+                    addThisFavourites = GuiHelper.imdiSchema.nodeCanExistInNode(targetImdiObject, currentFavouritesObject);
+                } else if (targetIsImdiChild && currentFavouritesObject.isImdiChild()) {
+                    addThisFavourites = GuiHelper.imdiSchema.nodeCanExistInNode(targetImdiObject, currentFavouritesObject);
                 }
-                if (addThisTemplate) {
-//                    System.out.println("adding: " + currentTemplateObject);
-                    validTemplates.add(new String[]{currentTemplateObject.toString(), currentTemplateObject.getUrlString()});
+                if (addThisFavourites) {
+//                    System.out.println("adding: " + currentFavouritesObject);
+                    validFavourites.add(new String[]{currentFavouritesObject.toString(), currentFavouritesObject.getUrlString()});
                 } else {
-                    // imdi child templates cannot be added to a corpus
+                    // imdi child favourites cannot be added to a corpus
                     // sessions cannot be added to a session
-//                    System.out.println("omitting: " + currentTemplateObject);
+//                    System.out.println("omitting: " + currentFavouriteObject);
                 }
             }
         }
-        return validTemplates.elements();
+        return validFavourites.elements();
     }
 
-    public String getNodeType(ImdiTreeObject templateImdiObject, ImdiTreeObject targetImdiObject) {
+    public String getNodeType(ImdiTreeObject favouriteImdiObject, ImdiTreeObject targetImdiObject) {
         // takes the source path and destination path and creates a complete combined path
         // in:
         // .METATRANSCRIPT.Session.MDGroup.Actors.Actor(12).Languages.Language(5)
@@ -127,99 +163,67 @@ public class LinorgFavourites {
         // out: 
         // .METATRANSCRIPT.Session.MDGroup.Actors.Actor(27).Languages.Language
 
-        String templateXmlPath = templateImdiObject.getURL().getRef();
+        String favouriteXmlPath = favouriteImdiObject.getURL().getRef();
         String targetXmlPath = targetImdiObject.getURL().getRef();
-        System.out.println("getNodeType: \ntemplateXmlPath: " + templateXmlPath + "\ntargetXmlPath:" + targetXmlPath);
+        System.out.println("getNodeType: \nfavouriteXmlPath: " + favouriteXmlPath + "\ntargetXmlPath:" + targetXmlPath);
         String returnValue;
-        if (templateImdiObject.isSession()) {
+        if (favouriteImdiObject.isSession()) {
             returnValue = ImdiSchema.imdiPathSeparator + "METATRANSCRIPT" + ImdiSchema.imdiPathSeparator + "Session";
-        } else if (templateImdiObject.isCorpus()) {
+        } else if (favouriteImdiObject.isCorpus()) {
             returnValue = ImdiSchema.imdiPathSeparator + "METATRANSCRIPT" + ImdiSchema.imdiPathSeparator + "Corpus";
-        } else if (templateImdiObject.isImdiChild()) {
+        } else if (favouriteImdiObject.isImdiChild()) {
             if (targetXmlPath == null) {
-                returnValue = templateXmlPath.replaceAll("\\(\\d*?\\)$", "");
+                returnValue = favouriteXmlPath.replaceAll("\\(\\d*?\\)$", "");
             } else {
                 // pass the (x) values on in the return value
                 System.out.println("targetXmlPath: " + targetXmlPath);
-                System.out.println("templateXmlPath: " + templateXmlPath);
-                templateXmlPath = templateXmlPath.replaceAll("\\(\\d*?\\)$", "");
-                String[] splitTemplateXmlPath = templateXmlPath.split("\\)");
+                System.out.println("favouriteXmlPath: " + favouriteXmlPath);
+                favouriteXmlPath = favouriteXmlPath.replaceAll("\\(\\d*?\\)$", "");
+                String[] splitFavouriteXmlPath = favouriteXmlPath.split("\\)");
                 String[] splitTargetXmlPath = targetXmlPath.split("\\)");
-                System.out.println("splitTemplateXmlPath: " + splitTemplateXmlPath.length + " splitTargetXmlPath: " + splitTargetXmlPath.length);
+                System.out.println("splitFavouriteXmlPath: " + splitFavouriteXmlPath.length + " splitTargetXmlPath: " + splitTargetXmlPath.length);
                 returnValue = "";
-                for (int partCounter = 0; partCounter < splitTemplateXmlPath.length; partCounter++) {
+                for (int partCounter = 0; partCounter < splitFavouriteXmlPath.length; partCounter++) {
                     if (splitTargetXmlPath.length > partCounter) {
                         returnValue = returnValue.concat(splitTargetXmlPath[partCounter] + ")");
                     } else {
-                        returnValue = returnValue.concat(splitTemplateXmlPath[partCounter] + ")");
+                        returnValue = returnValue.concat(splitFavouriteXmlPath[partCounter] + ")");
                     }
                 }
                 returnValue = returnValue.replaceAll("\\)$", "");
             }
-//            System.out.println("getNodeType returnValue: " + returnValue);
-//                        ////////////////////////////////////////////////////////////////////////////////////////////////////////////
-//                        // preserve the child node path
-//                        if (targetNodePath.replaceAll("[^(]*", "").length() >= templateNodePath.replaceAll("[^(]*", "").length()) {
-//                        String currentTemplateXPath = currentTemplate.replaceFirst("\\.xml$", "");
-//                        String currentTemplateName = currentTemplateXPath.substring(currentTemplateXPath.lastIndexOf(".") + 1);
-//                        System.out.println("currentTemplateXPath: " + currentTemplateXPath);
-//                        System.out.println("targetNodePath: " + targetNodePath);
-//                        String destinationXPath;
-//                        if (currentTemplateXPath.contains(")")) {
-//                            destinationXPath = targetNodePath + currentTemplateXPath.substring(currentTemplateXPath.lastIndexOf(")") + 1);
-//                        } else {
-//                            destinationXPath = currentTemplateXPath;
-//                        }
-//                        }
-//                        // end preserve the child node path
-//                        ////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-//                returnValue = returnValue.replaceAll("\\(\\d+\\)", "");
         } else {
             returnValue = null;
         }
         System.out.println("getNodeTypeReturnValue: " + returnValue);
-//        System.out.println("for : " + templateXmlPath);
         return returnValue;
     }
 
-    public void mergeFromFavourite(ImdiTreeObject targetImdiObject, ImdiTreeObject templateImdiObject, boolean overwriteValues) {
+    public void mergeFromFavourite(ImdiTreeObject targetImdiObject, ImdiTreeObject favouriteImdiObject, boolean overwriteValues) {
 //        System.out.println("mergeFromFavourite: " + addedNodeUrl + " : " + imdiTemplateUrl);
-//        ImdiTreeObject templateImdiObject = GuiHelper.imdiLoader.getImdiObject("", imdiTemplateObject);
-//        ImdiTreeObject targetImdiObject = GuiHelper.imdiLoader.getImdiObject("", addedNodeUrl);
-
-//        System.out.println("targetImdiObjectLoading: " + targetImdiObject.isLoading);
-//        while (targetImdiObject.isLoading) {
-//            try {
-//                Thread.currentThread().sleep(500);
-//            } catch (InterruptedException ie) {
-//                GuiHelper.linorgBugCatcher.logError(ie);
-//            }
-//        }
-
         Hashtable<String, ImdiField[]> targetFieldsHash = targetImdiObject.getFields();
-        for (Enumeration<ImdiField[]> templateFieldEnum = templateImdiObject.getFields().elements(); templateFieldEnum.hasMoreElements();) {
-            ImdiField[] currentTemplateFields = templateFieldEnum.nextElement();
-            if (currentTemplateFields.length > 0) {
-                ImdiField[] targetNodeFields = targetFieldsHash.get(currentTemplateFields[0].getTranslateFieldName());
+        for (Enumeration<ImdiField[]> favouriteFieldEnum = favouriteImdiObject.getFields().elements(); favouriteFieldEnum.hasMoreElements();) {
+            ImdiField[] currentFavouriteFields = favouriteFieldEnum.nextElement();
+            if (currentFavouriteFields.length > 0) {
+                ImdiField[] targetNodeFields = targetFieldsHash.get(currentFavouriteFields[0].getTranslateFieldName());
 
-                System.out.println("TranslateFieldName: " + currentTemplateFields[0].getTranslateFieldName());
+                System.out.println("TranslateFieldName: " + currentFavouriteFields[0].getTranslateFieldName());
                 System.out.println("targetImdiObjectLoading: " + targetImdiObject.isLoading());
                 if (targetNodeFields != null) {
                     System.out.println("copy fields");
-                    for (int fieldCounter = 0; fieldCounter < currentTemplateFields.length; fieldCounter++) {
+                    for (int fieldCounter = 0; fieldCounter < currentFavouriteFields.length; fieldCounter++) {
                         ImdiField currentField;
-                        if (targetNodeFields.length > fieldCounter) {// error here adding a template node
+                        if (targetNodeFields.length > fieldCounter) {
                             // copy to the exisiting fields
                             currentField = targetNodeFields[fieldCounter];
-                            currentField.setFieldValue(currentTemplateFields[fieldCounter].getFieldValue(), false);
+                            currentField.setFieldValue(currentFavouriteFields[fieldCounter].getFieldValue(), false);
                         } else {
                             // add sub nodes if they dont already exist
-                            currentField = new ImdiField(targetImdiObject, currentTemplateFields[fieldCounter].xmlPath, currentTemplateFields[fieldCounter].getFieldValue());
+                            currentField = new ImdiField(targetImdiObject, currentFavouriteFields[fieldCounter].xmlPath, currentFavouriteFields[fieldCounter].getFieldValue());
                             targetImdiObject.addField(currentField);
                             currentField.fieldNeedsSaveToDisk = true;
                         }
-                        String currentLanguageId = currentTemplateFields[fieldCounter].getLanguageId();
+                        String currentLanguageId = currentFavouriteFields[fieldCounter].getLanguageId();
                         if (currentLanguageId != null) {
                             currentField.setLanguageId(currentLanguageId, false);
                         }
@@ -227,9 +231,5 @@ public class LinorgFavourites {
                 }
             }
         }
-//        targetImdiObject.clearIcon();
-//        targetImdiObject.saveChangesToCache();
-    // loop the child nodes and add them accordingly
-
     }
 }
