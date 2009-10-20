dd8cd560ee7086207aa90c8bb571062676072594
Peter Withers <Peter.Withers@mpi.nl>
2009-10-20 14:05:26 +0000

    Added applet and jnlp templates that are used to create files with the current version number durring the build. Updated the and file to use the arbil directory on the server. Updated the ant file to create the jnlp and applet files and push them to the server at the same time as pushing the new jar file. Updated the type checker jar version in the ant file.

--- src/nl/mpi/arbil/LinorgVersion.java
+++ src/nl/mpi/arbil/LinorgVersion.java
@@ -2,9 +2,9 @@ package nl.mpi.arbil;
 public class LinorgVersion {
     String currentMajor = "1";
     String currentMinor = "0";
-    String currentRevision = "16757";
+    String currentRevision = "16771";
     String lastCommitDate = "Last Changed Date: 2009-10-20 12:06:05 +0200 (Tue, 20 Oct 2009)";
-    String compileDate = "Tue Oct 20 14:52:25 CEST 2009";
+    String compileDate = "Tue Oct 20 15:51:14 CEST 2009";
     String fullInfo = "\nPath: src/nl/mpi/arbil\nURL: svn+ssh://petwit@lux02.mpi.nl/srv/svn/LAT/Arbil/branches/arbil.1.0.x/src/nl/mpi/arbil\nRepository Root: svn+ssh://petwit@lux02.mpi.nl/srv/svn/LAT\nRepository UUID: d4234c5b-7c33-0410-a85d-91bdcf7faf44\nRevision: 16754\nNode Kind: directory\nSchedule: normal\nLast Changed Author: petwit\nLast Changed Rev: 16754\nLast Changed Date: 2009-10-20 12:06:05 +0200 (Tue, 20 Oct 2009)\n";
 }
         
