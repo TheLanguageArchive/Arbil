692cfce4aec7608c7266ddcfb0b1cf7c731e1e6a
Peter Withers <Peter.Withers@mpi.nl>
2009-10-20 10:06:05 +0000

    bumped Arbil up to version 1.0.x

--- src/nl/mpi/arbil/LinorgVersion.java
+++ src/nl/mpi/arbil/LinorgVersion.java
@@ -1,10 +1,10 @@
 package nl.mpi.arbil;
 public class LinorgVersion {
-    String currentMajor = "";
-    String currentMinor = "";
-    String currentRevision = "";
-    String lastCommitDate = "";
-    String compileDate = "";
-    String fullInfo = "";
+    String currentMajor = "0";
+    String currentMinor = "4";
+    String currentRevision = "16740";
+    String lastCommitDate = "Last Changed Date: 2009-10-07 13:31:21 +0200 (Wed, 07 Oct 2009)";
+    String compileDate = "Mon Oct 19 14:53:22 CEST 2009";
+    String fullInfo = "\nPath: src/nl/mpi/arbil\nURL: svn+ssh://petwit@lux02.mpi.nl/srv/svn/LAT/Arbil/trunk/src/nl/mpi/arbil\nRepository Root: svn+ssh://petwit@lux02.mpi.nl/srv/svn/LAT\nRepository UUID: d4234c5b-7c33-0410-a85d-91bdcf7faf44\nRevision: 16631\nNode Kind: directory\nSchedule: normal\nLast Changed Author: petwit\nLast Changed Rev: 16631\nLast Changed Date: 2009-10-07 13:31:21 +0200 (Wed, 07 Oct 2009)\n";
 }
-            
+        
