a5cd57d38e9c6c9e439f7e2ac6f82680928f22b8
Peter Withers <Peter.Withers@mpi.nl>
2010-03-25 16:42:03 +0000

    Branched the testing version of Arbil so that the Clarin changes can continue in trunk without affecting the current stable/testing versions.

--- src/nl/mpi/arbil/LinorgVersion.java
+++ src/nl/mpi/arbil/LinorgVersion.java
@@ -1,10 +1,10 @@
 package nl.mpi.arbil;
 public class LinorgVersion {
-    String currentMajor = "";
-    String currentMinor = "";
-    String currentRevision = "";
-    String lastCommitDate = "";
-    String compileDate = "";
-    String fullInfo = "";
+    public String currentMajor = "1";
+    public String currentMinor = "3";
+    public String currentRevision = "18085";
+    public String lastCommitDate = "Last Changed Date: 2010-02-10 16:03:24 +0100 (Wed, 10 Feb 2010)";
+    public String compileDate = "Wed Mar 17 17:30:14 CET 2010";
+    public String fullInfo = "\nPath: src/nl/mpi/arbil\nURL: svn+ssh://petwit@lux02.mpi.nl/srv/svn/LAT/Arbil/trunk/src/nl/mpi/arbil\nRepository Root: svn+ssh://petwit@lux02.mpi.nl/srv/svn/LAT\nRepository UUID: d4234c5b-7c33-0410-a85d-91bdcf7faf44\nRevision: 17750\nNode Kind: directory\nSchedule: normal\nLast Changed Author: petwit\nLast Changed Rev: 17747\nLast Changed Date: 2010-02-10 16:03:24 +0100 (Wed, 10 Feb 2010)\n";
 }
-            
+        
