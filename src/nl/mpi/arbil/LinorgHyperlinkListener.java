1e8d38f096d2cfa8ab1eeed587d4d43aac99572d
Peter Withers <Peter.Withers@mpi.nl>
2010-04-19 12:14:10 +0000

    Created the concept if Arbil Script to be used in the html help text, currently this can be used to create root node sessions, corpus branches and catalogues. When complete this should make it easy to create custom wizards to ease new users into the concept of the IMDI structure.

--- src/nl/mpi/arbil/LinorgHyperlinkListener.java
+++ src/nl/mpi/arbil/LinorgHyperlinkListener.java
@@ -3,6 +3,7 @@ package nl.mpi.arbil;
 import javax.swing.JEditorPane;
 import javax.swing.event.HyperlinkEvent;
 import javax.swing.event.HyperlinkListener;
+import nl.mpi.arbil.data.ImdiTreeObject;
 
 /**
  * Document   : LinorgHyperlinkListener
@@ -12,7 +13,7 @@ import javax.swing.event.HyperlinkListener;
 public class LinorgHyperlinkListener implements HyperlinkListener {
 
     public void hyperlinkUpdate(HyperlinkEvent evt) {
-        System.out.println("hyperlinkUpdate");
+//        System.out.println("hyperlinkUpdate");
         if (evt.getEventType() == HyperlinkEvent.EventType.ACTIVATED) {
             JEditorPane pane = (JEditorPane) evt.getSource();
 //            try {
@@ -21,7 +22,25 @@ public class LinorgHyperlinkListener implements HyperlinkListener {
 //            } catch (IOException e) {
 //                System.out.println(e.getMessage());
 //            }
-            if (!evt.getURL().toString().toLowerCase().startsWith("javascript")) {
+//            System.out.println("getURL: " +evt.getURL());
+//            System.out.println("getDescription: " + evt.getDescription());
+//            System.out.println(evt.getSourceElement());
+//            System.out.println(evt.getEventType());
+//            System.out.println(evt.toString());
+//            System.out.println(evt.getSource());
+            if (evt.getDescription().startsWith("arbilscript:")) {
+                String arbilscriptString = evt.getDescription().substring("arbilscript:".length());
+                System.out.println("acting on arbilscript: " + arbilscriptString);
+                String[] commandsArray = arbilscriptString.split("&");
+                for (String commandString : commandsArray) {
+                    System.out.println("commandString: " + commandString);
+                }
+                if (commandsArray[0].startsWith("add=")) {
+                    String nodeTypeString = commandsArray[0].substring("add=".length());
+                    System.out.println("nodeTypeString: " + nodeTypeString);
+                    ImdiTreeObject.requestRootAddNode(nodeTypeString, "Wizard Corpus");
+                }
+            } else if (evt.getURL() != null) {
                 LinorgWindowManager.getSingleInstance().openUrlWindowOnce(evt.getURL().toString(), evt.getURL());
             }
         }
