d9c040df1062ade0b9a9d47c163a38e0940cd0cf
Peter Withers <Peter.Withers@mpi.nl>
2010-05-22 15:27:45 +0000

    Updated the application title text in the windows installer. Modified the schema validation method (Check Format) to first use the name space url defined in the xml and then fall back to the imdi schema and then the schema provided in the jar file. Added clarin XSD reader. Loaded Clarin templates from the schema location of cmdi files. Addded component insertion into clarin files (via add menu). Added auto recovery from history when a file is missing. Updated the add menu contents to handle clarin sub nodes. Corrected nodes left after a delete from the tree. Updated the sub child node detection when reading in the metadata xml to handle cmdi templates with full paths. Added clarin link reader. Inserted Clarin linked nodes into the Arbil tree. Stored all clarin profile schemas on disk when they are first seen. Added Handle resolution and general URL resolution in Clarin links. Added test fields to the wizard html. Added CMDI function to tell when a resource can be added or not to a specific node. Set the profile name in clarin templates. Added test for drag and drop onto clarin nodes. Added resource drop action to add resource files to CMDI nodes. Set the drag cursor to indicate allow drops to CMDI nodes based on the values in the schema. Made more robust against broken cmdi resource links. Added resource proxy nodes for CMDI files when adding resources. Truncated clarin pathnames. Provided a default node name based on the profile name of a cmdi node. Started getting the filed constraints from the xsd. Corrected the adding of sub nodes such as actors. When a sub node is added the correct node path is returned for display in a table. Added check for updates menu item. Added template and profile selection dialogue. Added thread and progress bar for clarin profile loading due to 12mb file sizes. Populated the add menu from the chosen templates and profiles. Enabled adding the selected builtin templates and clarin profiles from the add menu. Modified the template system to work with multiple template/profile selections rather than a single selection. Removed need for "id" attributes that clash with the clarin resource attributes. Added icons to the add menu to make it clear which nodes are IMDI or Clarin before adding them. Prevented error with the language dropdown during editing in the table. Sorted the entries in the templates/profiles dialogue. Removed unrequired jar dependancies. Made the import dialogue process ready to start importing cmdi nodes. Added ability for users to add custom profile urls and files in the templates/profiles dialogue so that users can preview their work while it is still being designed in the clarin servers workspaces.

--- src/nl/mpi/arbil/MetadataFile/ImdiUtils.java
+++ src/nl/mpi/arbil/MetadataFile/ImdiUtils.java
@@ -5,13 +5,13 @@ import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URI;
 import java.net.URISyntaxException;
+import javax.xml.transform.TransformerException;
 import mpi.imdi.api.IMDIDom;
 import mpi.imdi.api.IMDILink;
 import mpi.imdi.api.WSNodeType;
 import mpi.util.OurURL;
 import nl.mpi.arbil.GuiHelper;
 import nl.mpi.arbil.LinorgWindowManager;
-import nl.mpi.arbil.data.ImdiTreeObject;
 import org.w3c.dom.Document;
 
 /**
@@ -23,12 +23,59 @@ public class ImdiUtils implements MetadataUtils {
 
     public static IMDIDom api = new IMDIDom();
 
-    public boolean addCorpusLink(ImdiTreeObject targetImdiNodes, ImdiTreeObject[] childImdiNodes) {
-        throw new UnsupportedOperationException("Not supported yet.");
+    private boolean isCatalogue(URI sourceURI) {
+        try {
+            mpi.util.OurURL inUrlLocal = new mpi.util.OurURL(sourceURI.toURL());
+            org.w3c.dom.Document nodDom = api.loadIMDIDocument(inUrlLocal, false);
+            return null != org.apache.xpath.XPathAPI.selectSingleNode(nodDom, "/:METATRANSCRIPT/:Catalogue");
+        } catch (MalformedURLException exception) {
+            GuiHelper.linorgBugCatcher.logError(exception);
+        } catch (TransformerException exception) {
+            GuiHelper.linorgBugCatcher.logError(exception);
+        }
+        return false;
     }
 
-    public boolean addCorpusLink(URI nodeURI, URI linkURI) {
-        throw new UnsupportedOperationException("Not supported yet.");
+    private boolean isSession(URI sourceURI) {
+        try {
+            mpi.util.OurURL inUrlLocal = new mpi.util.OurURL(sourceURI.toURL());
+            org.w3c.dom.Document nodDom = api.loadIMDIDocument(inUrlLocal, false);
+            return null != org.apache.xpath.XPathAPI.selectSingleNode(nodDom, "/:METATRANSCRIPT/:Session");
+        } catch (MalformedURLException exception) {
+            GuiHelper.linorgBugCatcher.logError(exception);
+        } catch (TransformerException exception) {
+            GuiHelper.linorgBugCatcher.logError(exception);
+        }
+        return false;
+    }
+
+    public boolean addCorpusLink(URI nodeURI, URI linkURI[]) {
+        try {
+            Document nodDom;
+            OurURL inUrlLocal = new OurURL(nodeURI.toURL());
+            nodDom = api.loadIMDIDocument(inUrlLocal, false);
+            if (nodDom == null) {
+                GuiHelper.linorgBugCatcher.logError(new Exception(api.getMessage()));
+                LinorgWindowManager.getSingleInstance().addMessageDialogToQueue("Error reading via the IMDI API", "Add Link");
+                return false;
+            } else {
+                int nodeType = WSNodeType.CORPUS;
+                for (URI currentLinkUri : linkURI) {
+                    if (isCatalogue(currentLinkUri)) {
+                        nodeType = WSNodeType.CATALOGUE;
+                    }
+                    if (isSession(currentLinkUri)) {
+                        nodeType = WSNodeType.SESSION;
+                    }
+                    api.createIMDILink(nodDom, inUrlLocal, currentLinkUri.toString(), "", nodeType, "");
+                }
+                api.writeDOM(nodDom, new File(nodeURI), true);
+                return true;
+            }
+        } catch (MalformedURLException ex) {
+            GuiHelper.linorgBugCatcher.logError(ex);
+        }
+        return true;
     }
 
     public boolean copyImdiFile(URI sourceURI, File destinationFile, URI[] linksToUpdate, boolean updateLinks) {
@@ -70,12 +117,34 @@ public class ImdiUtils implements MetadataUtils {
         throw new UnsupportedOperationException("Not supported yet.");
     }
 
-    public boolean removeCorpusLink(ImdiTreeObject targetImdiNodes, ImdiTreeObject[] childImdiNodes) {
-        throw new UnsupportedOperationException("Not supported yet.");
-    }
+    public boolean removeCorpusLink(URI nodeURI, URI linkURI[]) {
+        try {
+            OurURL destinationUrl = new OurURL(nodeURI.toString());
+            Document nodDom = api.loadIMDIDocument(destinationUrl, false);
+            IMDILink[] allImdiLinks;
+            allImdiLinks = api.getIMDILinks(nodDom, destinationUrl, WSNodeType.UNKNOWN);
+            if (allImdiLinks != null) {
+                for (IMDILink currentLink : allImdiLinks) {
+                    for (URI currentUri : linkURI) {
+                        try {
+                            if (currentUri.equals(currentLink.getRawURL().toURL().toURI())) {
+//                                if (currentLink.getType())
+                                api.removeIMDILink(nodDom, currentLink);
+//                                api.createIMDILink(nodDom, null, "", "", WSNodeType.CATALOGUE, "");
+                            }
+                        } catch (URISyntaxException exception) {
+                            GuiHelper.linorgBugCatcher.logError(exception);
+                        }
+                    }
+                }
+                return true;
+            }
 
-    public boolean removeCorpusLink(URI nodeURI, URI linkURI) {
-        throw new UnsupportedOperationException("Not supported yet.");
+        } catch (MalformedURLException exception) {
+            GuiHelper.linorgBugCatcher.logError(exception);
+            LinorgWindowManager.getSingleInstance().addMessageDialogToQueue("Error reading links via the IMDI API", "Get Links");
+        }
+        return false;
     }
 
     public URI[] getCorpusLinks(URI nodeURI) {
@@ -86,7 +155,8 @@ public class ImdiUtils implements MetadataUtils {
             allImdiLinks = api.getIMDILinks(nodDom, destinationUrl, WSNodeType.UNKNOWN);
             if (allImdiLinks != null) {
                 URI[] returnUriArray = new URI[allImdiLinks.length];
-                for (int linkCount = 0; linkCount < allImdiLinks.length; linkCount++) {
+                for (int linkCount = 0; linkCount
+                        < allImdiLinks.length; linkCount++) {
                     try {
                         returnUriArray[linkCount] = allImdiLinks[linkCount].getRawURL().toURL().toURI();
                     } catch (URISyntaxException exception) {
@@ -103,137 +173,4 @@ public class ImdiUtils implements MetadataUtils {
         }
         return null;
     }
-
-////    public boolean updateSingleLink(URI oldLocationURI, File currentLocationFile) {
-////        throw new UnsupportedOperationException("Not supported yet.");
-////    }
-//    private boolean addCorpusLink(ImdiTreeObject imdiTreeObject, ImdiTreeObject targetImdiNode) {
-//        boolean linkAlreadyExists = false;
-//        if (targetImdiNode.isCatalogue()) {
-//            if (imdiTreeObject.hasCatalogue()) {
-////                LinorgWindowManager.getSingleInstance().addMessageDialogToQueue("Only one catalogue can be added", null);
-//                // prevent adding a second catalogue file
-//                return false;
-//            }
-//        }
-////        for (String[] currentLinkPair : imdiTreeObject.childLinks) {
-////            String currentChildPath = currentLinkPair[0];
-////            if (!targetImdiNode.waitTillLoaded()) { // we must wait here before we can tell if it is a catalogue or not
-////                LinorgWindowManager.getSingleInstance().addMessageDialogToQueue("Error adding node, could not wait for file to load", "Loading Error");
-////                return false;
-////            }
-////            if (currentChildPath.equals(targetImdiNode.getUrlString())) {
-////                linkAlreadyExists = true;
-////            }
-////        }
-//        if (targetImdiNode.getUrlString().equals(imdiTreeObject.getUrlString())) {
-//            LinorgWindowManager.getSingleInstance().addMessageDialogToQueue("Cannot link or move a node into itself", null);
-//            return false;
-//        }
-//        if (linkAlreadyExists) {
-//            LinorgWindowManager.getSingleInstance().addMessageDialogToQueue(targetImdiNode + " already exists in " + this + " and will not be added again", null);
-//            return false;
-//        } else {
-//            // if link is not already there
-//            // if needs saving then save now while you can
-//            // TODO: it would be nice to warn the user about this, but its a corpus node so maybe it is not important
-//            if (imdiTreeObject.getNeedsSaveToDisk()) {
-//                imdiTreeObject.saveChangesToCache(true);
-//            }
-//
-//            Document nodDom;
-//            try {
-//                synchronized (imdiTreeObject.domLockObject) {
-//                    OurURL inUrlLocal = new OurURL(imdiTreeObject.getURI().toURL());
-//                    nodDom = api.loadIMDIDocument(inUrlLocal, false);
-//                    if (nodDom == null) {
-//                        GuiHelper.linorgBugCatcher.logError(new Exception(api.getMessage()));
-//                        LinorgWindowManager.getSingleInstance().addMessageDialogToQueue("Error reading via the IMDI API", "Add Link");
-//                        return false;
-//                    } else {
-//                        int nodeType = WSNodeType.CORPUS;
-//                        if (targetImdiNode.isSession()) {
-//                            nodeType = WSNodeType.SESSION;            // url: IMDI location, for link normalization.  urlToLink: target URL
-//                            // linkName: for CorpusLink name / for InfoFile description
-//                            // linkType: WSNodeType value  spec: where to put the link in the IMDI,
-//                            // NOTE: spec should only be used for linkType InfoFile...
-//                            // public IMDILink createIMDILink(Document doc, OurURL url, String urlToLink, String linkName, int linkType, String spec);
-//                        } else if (targetImdiNode.isCatalogue()) {
-//                            nodeType = WSNodeType.CATALOGUE;
-//                        }
-//// TODO: at this point due to the api we cannot get the id of the newly created link, so we will probably have to unload this object and reload the dom
-//                        System.out.println("createIMDILink: " + targetImdiNode.getUrlString());
-//                        api.createIMDILink(nodDom, inUrlLocal, targetImdiNode.getUrlString(), /*targetImdiNode.toString()*/ "", nodeType, "");
-//                        imdiTreeObject.bumpHistory();
-//                        api.writeDOM(nodDom, imdiTreeObject.getFile(), false);
-//                    }
-//                }
-//            } catch (Exception ex) {
-//                GuiHelper.linorgBugCatcher.logError(ex);
-////            System.out.println("Exception: " + ex.getMessage());
-//            }
-//            //loadChildNodes(); // this must not be done here
-////            clearIcon(); // this must be cleared so that the leaf / branch flag gets set
-//            return true;
-//        }
-//    }
-//    // this is used to delete an IMDI node from a corpus branch
-//
-//    private void deleteCorpusLink(ImdiTreeObject imdiTreeObject, ImdiTreeObject[] targetImdiNodes) {
-//        // retrieve the node id for the link
-//        ArrayList<String> fieldIdList = new ArrayList<String>();
-//        String linkIdString = null;
-////        for (String[] currentLinkPair : imdiTreeObject.childLinks) {
-////            String currentChildPath = currentLinkPair[0];
-//////                System.out.println("currentChildPath: " + currentChildPath);
-////            for (ImdiTreeObject currentImdiNode : targetImdiNodes) {
-//////                    System.out.println("targetImdiNode :  " + currentImdiNode.getUrlString());
-////                if (currentChildPath.equals(currentImdiNode.getUrlString())) {
-//////                      System.out.println("currentLinkPair[1]: " + currentLinkPair[1]);
-////                    linkIdString = currentLinkPair[1];
-////                    fieldIdList.add(linkIdString);
-////                }
-////            }
-////        }
-//        if (fieldIdList.size() > 0) {
-////            todo: this use must stay since it is the only reliable way to delete links via the api
-//            //deleteFromDomViaId(fieldIdList.toArray(new String[]{}));
-//            GuiHelper.linorgBugCatcher.logError(new Exception("deleteFromDomViaId"));
-//        }
-//        for (ImdiTreeObject currentImdiNode : targetImdiNodes) {
-//            if (currentImdiNode.isCatalogue()) {
-//                // the catalogue implemention in the imdi api requires special treatment and so must be done in this way not via deleteFromDomViaId
-//                // do this last because it might change the domid ordering
-//                deleteCatalogueLink(imdiTreeObject);
-//            }
-//        }
-//        //loadChildNodes(); // this must not be done here
-//        imdiTreeObject.clearIcon(); // this must be cleared so that the leaf / branch flag gets set
-//    }
-//
-//    private void deleteCatalogueLink(ImdiTreeObject imdiTreeObject) {
-////        todo move this to a separate class and remove the api from this class
-//        // the catalogue implemention in the imdi api requires special treatment and so must be done in this way not via deleteFromDomViaId
-//        Document nodDom;
-//        try {
-//            if (imdiTreeObject.getNeedsSaveToDisk()) {
-//                imdiTreeObject.saveChangesToCache(false);
-//            }
-//            synchronized (imdiTreeObject.domLockObject) {
-//                OurURL inUrlLocal = new OurURL(imdiTreeObject.getURI().toURL());
-//                nodDom = api.loadIMDIDocument(inUrlLocal, false);
-//                if (nodDom == null) {
-//                    GuiHelper.linorgBugCatcher.logError(new Exception(api.getMessage()));
-//                    LinorgWindowManager.getSingleInstance().addMessageDialogToQueue("Error loading via the IMDI API", "Remove Catalogue");
-//                } else {
-//                    api.createIMDILink(nodDom, null, "", "", WSNodeType.CATALOGUE, "");
-//                    imdiTreeObject.bumpHistory();
-//                    api.writeDOM(nodDom, imdiTreeObject.getFile(), false);
-//                    imdiTreeObject.reloadNode();
-//                }
-//            }
-//        } catch (Exception ex) {
-//            GuiHelper.linorgBugCatcher.logError(ex);
-//        }
-//    }
 }
