abe7bd152862b42aaa4be2bdcb10dd502f5660fc
Peter Withers <Peter.Withers@mpi.nl>
2010-06-25 11:53:44 +0000

    Resolved the issue re-importing nodes when the storage location is non standard. Replaced indicache with ArbilWorkingFiles as default when no existing directory is found. Replaced change cache directory menu item with change local storage directory menu item. Added text find and replace to the table. Added find replace into the context menu. Added the insertion of dummy data into clarin nodes when required. Added profile preview class. Added the display of info links in the tree and the table or previewed when html/txt. Corrected the defaults in the catalogue template. Resolved issue when a user selects a cell and uses ctrl+a to change the selection in a table the selection mode does not change from cell to row and hence caused the copied data to be in the wrong format. Fixed error reloading remote nodes via the tree context menu. Corrected error detecting deletable fields in a table of cmdi data.

--- src/nl/mpi/arbil/templates/ArbilTemplate.java
+++ src/nl/mpi/arbil/templates/ArbilTemplate.java
@@ -134,8 +134,9 @@ public class ArbilTemplate {
     public boolean pathIsDeleteableField(String nodePath) {
         // modify the path to match the file name until the file name and assosiated array is updated to contain the xmpath filename and menu text
         String imdiNodePath = nodePath.substring(1) + ".xml";
+        String cmdiNodePath = nodePath.replaceAll("\\(x\\)", "");
         for (String[] pathString : templatesArray) {
-            if (pathString[0].equals((nodePath)) || pathString[0].equals(imdiNodePath)) {
+            if (pathString[0].equals((cmdiNodePath)) || pathString[0].equals(imdiNodePath)) {
                 return true;
             }
         }
