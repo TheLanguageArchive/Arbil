3bf1799c503ec41461486a72d9fa8da4b3784aa6
Peter Withers <Peter.Withers@mpi.nl>
2010-09-23 09:47:02 +0000

    Moved the table cell and long field editor and sub components into a separate package. Preserved the archive handles on import and export so that LAMUS knows which files are which. Added the field usage description as a heading in the long field editor. Added local caching and ability to use external xsd files for Clarin such as xml-lang. Added check for unnamed nodes when reading clarin profiles. Updated delete node to remove the deleted node from all tables when deleting from the root of the tree. Updated the delete node to also close the relevant long field editors. Added an indicator for individual file download progress (file name and bytes) to the import export dialogue. Corrected the removal of nodes and sub nodes from the tree, tables and long field editors when a node is deleted. Allowed cookies in the applet version, so that shibboleth logins will function (when not logged in the shibboleth cookies overrun the server headers hence they are normally disabled). When there is no type checker result any previous value is now kept rather than being removed. Prevented the test file that is used to test write access to the working directory being left when the application is abnormally terminated. Added function of closing any long field editors of a node when it is deleted. Corrected issue when saving from the long field editor where the freshly typed value was lost. Corrected issue when adding a description etc. where the long field editor could contain the wrong number of fields after reloading. When saving made sure that the currently edited table or long field editor stores its data first. Modified the lux17 applet page to be php and provided the logged in username to the applet. Prevented cookies being blocked when in the applet. Added a log out file menu item to the applet. Added logged in user name to the log out file menu item in the applet. Corrected typo in dialogue. Added remote corpus search.

--- src/nl/mpi/arbil/RemoteServerSearchTerm.java
+++ src/nl/mpi/arbil/RemoteServerSearchTerm.java
@@ -3,8 +3,24 @@ package nl.mpi.arbil;
 import java.awt.Color;
 import java.awt.event.FocusEvent;
 import java.awt.event.FocusListener;
+import java.io.IOException;
+import java.net.MalformedURLException;
 import java.net.URI;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.util.ArrayList;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.TransformerException;
 import nl.mpi.arbil.data.ImdiTreeObject;
+import org.w3c.dom.DOMException;
+import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
 
 /**
  * Document   : RemoteServerSearchTerm
@@ -15,7 +31,8 @@ public class RemoteServerSearchTerm extends javax.swing.JPanel {
 
     javax.swing.JPanel thisPanel = this;
     ImdiNodeSearchPanel parentPanel;
-    private javax.swing.JTextField searchField;
+    private JTextField searchField;
+    private JLabel resultCountLabel;
     public String searchString = "";
     private String valueFieldMessage = "<remote server search term (required)>";
     URI[] searchResults = null;
@@ -52,8 +69,57 @@ public class RemoteServerSearchTerm extends javax.swing.JPanel {
                 parentPanel.stopSearch();
             }
         });
+        resultCountLabel = new JLabel();
         this.add(searchField);
+        this.add(resultCountLabel);
+    }
 
+    private String[] performSearch(String searchString) {
+        ArrayList<String> returnArray = new ArrayList<String>();
+        int maxResultNumber = 10;
+        try {
+            String fullQueryString = "http://corpus1.mpi.nl/ds/imdi_search/servlet?action=getMatches";
+            fullQueryString += "&num=" + maxResultNumber;
+            fullQueryString += "&query=" + URLEncoder.encode(searchString, "UTF-8");
+            fullQueryString += "&type=simple";
+            fullQueryString += "&nodeid=MPI77915%23";
+            fullQueryString += "&returnType=xml";
+            try {
+                LinorgWindowManager.getSingleInstance().openUrlWindowOnce("Search Result", new URL(fullQueryString));
+            } catch (MalformedURLException exception) {
+                GuiHelper.linorgBugCatcher.logError(exception);
+            }
+            DocumentBuilderFactory documentBuilderFactory = DocumentBuilderFactory.newInstance();
+            documentBuilderFactory.setValidating(false);
+            documentBuilderFactory.setNamespaceAware(true);
+            DocumentBuilder documentBuilder = documentBuilderFactory.newDocumentBuilder();
+            Document resultsDocument = documentBuilder.parse(fullQueryString);
+
+            String handleXpath = "/ImdiSearchResponse/Result/Match/URL";
+
+            NodeList domIdNodeList = org.apache.xpath.XPathAPI.selectNodeList(resultsDocument, handleXpath);
+            for (int nodeCounter = 0; nodeCounter < domIdNodeList.getLength(); nodeCounter++) {
+                Node urlNode = domIdNodeList.item(nodeCounter);
+                if (urlNode != null) {
+                    System.out.println(urlNode.getTextContent());
+                    returnArray.add(urlNode.getTextContent());
+                }
+            }
+        } catch (DOMException exception) {
+            GuiHelper.linorgBugCatcher.logError(exception);
+        } catch (IOException exception) {
+            GuiHelper.linorgBugCatcher.logError(exception);
+        } catch (ParserConfigurationException exception) {
+            GuiHelper.linorgBugCatcher.logError(exception);
+        } catch (SAXException exception) {
+            GuiHelper.linorgBugCatcher.logError(exception);
+        } catch (TransformerException exception) {
+            GuiHelper.linorgBugCatcher.logError(exception);
+        }
+        if (returnArray.size() >= maxResultNumber) {
+            LinorgWindowManager.getSingleInstance().addMessageDialogToQueue("Found more results than can be displayed, only showing the first " + maxResultNumber + " results", "Remote Search");
+        }
+        return returnArray.toArray(new String[]{});
     }
 
     public URI[] getServerSearchResults(ImdiTreeObject[] imdiTreeObject) {
@@ -66,9 +132,12 @@ public class RemoteServerSearchTerm extends javax.swing.JPanel {
                 return searchResults;
             } else {
                 lastSearchString = searchField.getText();
+                performSearch(lastSearchString);
+
+
+
                 searchResults = new URI[]{imdiTreeObject[0].getURI()};
-
-
+                resultCountLabel.setText(searchResults.length + " found on server ");
 
 //    // todo: add remote search: use console output of the applet search at http://corpus1.mpi.nl/ds/imdi_browser/
 //    // for example http://corpus1.mpi.nl/ds/imdi_search/servlet?action=getMatches&num=50&query=Sebastian&type=simple&nodeid=MPI77915%23&returnType=xml
