78e9c5e49349e857ac7013ec7e084636c2b6d16f
Peter Withers <Peter.Withers@mpi.nl>
2010-10-08 11:38:20 +0000

    Added a restriction on how many nodes can be added to a file when specified in the template and added user feedback when the limit is reached.

--- src/nl/mpi/arbil/LinorgHyperlinkListener.java
+++ src/nl/mpi/arbil/LinorgHyperlinkListener.java
@@ -1,12 +1,17 @@
 package nl.mpi.arbil;
 
+import java.awt.Component;
+import java.awt.Container;
 import java.io.File;
 import java.net.URI;
+import javax.swing.JButton;
 import javax.swing.JEditorPane;
+import javax.swing.JTextField;
 import javax.swing.event.HyperlinkEvent;
 import javax.swing.event.HyperlinkListener;
 import nl.mpi.arbil.data.ImdiLoader;
 import nl.mpi.arbil.MetadataFile.MetadataReader;
+import nl.mpi.arbil.clarin.ArbilMetadataException;
 import nl.mpi.arbil.data.ImdiTreeObject;
 import nl.mpi.arbil.data.MetadataBuilder;
 
@@ -21,6 +26,10 @@ public class LinorgHyperlinkListener implements HyperlinkListener {
 //        System.out.println("hyperlinkUpdate");
         if (evt.getEventType() == HyperlinkEvent.EventType.ACTIVATED) {
             JEditorPane pane = (JEditorPane) evt.getSource();
+
+//            HTMLDocument doc = (HTMLDocument) pane.getDocument();
+//            System.out.println("# of Components in JTextPane: " + pane.getComponentCount());
+
 //            try {
 //                System.out.println(evt.getURL());
 //                pane.setPage(evt.getURL());
@@ -34,23 +43,53 @@ public class LinorgHyperlinkListener implements HyperlinkListener {
 //            System.out.println(evt.toString());
 //            System.out.println(evt.getSource());
             if (evt.getDescription().startsWith("arbilscript:")) {
-                ImdiTreeObject currentImdiObject = null;
-                String arbilscriptString = evt.getDescription().substring("arbilscript:".length());
-                System.out.println("acting on arbilscript: " + arbilscriptString);
-                String[] commandsArray = arbilscriptString.split("&");
-                for (String commandString : commandsArray) {
-                    System.out.println("commandString: " + commandString);
-                    if (commandString.startsWith("add=")) {
-                        String nodeTypeString = commandString.substring("add=".length());
-                        System.out.println("nodeTypeString: " + nodeTypeString);
-                        currentImdiObject = addNode(currentImdiObject, nodeTypeString, "Wizard Corpus", null, null, null);
+                try {
+                    ImdiTreeObject currentImdiObject = null;
+                    String arbilscriptString = evt.getDescription().substring("arbilscript:".length());
+                    System.out.println("acting on arbilscript: " + arbilscriptString);
+                    String[] commandsArray = arbilscriptString.split("&");
+                    for (String commandString : commandsArray) {
+                        System.out.println("commandString: " + commandString);
+                        if (commandString.startsWith("add=")) {
+                            String nodeTypeString = commandString.substring("add=".length());
+                            System.out.println("nodeTypeString: " + nodeTypeString);
+                            currentImdiObject = addNode(currentImdiObject, nodeTypeString, "Wizard Corpus", null, null, null);
+                        }
+                        if (commandString.startsWith("set=")) {
+                            String[] fieldCommand = commandString.substring("set=".length()).split(":");
+                            System.out.println("set: " + fieldCommand[0] + " = " + fieldCommand[1]);
+                            setField(currentImdiObject, fieldCommand[0], fieldCommand[1]);
+                        }
                     }
-                    if (commandString.startsWith("set=")) {
-                        String[] fieldCommand = commandString.substring("set=".length()).split(":");
-                        System.out.println("set: " + fieldCommand[0] + " = " + fieldCommand[1]);
-                        setField(currentImdiObject, fieldCommand[0], fieldCommand[1]);
+                    // read the form values
+                    // todo: resolve the issue of not being able to get the html name of the components, only the index number is available
+                    for (int i = 0; i < pane.getComponentCount(); i++) {
+                        Container c = (Container) pane.getComponent(i);
+                        System.out.println(c.getComponentCount());
+                        Component swingComponentOfHTMLInputType = c.getComponent(0);
+                        System.out.println(swingComponentOfHTMLInputType.getClass().getName());
+                        if (swingComponentOfHTMLInputType instanceof JTextField) {
+                            JTextField tf = (JTextField) swingComponentOfHTMLInputType;
+                            System.out.println(tf.getName());
+                            System.out.println(tf.getText());
+                            System.out.println(tf.getAction());
+                            System.out.println(swingComponentOfHTMLInputType.getName());
+                            System.out.println(swingComponentOfHTMLInputType.getName());
+                            System.out.println(swingComponentOfHTMLInputType.getName());
+                            String formCommandString = swingComponentOfHTMLInputType.getName();
+                            System.out.println("formCommandString: " + formCommandString);
+                            if (formCommandString != null && formCommandString.startsWith("arbilscript:set=")) {
+                                String nodeTypeString = formCommandString.substring("arbilscript:set=".length());
+                                System.out.println("nodeTypeString: " + nodeTypeString);
+                                currentImdiObject = addNode(currentImdiObject, nodeTypeString, tf.getText(), null, null, null);
+                            }
+                        } else if (swingComponentOfHTMLInputType instanceof JButton) {
+                        }
                     }
+                } catch (ArbilMetadataException exception) {
+                    LinorgWindowManager.getSingleInstance().addMessageDialogToQueue(exception.getLocalizedMessage(), "Insert node error");
                 }
+
             } else if (evt.getURL() != null) {
                 LinorgWindowManager.getSingleInstance().openUrlWindowOnce(evt.getURL().toString(), evt.getURL());
             }
@@ -66,7 +105,7 @@ public class LinorgHyperlinkListener implements HyperlinkListener {
     }
 
     // note that this must not be used on nodes currently being edited because it bypasses the imdi loader process
-    private ImdiTreeObject addNode(ImdiTreeObject parentNode, String nodeType, String nodeTypeDisplayName, String targetXmlPath, URI resourceUri, String mimeType) {
+    private ImdiTreeObject addNode(ImdiTreeObject parentNode, String nodeType, String nodeTypeDisplayName, String targetXmlPath, URI resourceUri, String mimeType) throws ArbilMetadataException {
         System.out.println("wizard add node: " + nodeType);
         System.out.println("adding into: " + parentNode);
         ImdiTreeObject addedImdiObject;
@@ -78,7 +117,7 @@ public class LinorgHyperlinkListener implements HyperlinkListener {
             TreeHelper.getSingleInstance().applyRootLocations();
         } else {
             parentNode.saveChangesToCache(true);
-            addedImdiObject = ImdiLoader.getSingleInstance().getImdiObject(null, new MetadataBuilder().addChildNode(parentNode,nodeType, targetXmlPath, resourceUri, mimeType));
+            addedImdiObject = ImdiLoader.getSingleInstance().getImdiObject(null, new MetadataBuilder().addChildNode(parentNode, nodeType, targetXmlPath, resourceUri, mimeType));
         }
         addedImdiObject.waitTillLoaded();
         ImdiTableModel imdiTableModel = LinorgWindowManager.getSingleInstance().openFloatingTableOnce(new ImdiTreeObject[]{addedImdiObject}, nodeTypeDisplayName);
