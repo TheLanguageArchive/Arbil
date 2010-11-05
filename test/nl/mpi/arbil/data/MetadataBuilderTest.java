fb4707623b1365cfb7209dad021e67a6ebd5646b
Peter Withers <Peter.Withers@mpi.nl>
2010-11-05 17:25:18 +0000

    Added a test for the metadata building and node insertion ordering.

--- test/nl/mpi/arbil/data/MetadataBuilderTest.java
+++ test/nl/mpi/arbil/data/MetadataBuilderTest.java
@@ -1,11 +1,14 @@
 package nl.mpi.arbil.data;
 
+import java.util.Hashtable;
+import nl.mpi.arbil.ImdiField;
 import nl.mpi.arbil.XsdChecker;
 import java.util.ArrayList;
 import java.util.Enumeration;
 import nl.mpi.arbil.LinorgSessionStorage;
 import nl.mpi.arbil.clarin.CmdiComponentBuilder;
 import java.net.URI;
+import nl.mpi.arbil.LinorgWindowManager;
 import nl.mpi.arbil.clarin.ArbilMetadataException;
 import org.junit.After;
 import org.junit.AfterClass;
@@ -51,6 +54,9 @@ public class MetadataBuilderTest {
     public void testAddChildNode() {
         MetadataBuilder metadataBuilder = new MetadataBuilder();
         CmdiComponentBuilder componentBuilder = new CmdiComponentBuilder();
+
+        String[][] autoFields = new String[][]{{"Language.ISO639.iso-639-3-code", "zts"}, {"Language.ISO639.iso-639-3-code", "ymt"}, {"AnnotationType", "Phonetics"}};
+
         for (String currentTestTemplate : new String[]{"http://catalog.clarin.eu/ds/ComponentRegistry/rest/registry/profiles/clarin.eu:cr1:p_1271859438162/xsd", ".METATRANSCRIPT.Session : this is currently handled by metadatareader and should be consolidated"}) {
             URI targetFileURI = LinorgSessionStorage.getSingleInstance().getNewImdiFileName(LinorgSessionStorage.getSingleInstance().getCacheDirectory(), currentTestTemplate);
             //            try {
@@ -75,15 +81,38 @@ public class MetadataBuilderTest {
                     //currentLevel.remove(currentLevelNode);
                     System.out.println(currentLevelNode.getUrlString());
                     for (Enumeration menuItemName = currentLevelNode.getParentDomNode().nodeTemplate.listTypesFor(currentLevelNode); menuItemName.hasMoreElements();) {
-                        String[] currentField = (String[]) menuItemName.nextElement();
-                        System.out.println(currentField[0] + " : " + currentField[1]);// + " : "+ currentField[2]);
-                        allTemplates.remove(currentField[1]);
+                        String[] currentComponent = (String[]) menuItemName.nextElement();
+                        System.out.println(currentComponent[0] + " : " + currentComponent[1]);// + " : "+ currentField[2]);
+                        allTemplates.remove(currentComponent[1]);
                         try {
-                            URI linkUri = metadataBuilder.addChildNode(currentLevelNode, currentField[1], null, null, null);
+                            URI linkUri = metadataBuilder.addChildNode(currentLevelNode, currentComponent[1], null, null, null);
+                            ImdiTreeObject addedImdiObject = ImdiLoader.getSingleInstance().getImdiObject(null, linkUri);
+                            gedcomImdiObject.loadImdiDom();
+                            gedcomImdiObject.waitTillLoaded();
+
+                            if (addedImdiObject != null) {
+                                Hashtable<String, ImdiField[]> currentFields = addedImdiObject.getFields();
+                                if (currentFields != null) {
+                                    // populate any fields in the list provided
+                                    for (String[] currentFieldData : autoFields) {
+//                                for (ImdiField[] currentField : addedImdiObject.getFields().values()) {
+//                                    if (currentField != null && currentField.length > 0) {
+//                                        System.out.println(currentField[0].getTranslateFieldName());
+//                                        currentField[0].setFieldValue(currentFieldData[1], false, true);
+//                                    }
+//                                }
+                                        ImdiField[] currentField = addedImdiObject.getFields().get(currentFieldData[0]);
+                                        if (currentField != null && currentField.length > 0) {
+                                            currentField[0].setFieldValue(currentFieldData[1], false, true);
+                                        }
+                                    }
+                                }
+                            }
+                            gedcomImdiObject.saveChangesToCache(false);
+                            gedcomImdiObject.waitTillLoaded();
                         } catch (ArbilMetadataException exception) {
                             fail(exception.getMessage());
                         }
-                        checkAgainstSchema(gedcomImdiObject);
                         //        System.out.println("getNodeTypeFromMimeType");
                         //        String[][] testCases = {
                         //            {"application/pdf", ".METATRANSCRIPT.Session.Resources.WrittenResource"},
@@ -99,6 +128,7 @@ public class MetadataBuilderTest {
                 gedcomImdiObject.waitTillLoaded();
                 gedcomImdiObject.reloadNode();
                 gedcomImdiObject.waitTillLoaded();
+                checkAgainstSchema(gedcomImdiObject);
                 ArrayList<ImdiTreeObject> nextLevel = new ArrayList<ImdiTreeObject>();
                 for (ImdiTreeObject currentLevelNode : currentLevel) {
                     for (ImdiTreeObject nextLevelNode : currentLevelNode.getChildArray()) {
