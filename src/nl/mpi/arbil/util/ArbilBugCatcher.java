7e0d84d2e3be154fd2cae800866f7e692fab21aa
Twan Goosen <Twan.Goosen@mpi.nl>
2011-05-20 09:58:56 +0000

    bug catcher: logError can deal with null exception. This should fix a medium-priority findbugs hit

--- src/nl/mpi/arbil/util/ArbilBugCatcher.java
+++ src/nl/mpi/arbil/util/ArbilBugCatcher.java
@@ -25,54 +25,53 @@ public class ArbilBugCatcher implements BugCatcher {
 //            log.warn("warn message.");
 //            log.error("error message.");
 //            log.fatal("fatal message.");
-
     private static WindowManager windowManager;
-    public static void setWindowManager(WindowManager windowManagerInstance){
-        windowManager = windowManagerInstance;
+
+    public static void setWindowManager(WindowManager windowManagerInstance) {
+	windowManager = windowManagerInstance;
     }
 
     public ArbilBugCatcher() {
-        // remove all previous error logs for this version other than the one for this build number
-        File errorLogFile = new File(ArbilSessionStorage.getSingleInstance().storageDirectory, "linorgerror.log");
-        if (errorLogFile.exists()) {
-            errorLogFile.delete();
-        }
-        // look for previous error logs for this version only
-        ArbilVersion linorgVersion = new ArbilVersion();
-        String currentApplicationVersionMatch = "error-" + linorgVersion.currentMajor + "-" + linorgVersion.currentMinor + "-";
-        String currentLogFileMatch = "error-" + linorgVersion.currentMajor + "-" + linorgVersion.currentMinor + "-" + linorgVersion.currentRevision + ".log";
-        for (String currentFile : ArbilSessionStorage.getSingleInstance().storageDirectory.list()) {
-            if (currentFile.startsWith(currentApplicationVersionMatch)) {
-                if (currentFile.startsWith(currentLogFileMatch)) {
-                    // keeping this builds log file
-                    System.out.println("currentLogFileMatch: " + currentFile);
-                } else {
-                    System.out.println("deleting old log file: " + currentFile);
-                    new File(ArbilSessionStorage.getSingleInstance().storageDirectory, currentFile).delete();
-                }
-            }
-        }
+	// remove all previous error logs for this version other than the one for this build number
+	File errorLogFile = new File(ArbilSessionStorage.getSingleInstance().storageDirectory, "linorgerror.log");
+	if (errorLogFile.exists()) {
+	    errorLogFile.delete();
+	}
+	// look for previous error logs for this version only
+	ArbilVersion linorgVersion = new ArbilVersion();
+	String currentApplicationVersionMatch = "error-" + linorgVersion.currentMajor + "-" + linorgVersion.currentMinor + "-";
+	String currentLogFileMatch = "error-" + linorgVersion.currentMajor + "-" + linorgVersion.currentMinor + "-" + linorgVersion.currentRevision + ".log";
+	for (String currentFile : ArbilSessionStorage.getSingleInstance().storageDirectory.list()) {
+	    if (currentFile.startsWith(currentApplicationVersionMatch)) {
+		if (currentFile.startsWith(currentLogFileMatch)) {
+		    // keeping this builds log file
+		    System.out.println("currentLogFileMatch: " + currentFile);
+		} else {
+		    System.out.println("deleting old log file: " + currentFile);
+		    new File(ArbilSessionStorage.getSingleInstance().storageDirectory, currentFile).delete();
+		}
+	    }
+	}
     }
-
     private int captureCount = 0;
 
     public File getLogFile() {
-        ArbilVersion linorgVersion = new ArbilVersion();
-        return new File(ArbilSessionStorage.getSingleInstance().storageDirectory, "error-" + linorgVersion.currentMajor + "-" + linorgVersion.currentMinor + "-" + linorgVersion.currentRevision + ".log");
+	ArbilVersion linorgVersion = new ArbilVersion();
+	return new File(ArbilSessionStorage.getSingleInstance().storageDirectory, "error-" + linorgVersion.currentMajor + "-" + linorgVersion.currentMinor + "-" + linorgVersion.currentRevision + ".log");
     }
 
     public void grabApplicationShot() {
-        try {
-            Robot robot = new Robot();
-            BufferedImage screenShot = robot.createScreenCapture(windowManager.getMainFrame().getBounds());
-            DecimalFormat myFormat = new DecimalFormat("000");
-            SimpleDateFormat formatter = new SimpleDateFormat("yyyyMMddHHmmss");
-            String formattedDate = formatter.format(new Date());
-            String formattedCount = myFormat.format(new Integer(captureCount));
-            ImageIO.write(screenShot, "JPG", new File(ArbilSessionStorage.getSingleInstance().storageDirectory, "screenshots" + File.separatorChar + formattedDate + "-" + formattedCount + ".jpg"));
-            captureCount++;
-        } catch (Exception e) {
-        }
+	try {
+	    Robot robot = new Robot();
+	    BufferedImage screenShot = robot.createScreenCapture(windowManager.getMainFrame().getBounds());
+	    DecimalFormat myFormat = new DecimalFormat("000");
+	    SimpleDateFormat formatter = new SimpleDateFormat("yyyyMMddHHmmss");
+	    String formattedDate = formatter.format(new Date());
+	    String formattedCount = myFormat.format(new Integer(captureCount));
+	    ImageIO.write(screenShot, "JPG", new File(ArbilSessionStorage.getSingleInstance().storageDirectory, "screenshots" + File.separatorChar + formattedDate + "-" + formattedCount + ".jpg"));
+	    captureCount++;
+	} catch (Exception e) {
+	}
     }
 
 //    public void logMessage(String messageString) {
@@ -89,32 +88,35 @@ public class ArbilBugCatcher implements BugCatcher {
 //            System.err.println("failed to write to the error log: " + ex.getMessage());
 //        }
 //    }
-    
     public void logError(Exception exception) {
-        logError("", exception);
+	logError("", exception);
     }
 
     public void logError(String messageString, Exception exception) {
-        try {
-            ArbilVersion linorgVersion = new ArbilVersion();
-            System.err.println("exception: " + exception.getMessage());
-            System.err.println(messageString);
-            exception.printStackTrace();
-            FileWriter errorLogFile = new FileWriter(getLogFile(), true);
+	try {
+	    ArbilVersion linorgVersion = new ArbilVersion();
+	    System.err.println(messageString);
+	    if (exception != null) {
+		System.err.println("exception: " + exception.getMessage());
+		exception.printStackTrace(System.err);
+	    }
+	    FileWriter errorLogFile = new FileWriter(getLogFile(), true);
 //            System.out.println("logCatch: " + messageString);
-            errorLogFile.append(messageString + System.getProperty("line.separator"));
-            errorLogFile.append("Error Date: " + new Date().toString() + System.getProperty("line.separator"));
-            errorLogFile.append("Compile Date: " + linorgVersion.compileDate + System.getProperty("line.separator"));
-            errorLogFile.append("Current Revision: " + linorgVersion.currentMajor + "-" + linorgVersion.currentMinor + "-" + linorgVersion.currentRevision + System.getProperty("line.separator"));
-            errorLogFile.append("Exception Message: " + exception.getMessage() + System.getProperty("line.separator"));
-            StackTraceElement[] stackTraceElements = exception.getStackTrace();
-            for (StackTraceElement element : stackTraceElements) {
-                errorLogFile.append(element.toString() + System.getProperty("line.separator"));
-            }
-            errorLogFile.append("======================================================================" + System.getProperty("line.separator"));
-            errorLogFile.close();
-        } catch (Exception ex) {
-            System.err.println("failed to write to the error log: " + ex.getMessage());
-        }
+	    errorLogFile.append(messageString + System.getProperty("line.separator"));
+	    errorLogFile.append("Error Date: " + new Date().toString() + System.getProperty("line.separator"));
+	    errorLogFile.append("Compile Date: " + linorgVersion.compileDate + System.getProperty("line.separator"));
+	    errorLogFile.append("Current Revision: " + linorgVersion.currentMajor + "-" + linorgVersion.currentMinor + "-" + linorgVersion.currentRevision + System.getProperty("line.separator"));
+	    if (exception != null) {
+		errorLogFile.append("Exception Message: " + exception.getMessage() + System.getProperty("line.separator"));
+		StackTraceElement[] stackTraceElements = exception.getStackTrace();
+		for (StackTraceElement element : stackTraceElements) {
+		    errorLogFile.append(element.toString() + System.getProperty("line.separator"));
+		}
+	    }
+	    errorLogFile.append("======================================================================" + System.getProperty("line.separator"));
+	    errorLogFile.close();
+	} catch (Exception ex) {
+	    System.err.println("failed to write to the error log: " + ex.getMessage());
+	}
     }
 }
