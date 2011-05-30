9bbaec578a8768c450fa0265e90b573d0b329810
Twan Goosen <Twan.Goosen@mpi.nl>
2011-05-30 13:29:40 +0000

    Moved resources to src/main/resources

--- arbil/src/main/java/nl/mpi/arbil/ArbilVersion.java
+++ arbil/src/main/java/nl/mpi/arbil/ArbilVersion.java
@@ -4,10 +4,10 @@ public class ArbilVersion {
     public String applicationIconName = "arbil-pretesting128x128.png";
     public String currentMajor = "2";
     public String currentMinor = "2";
-    public String currentRevision = "24367";
-    public String lastCommitDate = "Last Changed Date: 2011-05-26 16:29:49 +0200 (Thu, 26 May 2011)";
-    public String compileDate = "Mon May 30 11:59:25 CEST 2011";
-    public String fullInfo = "\nPath: src/main/java/nl/mpi/arbil\nURL: svn+ssh://svn.mpi.nl/srv/svn/LAT/Arbil/trunk/arbil/src/main/java/nl/mpi/arbil\nRepository Root: svn+ssh://svn.mpi.nl/srv/svn/LAT\nRepository UUID: d4234c5b-7c33-0410-a85d-91bdcf7faf44\nRevision: 24352\nNode Kind: directory\nSchedule: normal\nLast Changed Author: twagoo\nLast Changed Rev: 24340\nLast Changed Date: 2011-05-26 16:29:49 +0200 (Thu, 26 May 2011)\n";
+    public String currentRevision = "24390";
+    public String lastCommitDate = "Last Changed Date: 2011-05-30 12:04:56 +0200 (Mon, 30 May 2011)";
+    public String compileDate = "Mon May 30 15:28:12 CEST 2011";
+    public String fullInfo = "\nPath: src/main/java/nl/mpi/arbil\nURL: svn+ssh://svn.mpi.nl/srv/svn/LAT/Arbil/trunk/arbil/src/main/java/nl/mpi/arbil\nRepository Root: svn+ssh://svn.mpi.nl/srv/svn/LAT\nRepository UUID: d4234c5b-7c33-0410-a85d-91bdcf7faf44\nRevision: 24392\nNode Kind: directory\nSchedule: normal\nLast Changed Author: twagoo\nLast Changed Rev: 24390\nLast Changed Date: 2011-05-30 12:04:56 +0200 (Mon, 30 May 2011)\n";
     public String currentVersionFile = "http://www.mpi.nl/tg/j2se/jnlp/arbil/arbil-pretesting-current.txt";
 }
         
