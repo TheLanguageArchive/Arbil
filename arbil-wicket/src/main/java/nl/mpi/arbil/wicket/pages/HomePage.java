1711947b440772efbfd38b8c7ea84ca86adcd2be
Twan Goosen <Twan.Goosen@mpi.nl>
2011-05-30 10:04:56 +0000

    Restructured directory, preparing for mavenizing

--- arbil-wicket/src/main/java/nl/mpi/arbil/wicket/pages/HomePage.java
+++ arbil-wicket/src/main/java/nl/mpi/arbil/wicket/pages/HomePage.java
@@ -1,8 +1,10 @@
 package nl.mpi.arbil.wicket.pages;
 
+import org.apache.wicket.Component;
 import org.apache.wicket.PageParameters;
-import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.extensions.markup.html.repeater.data.table.DataTable;
 import org.apache.wicket.markup.html.WebPage;
+import org.apache.wicket.markup.repeater.data.ListDataProvider;
 
 /**
  * Homepage
@@ -20,10 +22,11 @@ public class HomePage extends WebPage {
 	 *            Page parameters
 	 */
     public HomePage(final PageParameters parameters) {
-
-        // Add the simplest type of label
-        add(new Label("message", "If you see this message wicket is properly configured and running"));
-
-        // TODO Add your page's components here
+	add(createTable("datatable"));
+    }
+    
+    private Component createTable(String name){
+	DataTable<Object> table = new DataTable(name, null, new ListDataProvider(null)  , 100);
+	return table;
     }
 }
