ccfb4ba09b6510678eed127f88e78ee5028f7ee0
Twan Goosen <Twan.Goosen@mpi.nl>
2011-06-03 14:15:35 +0000

    Made abstract class DetachableArbilDataNodeCollector (could use better name), which handles attaching and detaching of ArbilDataNodes on basis of URI's. Extended by (now) DataNodeProvider and (not yet) ArbilTreeModel

--- arbil-wicket/src/main/java/nl/mpi/arbil/wicket/model/DataNodeDataProvider.java
+++ arbil-wicket/src/main/java/nl/mpi/arbil/wicket/model/DataNodeDataProvider.java
@@ -1,89 +1,35 @@
 package nl.mpi.arbil.wicket.model;
 
-import com.google.common.base.Function;
-import com.google.common.collect.ImmutableList;
-import com.google.common.collect.Lists;
 import java.net.URI;
-import java.util.Arrays;
 import java.util.Iterator;
 import java.util.List;
 import nl.mpi.arbil.data.ArbilDataNode;
-import nl.mpi.arbil.data.ArbilDataNodeLoader;
 import org.apache.wicket.markup.repeater.data.IDataProvider;
 import org.apache.wicket.model.IModel;
 
 /**
- * Provider for ArbilDataNodes. Gets nodes on basis of URIs
+ * Provider for ArbilDataNodes
  * @author Twan Goosen <twan.goosen@mpi.nl>
  */
-public class DataNodeDataProvider implements IDataProvider<ArbilDataNode> {
-
-    /**
-     * URIs that represent data nodes
-     */
-    private List<URI> URIs;
-    /**
-     * Transient storage of actual data nodes
-     */
-    private transient List<ArbilDataNode> dataNodes;
+public class DataNodeDataProvider extends DetachableArbilDataNodeCollector implements IDataProvider<ArbilDataNode> {
 
     public DataNodeDataProvider(List<URI> uris) {
-	this.URIs = uris;
+	super(uris);
     }
 
     public DataNodeDataProvider(ArbilDataNode[] dataNodes) {
-	this.dataNodes = Arrays.asList(dataNodes);
-	URIs = URIsFromNodes(this.dataNodes);
+	super(dataNodes);
     }
 
     public Iterator<? extends ArbilDataNode> iterator(int first, int count) {
-	ensureNodesLoaded();
-	return dataNodes.subList(first, count).listIterator();
+	return getDataNodes().subList(first, count).listIterator();
     }
 
     public int size() {
-	return URIs.size();
+	return getURIs().size();
     }
 
     public IModel<ArbilDataNode> model(final ArbilDataNode object) {
 	return new DataNodeModel(object);
-    }
-
-    public void detach() {
-	// Data nodes are not serializable, leave no reference
-	dataNodes = null;
-    }
-
-    public List<ArbilDataNode> getDataNodes() {
-	ensureNodesLoaded();
-	return dataNodes;
-    }
-
-    private void ensureNodesLoaded() {
-	synchronized (this) {
-	    if (dataNodes == null) {
-		dataNodes = nodesFromURIs(URIs);
-	    }
-	}
-    }
-
-    public static List<URI> URIsFromNodes(List<ArbilDataNode> nodes) {
-	// Copy to immutable because transform returns lazy loaded list, we don't want this
-	return ImmutableList.copyOf(Lists.transform(nodes, new Function<ArbilDataNode, URI>() {
-
-	    public URI apply(ArbilDataNode f) {
-		return f.getURI();
-	    }
-	}));
-    }
-
-    public static List<ArbilDataNode> nodesFromURIs(List<URI> nodeURIs) {
-	// Copy to immutable because transform returns lazy loaded list, we don't want this
-	return ImmutableList.copyOf(Lists.transform(nodeURIs, new Function<URI, ArbilDataNode>() {
-
-	    public ArbilDataNode apply(URI f) {
-		return ArbilDataNodeLoader.getSingleInstance().getArbilDataNode(null, f);
-	    }
-	}));
     }
 }
---
+++ arbil-wicket/src/main/java/nl/mpi/arbil/wicket/model/DetachableArbilDataNodeCollector.java
@@ -0,0 +1,80 @@
+package nl.mpi.arbil.wicket.model;
+
+import com.google.common.base.Function;
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.Lists;
+import java.net.URI;
+import java.util.Arrays;
+import java.util.List;
+import nl.mpi.arbil.data.ArbilDataNode;
+import nl.mpi.arbil.data.ArbilDataNodeLoader;
+
+/**
+ * Base class that keeps a list of URI's and attaches/detaches ArbilDataNodes
+ * on basis of these. Use to prevent wicket from trying to serialize (collections
+ * of ArbilDataNodes)
+ * @see ArbilDataNode
+ * @author Twan Goosen <twan.goosen@mpi.nl>
+ */
+public abstract class DetachableArbilDataNodeCollector {
+
+    /**
+     * URIs that represent data nodes
+     */
+    private List<URI> URIs;
+    /**
+     * Transient storage of actual data nodes
+     */
+    private transient List<ArbilDataNode> dataNodes;
+
+    public DetachableArbilDataNodeCollector(List<URI> uris) {
+	this.URIs = uris;
+    }
+
+    public DetachableArbilDataNodeCollector(ArbilDataNode[] dataNodes) {
+	this.dataNodes = Arrays.asList(dataNodes);
+	URIs = URIsFromNodes(this.dataNodes);
+    }
+
+    public void detach() {
+	// Data nodes are not serializable, leave no reference
+	dataNodes = null;
+    }
+
+    public static List<URI> URIsFromNodes(List<ArbilDataNode> nodes) {
+	// Copy to immutable because transform returns lazy loaded list, we don't want this
+	return ImmutableList.copyOf(Lists.transform(nodes, new Function<ArbilDataNode, URI>() {
+
+	    public URI apply(ArbilDataNode f) {
+		return f.getURI();
+	    }
+	}));
+    }
+
+    public static List<ArbilDataNode> nodesFromURIs(List<URI> nodeURIs) {
+	// Copy to immutable because transform returns lazy loaded list, we don't want this
+	return ImmutableList.copyOf(Lists.transform(nodeURIs, new Function<URI, ArbilDataNode>() {
+
+	    public ArbilDataNode apply(URI f) {
+		return ArbilDataNodeLoader.getSingleInstance().getArbilDataNode(null, f);
+	    }
+	}));
+    }
+    
+    /**
+     * @return the URIs
+     */
+    protected List<URI> getURIs() {
+	return URIs;
+    }
+
+    /**
+     * @return the dataNodes (will be attached if not already)
+     */
+    public synchronized List<ArbilDataNode> getDataNodes() {
+	if (dataNodes == null) {
+	    dataNodes = nodesFromURIs(URIs);
+	}
+	return dataNodes;
+    }
+}
