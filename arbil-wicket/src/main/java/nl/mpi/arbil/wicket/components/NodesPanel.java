b80db6c3af6afeb1126cf68ae4d254aa65e3cc86
Twan Goosen <Twan.Goosen@mpi.nl>
2011-06-03 09:57:28 +0000

    Using wicket push to update nodestable when update requested by node

--- arbil-wicket/src/main/java/nl/mpi/arbil/wicket/components/NodesPanel.java
+++ arbil-wicket/src/main/java/nl/mpi/arbil/wicket/components/NodesPanel.java
@@ -1,16 +1,67 @@
 package nl.mpi.arbil.wicket.components;
 
 import nl.mpi.arbil.data.ArbilDataNode;
+import nl.mpi.arbil.data.ArbilDataNodeContainer;
+import org.apache.wicket.ajax.AjaxRequestTarget;
 import org.apache.wicket.markup.html.panel.Panel;
+import org.wicketstuff.push.AbstractPushEventHandler;
+import org.wicketstuff.push.IPushEventContext;
+import org.wicketstuff.push.IPushEventHandler;
+import org.wicketstuff.push.IPushNode;
+import org.wicketstuff.push.IPushService;
+import org.wicketstuff.push.timer.TimerPushService;
 
 /**
  *
  * @author Twan Goosen <twan.goosen@mpi.nl>
  */
-public class NodesPanel extends Panel {
+public class NodesPanel extends Panel implements ArbilDataNodeContainer {
+
+    private transient IPushEventHandler handler;
+    private transient IPushService pushService;
+    private transient IPushNode<ArbilDataNode> pushNode;
+    private ArbilTable table;
 
     public NodesPanel(String id, ArbilDataNode[] dataNodes) {
 	super(id);
-	add(new ArbilTable("datatable", dataNodes));
+	table = new ArbilTable("datatable", dataNodes);
+	add(table);
+	table.setOutputMarkupId(true);
+
+	/**
+	 * Push handler
+	 */
+	handler = new AbstractPushEventHandler<ArbilDataNode>() {
+
+	    /**
+	     * DataNode event (could be made more specific)
+	     */
+	    public void onEvent(AjaxRequestTarget target, ArbilDataNode event, IPushNode node, IPushEventContext ctx) {
+		target.addComponent(table);
+	    }
+	};
+
+	pushService = TimerPushService.get();
+	pushNode = pushService.installNode(this, handler);
+	
+	for(ArbilDataNode node:dataNodes){
+	    node.registerContainer(this);
+	}
+    }
+
+    public void dataNodeRemoved(ArbilDataNode dataNode) {
+	if(pushService.isConnected(pushNode)){
+	    pushService.publish(pushNode, dataNode);
+	} else{
+	    dataNode.removeContainer(this);
+	}
+    }
+
+    public void dataNodeIconCleared(ArbilDataNode dataNode) {
+	if(pushService.isConnected(pushNode)){
+	    pushService.publish(pushNode, dataNode);
+	} else{
+	    dataNode.removeContainer(this);
+	}
     }
 }
