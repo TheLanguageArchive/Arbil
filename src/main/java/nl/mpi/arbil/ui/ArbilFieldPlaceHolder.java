4e98220c533f5119f1b5d3cc2f47f225b609a85f
Twan Goosen <Twan.Goosen@mpi.nl>
2011-06-09 14:31:55 +0000

    Made some data classes serializable for use in wicket components

--- src/main/java/nl/mpi/arbil/ui/ArbilFieldPlaceHolder.java
+++ src/main/java/nl/mpi/arbil/ui/ArbilFieldPlaceHolder.java
@@ -1,6 +1,9 @@
 package nl.mpi.arbil.ui;
 
+import java.io.Serializable;
+import java.net.URI;
 import nl.mpi.arbil.data.ArbilDataNode;
+import nl.mpi.arbil.data.ArbilDataNodeLoader;
 
 /**
  * Used as blank in horizontal tables (grid view) in cells that represent
@@ -8,26 +11,33 @@ import nl.mpi.arbil.data.ArbilDataNode;
  * 'grey fields'.
  * @author Twan Goosen <twan.goosen@mpi.nl>
  */
-public class ArbilFieldPlaceHolder {
+public class ArbilFieldPlaceHolder implements Serializable {
 
     private String fieldName;
-    private ArbilDataNode arbilDataNode;
+    private transient ArbilDataNode arbilDataNode;
+    private URI arbilDataNodeURI = null;
 
     public ArbilFieldPlaceHolder(String fieldName, ArbilDataNode dataNode) {
-        this.fieldName = fieldName;
-        this.arbilDataNode = dataNode;
+	this.fieldName = fieldName;
+	this.arbilDataNode = dataNode;
+	if (dataNode != null) {
+	    this.arbilDataNodeURI = arbilDataNode.getURI();
+	}
     }
 
     public String getFieldName() {
-        return fieldName;
+	return fieldName;
     }
 
     public ArbilDataNode getArbilDataNode() {
-        return arbilDataNode;
+	if (arbilDataNode == null && arbilDataNodeURI != null) {
+	    arbilDataNode = ArbilDataNodeLoader.getSingleInstance().getArbilDataNode(null, arbilDataNodeURI);
+	}
+	return arbilDataNode;
     }
 
     @Override
     public String toString() {
-        return "";
+	return "";
     }
 }
