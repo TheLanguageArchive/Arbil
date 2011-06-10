5afa4d8e48f8cef45dc4422fd476d12659685ca6
Twan Goosen <Twan.Goosen@mpi.nl>
2011-06-10 07:45:13 +0000

    ArbilWicketTableModel now model of List of ArbilTableCells (=rows), coming from the Arbil(Wicket)TableModel

--- arbil-wicket/src/main/java/nl/mpi/arbil/wicket/components/ArbilWicketDataTable.java
+++ arbil-wicket/src/main/java/nl/mpi/arbil/wicket/components/ArbilWicketDataTable.java
@@ -1,13 +1,18 @@
 package nl.mpi.arbil.wicket.components;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
 import nl.mpi.arbil.data.ArbilDataNode;
+import nl.mpi.arbil.data.ArbilField;
+import nl.mpi.arbil.data.ArbilTableCell;
+import nl.mpi.arbil.wicket.model.ArbilWicketTableModel;
 import org.apache.wicket.extensions.markup.html.repeater.data.grid.ICellPopulator;
 import org.apache.wicket.extensions.markup.html.repeater.data.table.AbstractColumn;
-import org.apache.wicket.extensions.markup.html.repeater.data.table.DataTable;
+import org.apache.wicket.extensions.markup.html.repeater.data.table.DefaultDataTable;
 import org.apache.wicket.extensions.markup.html.repeater.data.table.IColumn;
 import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.repeater.Item;
-import org.apache.wicket.markup.repeater.data.IDataProvider;
 import org.apache.wicket.model.IModel;
 import org.apache.wicket.model.Model;
 
@@ -15,27 +20,73 @@ import org.apache.wicket.model.Model;
  *
  * @author Twan Goosen <twan.goosen@mpi.nl>
  */
-public class ArbilWicketDataTable extends DataTable<ArbilDataNode> {
+public class ArbilWicketDataTable extends DefaultDataTable<ArrayList<ArbilTableCell>> {
 
-    public ArbilWicketDataTable(String id, IDataProvider<ArbilDataNode> dataNodes) {
-	super(id, createColumns(), dataNodes, 100);
+    public ArbilWicketDataTable(String id, ArbilWicketTableModel model) {
+	super(id, createColumns(model), model, model.getRowCount());
     }
 
-    private static IColumn<ArbilDataNode>[] createColumns() {
-	IColumn<ArbilDataNode> iconColumn = new AbstractColumn<ArbilDataNode>(new Model<String>("Icon")) {
+    private static IColumn<ArrayList<ArbilTableCell>>[] createColumns(ArbilWicketTableModel model) {
+	IColumn<?>[] columns = new IColumn<?>[model.getColumnCount()];
 
-	    public void populateItem(Item<ICellPopulator<ArbilDataNode>> cellItem, String componentId, IModel<ArbilDataNode> rowModel) {
-		cellItem.add(new NodeIcon(componentId, rowModel.getObject().getIcon().getImage()));
+	// Actual data columns
+	for (int i = 0; i < model.getColumnCount(); i++) {
+	    final int column = i;
+	    columns[i] = new AbstractColumn<ArrayList<ArbilTableCell>>(new Model<String>(model.getColumnName(column))) {
+
+		public void populateItem(Item<ICellPopulator<ArrayList<ArbilTableCell>>> cellItem, String componentId, IModel<ArrayList<ArbilTableCell>> rowModel) {
+		    ArbilTableCell cell = rowModel.getObject().get(column);
+		    cellItem.add(new Label(componentId, getText(cell)));
+		}
+	    };
+	}
+
+	return (IColumn<ArrayList<ArbilTableCell>>[]) columns;
+    }
+
+    // CODE FOR SHOWING ICON
+    //	IColumn<ArbilDataNode> iconColumn = new AbstractColumn<ArbilDataNode>(new Model<String>("Icon")) {
+    //
+    //	    public void populateItem(Item<ICellPopulator<ArbilDataNode>> cellItem, String componentId, IModel<ArbilDataNode> rowModel) {
+    //		cellItem.add(new NodeIcon(componentId, rowModel.getObject().getIcon().getImage()));
+    //	    }
+    //	};
+
+    private static String getText(ArbilTableCell cell) {
+	Object cellObject = cell.getContent();
+	if (cellObject instanceof ArbilDataNode) {
+	    return (((ArbilDataNode) cellObject).toString());
+	} else if (cellObject instanceof ArbilDataNode[]) {
+	    String cellText = "";
+	    Arrays.sort((ArbilDataNode[]) cellObject, new Comparator() {
+
+		public int compare(Object o1, Object o2) {
+		    String value1 = o1.toString();
+		    String value2 = o2.toString();
+		    return value1.compareToIgnoreCase(value2);
+		}
+	    });
+	    boolean hasAddedValues = false;
+	    for (ArbilDataNode currentArbilDataNode : (ArbilDataNode[]) cellObject) {
+		cellText = cellText + "[" + currentArbilDataNode.toString() + "],";
+		hasAddedValues = true;
 	    }
-	};
-
-	IColumn<ArbilDataNode> nodeNameColumn = new AbstractColumn<ArbilDataNode>(new Model<String>("Data node")) {
-
-	    public void populateItem(Item<ICellPopulator<ArbilDataNode>> cellItem, String componentId, IModel<ArbilDataNode> rowModel) {
-		cellItem.add(new Label(componentId, rowModel.getObject().toString()));
+	    if (hasAddedValues) {
+		cellText = cellText.substring(0, cellText.length() - 1);
 	    }
-	};
-
-	return (IColumn<ArbilDataNode>[]) new IColumn<?>[]{iconColumn, nodeNameColumn};
+	    return (cellText);
+	} else if (cellObject instanceof ArbilField[]) {
+	    return "<multiple values>";
+	} else if (cellObject instanceof ArbilField && ((ArbilField) cellObject).isRequiredField() && ((ArbilField) cellObject).toString().length() == 0) {
+	    //super.setForeground(Color.RED);
+	    return "<required field>";
+	} else if (cellObject instanceof ArbilField && !((ArbilField) cellObject).fieldValueValidates()) {
+	    //super.setForeground(Color.RED);
+	}
+	if (cellObject != null) {
+	    return cellObject.toString();
+	} else {
+	    return "";
+	}
     }
 }
--- arbil-wicket/src/main/java/nl/mpi/arbil/wicket/model/ArbilWicketTableModel.java
+++ arbil-wicket/src/main/java/nl/mpi/arbil/wicket/model/ArbilWicketTableModel.java
@@ -1,16 +1,22 @@
 package nl.mpi.arbil.wicket.model;
 
 import java.net.URI;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Hashtable;
 import java.util.Iterator;
+import java.util.List;
 import java.util.Map.Entry;
 import nl.mpi.arbil.data.ArbilDataNode;
 import nl.mpi.arbil.data.ArbilDataNodeLoader;
+import nl.mpi.arbil.data.ArbilTableCell;
 import nl.mpi.arbil.ui.AbstractArbilTableModel;
 import nl.mpi.arbil.ui.ArbilFieldView;
-import org.apache.wicket.markup.repeater.data.IDataProvider;
+import org.apache.wicket.extensions.markup.html.repeater.data.sort.ISortState;
+import org.apache.wicket.extensions.markup.html.repeater.data.table.ISortableDataProvider;
 import org.apache.wicket.model.IModel;
+import org.apache.wicket.model.Model;
 
 /**
  * Table model that can be used as DataProvider for Wicket DataTables, inherits
@@ -18,12 +24,12 @@ import org.apache.wicket.model.IModel;
  * will be serialized
  * @author Twan Goosen <twan.goosen@mpi.nl>
  */
-public class ArbilWicketTableModel extends AbstractArbilTableModel implements IDataProvider<ArbilDataNode> {
+public class ArbilWicketTableModel extends AbstractArbilTableModel implements ISortableDataProvider<ArrayList<ArbilTableCell>> {
 
-    private Object[][] data = new Object[0][0];
-    private transient Hashtable<String,ArbilDataNode> dataNodeHash;
+    private ArbilTableCell[][] data = new ArbilTableCell[0][0];
+    private transient Hashtable<String, ArbilDataNode> dataNodeHash;
     private HashMap<String, URI> dataNodeUrisMap = new HashMap<String, URI>();
-    
+
     public ArbilWicketTableModel(ArbilFieldView fieldView) {
 	super(fieldView);
     }
@@ -31,18 +37,37 @@ public class ArbilWicketTableModel extends AbstractArbilTableModel implements ID
     public ArbilWicketTableModel() {
 	super(new ArbilFieldView());
     }
-
+    
     // IDataProvider<ArbilDataNode> method implementations
-    public Iterator<? extends ArbilDataNode> iterator(int first, int count) {
-	return getDataNodeHash().values().iterator();
+    public Iterator<? extends ArrayList<ArbilTableCell>> iterator(int first, int count) {
+	// Create list of rows (over requested range)
+	List<ArbilTableCell[]> arrayRows = Arrays.asList(data).subList(first, count);
+	// Iterator from this will be used by resulting iterator
+	final Iterator<ArbilTableCell[]> arrayRowsIterator = arrayRows.iterator();
+	// Wrap into iterator that converts array based rows to row lists
+	return new Iterator<ArrayList<ArbilTableCell>>() {
+
+	    public ArrayList<ArbilTableCell> next() {
+		// Create ArrayList out of array
+		return new ArrayList<ArbilTableCell>(Arrays.asList(arrayRowsIterator.next()));
+	    }
+
+	    public boolean hasNext() {
+		return arrayRowsIterator.hasNext();
+	    }
+
+	    public void remove() {
+		arrayRowsIterator.remove();
+	    }
+	};
+    }
+
+    public IModel<ArrayList<ArbilTableCell>> model(ArrayList<ArbilTableCell> object) {
+	return new Model<ArrayList<ArbilTableCell>>(object);
     }
 
     public int size() {
-	return dataNodeUrisMap.size();
-    }
-
-    public IModel<ArbilDataNode> model(ArbilDataNode object) {
-	return new DataNodeModel(object);
+	return data.length;
     }
 
     public void detach() {
@@ -57,14 +82,14 @@ public class ArbilWicketTableModel extends AbstractArbilTableModel implements ID
     }
 
     private void loadDataNodeHash() {
-	if(dataNodeHash == null){
+	if (dataNodeHash == null) {
 	    dataNodeHash = new Hashtable<String, ArbilDataNode>();
-	    for(Entry<String,URI> entry : dataNodeUrisMap.entrySet()){
+	    for (Entry<String, URI> entry : dataNodeUrisMap.entrySet()) {
 		dataNodeHash.put(entry.getKey(), ArbilDataNodeLoader.getSingleInstance().getArbilDataNode(null, entry.getValue()));
 	    }
 	}
     }
-    
+
     @Override
     protected synchronized void putInDataNodeHash(ArbilDataNode arbilDataNode) {
 	super.putInDataNodeHash(arbilDataNode);
@@ -86,12 +111,12 @@ public class ArbilWicketTableModel extends AbstractArbilTableModel implements ID
     }
 
     @Override
-    protected Object[][] getData() {
+    protected ArbilTableCell[][] getData() {
 	return this.data;
     }
 
     @Override
-    protected void setData(Object[][] data) {
+    protected void setData(ArbilTableCell[][] data) {
 	this.data = data;
     }
 
@@ -121,4 +146,14 @@ public class ArbilWicketTableModel extends AbstractArbilTableModel implements ID
     public void dataNodeRemoved(ArbilDataNode dataNode) {
 	requestReloadTableData();
     }
+
+    public ISortState getSortState() {
+	return sortState;
+    }
+
+    public void setSortState(ISortState state) {
+	this.sortState = state;
+    }
+    
+    private ISortState sortState;
 }
