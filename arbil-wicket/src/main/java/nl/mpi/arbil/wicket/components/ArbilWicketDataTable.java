1d09537182f0ae3fff0bbbffe635a92e56ec2016
Twan Goosen <Twan.Goosen@mpi.nl>
2011-06-14 14:56:24 +0000

    Made ArbilWicketDataTable cell components AjaxEditableLabels, so that they can (in principle) be edited. No logic for actual editing.
    The cell component gets a CellValueStyleAppender, which is a behavior that assigns a CSS class depending on the nature and/or state of the cell object

--- arbil-wicket/src/main/java/nl/mpi/arbil/wicket/components/ArbilWicketDataTable.java
+++ arbil-wicket/src/main/java/nl/mpi/arbil/wicket/components/ArbilWicketDataTable.java
@@ -3,14 +3,18 @@ package nl.mpi.arbil.wicket.components;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Comparator;
+import java.util.Locale;
 import nl.mpi.arbil.data.ArbilDataNode;
 import nl.mpi.arbil.data.ArbilDataNodeTableCell;
 import nl.mpi.arbil.data.ArbilField;
 import nl.mpi.arbil.data.ArbilTableCell;
+import nl.mpi.arbil.data.DefaultArbilTableCell;
+import nl.mpi.arbil.ui.ArbilFieldPlaceHolder;
 import nl.mpi.arbil.wicket.model.ArbilDataNodeModel;
 import nl.mpi.arbil.wicket.model.ArbilWicketTableModel;
 import org.apache.wicket.Component;
 import org.apache.wicket.behavior.AttributeAppender;
+import org.apache.wicket.extensions.ajax.markup.html.AjaxEditableLabel;
 import org.apache.wicket.extensions.markup.html.repeater.data.grid.ICellPopulator;
 import org.apache.wicket.extensions.markup.html.repeater.data.table.AbstractColumn;
 import org.apache.wicket.extensions.markup.html.repeater.data.table.DefaultDataTable;
@@ -19,6 +23,7 @@ import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.repeater.Item;
 import org.apache.wicket.model.IModel;
 import org.apache.wicket.model.Model;
+import org.apache.wicket.util.convert.IConverter;
 
 /**
  *
@@ -36,6 +41,18 @@ public class ArbilWicketDataTable extends DefaultDataTable<ArrayList<ArbilTableC
 		return model.isHorizontalView() ? "horizontal" : "non-horizontal";
 	    }
 	}, " "));
+    }
+
+    private static boolean isEditable(Object content) {
+	if (content instanceof Object[] || content instanceof ArbilField) {
+	    return true;
+	} else if (content instanceof ArbilField[]) {
+	    ArbilDataNode parentObject = ((ArbilField[]) content)[0].getParentDataNode();
+	    // check that the field id exists and that the file is in the local cache or in the favourites not loose on a drive, as the determinator of editability
+	    return !parentObject.isLoading() && parentObject.isEditable() && parentObject.isMetaDataNode(); // todo: consider limiting editing to files withing the cache only
+	} else {
+	    return false;
+	}
     }
 
     private static IColumn<ArrayList<ArbilTableCell>>[] createColumns(ArbilWicketTableModel model) {
@@ -56,18 +73,43 @@ public class ArbilWicketDataTable extends DefaultDataTable<ArrayList<ArbilTableC
 	return (IColumn<ArrayList<ArbilTableCell>>[]) columns;
     }
 
-    // CODE FOR SHOWING ICON
-    //	IColumn<ArbilDataNode> iconColumn = new AbstractColumn<ArbilDataNode>(new Model<String>("Icon")) {
-    //
-    //	    public void populateItem(Item<ICellPopulator<ArbilDataNode>> cellItem, String componentId, IModel<ArbilDataNode> rowModel) {
-    //		cellItem.add(new NodeIcon(componentId, rowModel.getObject().getIcon().getImage()));
-    //	    }
-    //	};
-    private static Component createComponentForCell(String componentId, ArbilTableCell cell) {
+    private static Component createComponentForCell(String componentId, final ArbilTableCell cell) {
 	if (cell instanceof ArbilDataNodeTableCell) {
-	    return new NodePanel(componentId, new ArbilDataNodeModel(((ArbilDataNodeTableCell)cell).getContent()));
+	    return new NodePanel(componentId, new ArbilDataNodeModel(((ArbilDataNodeTableCell) cell).getContent()));
 	} else {
-	    return new Label(componentId, getText(cell));
+	    //return new Label(componentId, getText(cell));
+	    Object content = cell.getContent();
+
+	    if (isEditable(content)) {
+		AjaxEditableLabel component = new AjaxEditableLabel(componentId, new Model<ArbilTableCell>(cell)) {
+
+		    @Override
+		    public IConverter getConverter(final Class type) {
+			return new IConverter() {
+
+			    public Object convertToObject(String value, Locale locale) {
+				//throw new UnsupportedOperationException("Not supported yet.");
+				return new DefaultArbilTableCell(value);
+			    }
+
+			    public String convertToString(Object value, Locale locale) {
+				return getText((ArbilTableCell) value);
+			    }
+			};
+		    }
+		};
+		component.add(new CellValueStyleAppender(cell, true));
+		component.setType(ArbilTableCell.class);
+		component.setRequired(
+			true);
+
+
+		return component;
+	    } else {
+		Label component = new Label(componentId, getText(cell));
+		component.add(new CellValueStyleAppender(cell, false));
+		return component;
+	    }
 	}
     }
 
@@ -97,10 +139,7 @@ public class ArbilWicketDataTable extends DefaultDataTable<ArrayList<ArbilTableC
 	} else if (cellObject instanceof ArbilField[]) {
 	    return "<multiple values>";
 	} else if (cellObject instanceof ArbilField && ((ArbilField) cellObject).isRequiredField() && ((ArbilField) cellObject).toString().length() == 0) {
-	    //super.setForeground(Color.RED);
 	    return "<required field>";
-	} else if (cellObject instanceof ArbilField && !((ArbilField) cellObject).fieldValueValidates()) {
-	    //super.setForeground(Color.RED);
 	}
 	if (cellObject != null) {
 	    return cellObject.toString();
@@ -108,4 +147,36 @@ public class ArbilWicketDataTable extends DefaultDataTable<ArrayList<ArbilTableC
 	    return "";
 	}
     }
+
+    private static class CellValueStyleAppender extends AttributeAppender {
+
+	private CellValueStyleAppender(ArbilTableCell cell, boolean editable) {
+	    super("class", new Model<String>(getStyle(cell, editable)), " ");
+	}
+
+	private static String getStyle(ArbilTableCell cell, boolean editable) {
+	    StringBuilder style = new StringBuilder();
+
+	    if (editable) {
+		style.append("editable ");
+	    }
+
+	    Object cellObject = cell.getContent();
+	    if (cellObject instanceof ArbilField && ((ArbilField) cellObject).isRequiredField() && ((ArbilField) cellObject).toString().length() == 0) {
+		style.append("required ");
+	    } else if (cellObject instanceof ArbilField && !((ArbilField) cellObject).fieldValueValidates()) {
+		style.append("invalid ");
+	    }
+
+	    if (cellObject instanceof ArbilFieldPlaceHolder || cellObject instanceof String && "".equals(cellObject)) {
+		// Field does not exist in node OR childs column and node has no children of this type
+		style.append(" nosuchfield ");
+	    } else if (cellObject instanceof ArbilField && ((ArbilField) cellObject).fieldNeedsSaveToDisk()) {
+		// Value has changed since last save
+		style.append(" needssave ");
+	    }
+
+	    return style.toString().trim();
+	}
+    };
 }
