dee160c1f31a08fffd1b8145a1ce8ed89d3dab38
Twan Goosen <Twan.Goosen@mpi.nl>
2011-06-16 10:06:44 +0000

    Removing files accidentally added to version control

--- src/main/java/nl/mpi/arbil/ArbilVersion.java
+++ src/main/java/nl/mpi/arbil/ArbilVersion.java
@@ -3,11 +3,11 @@ public class ArbilVersion {
     public String applicationTitle = "Arbil (pretesting)";
     public String applicationIconName = "arbil-pretesting128x128.png";
     public String currentMajor = "2";
-    public String currentMinor = "2";
-    public String currentRevision = "24390";
-    public String lastCommitDate = "Last Changed Date: 2011-05-30 12:04:56 +0200 (Mon, 30 May 2011)";
-    public String compileDate = "Mon May 30 15:28:12 CEST 2011";
-    public String fullInfo = "\nPath: src/main/java/nl/mpi/arbil\nURL: svn+ssh://svn.mpi.nl/srv/svn/LAT/Arbil/trunk/arbil/src/main/java/nl/mpi/arbil\nRepository Root: svn+ssh://svn.mpi.nl/srv/svn/LAT\nRepository UUID: d4234c5b-7c33-0410-a85d-91bdcf7faf44\nRevision: 24392\nNode Kind: directory\nSchedule: normal\nLast Changed Author: twagoo\nLast Changed Rev: 24390\nLast Changed Date: 2011-05-30 12:04:56 +0200 (Mon, 30 May 2011)\n";
+    public String currentMinor = "3";
+    public String currentRevision = "24871";
+    public String lastCommitDate = "Last Changed Date: 2011-06-15 10:42:39 +0200 (Wed, 15 Jun 2011)";
+    public String compileDate = "Thu Jun 16 09:38:54 CEST 2011";
+    public String fullInfo = "\nPath: src/main/java/nl/mpi/arbil\nURL: svn+ssh://svn.mpi.nl/srv/svn/LAT/Arbil/trunk/arbil/src/main/java/nl/mpi/arbil\nRepository Root: svn+ssh://svn.mpi.nl/srv/svn/LAT\nRepository UUID: d4234c5b-7c33-0410-a85d-91bdcf7faf44\nRevision: 24881\nNode Kind: directory\nSchedule: normal\nLast Changed Author: twagoo\nLast Changed Rev: 24871\nLast Changed Date: 2011-06-15 10:42:39 +0200 (Wed, 15 Jun 2011)\n";
     public String currentVersionFile = "http://www.mpi.nl/tg/j2se/jnlp/arbil/arbil-pretesting-current.txt";
 }
         
