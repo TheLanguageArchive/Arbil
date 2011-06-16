0e4824c9537731f3a97add3c02947e4a4b96aba4
Twan Goosen <Twan.Goosen@mpi.nl>
2011-06-16 13:46:36 +0000

    ant-deb-jar added to svn:ignore

--- src/main/java/nl/mpi/arbil/ArbilVersion.java
+++ src/main/java/nl/mpi/arbil/ArbilVersion.java
@@ -4,9 +4,9 @@ public class ArbilVersion {
     public String applicationIconName = "arbil-pretesting128x128.png";
     public String currentMajor = "2";
     public String currentMinor = "3";
-    public String currentRevision = "24871";
+    public String currentRevision = "24891";
     public String lastCommitDate = "Last Changed Date: 2011-06-15 10:42:39 +0200 (Wed, 15 Jun 2011)";
-    public String compileDate = "Thu Jun 16 09:38:54 CEST 2011";
+    public String compileDate = "Thu Jun 16 15:35:28 CEST 2011";
     public String fullInfo = "\nPath: src/main/java/nl/mpi/arbil\nURL: svn+ssh://svn.mpi.nl/srv/svn/LAT/Arbil/trunk/arbil/src/main/java/nl/mpi/arbil\nRepository Root: svn+ssh://svn.mpi.nl/srv/svn/LAT\nRepository UUID: d4234c5b-7c33-0410-a85d-91bdcf7faf44\nRevision: 24881\nNode Kind: directory\nSchedule: normal\nLast Changed Author: twagoo\nLast Changed Rev: 24871\nLast Changed Date: 2011-06-15 10:42:39 +0200 (Wed, 15 Jun 2011)\n";
     public String currentVersionFile = "http://www.mpi.nl/tg/j2se/jnlp/arbil/arbil-pretesting-current.txt";
 }
