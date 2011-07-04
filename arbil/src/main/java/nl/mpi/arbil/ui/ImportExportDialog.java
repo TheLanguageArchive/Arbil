4b73de1aa62c567a31053baf50f53ecb6e7138fd
Twan Goosen <Twan.Goosen@mpi.nl>
2011-07-04 13:33:33 +0000

    Fixed missing catch for newly thrown IOException from bumpHistory. Upgraded xalan dependency in pom

--- arbil/src/main/java/nl/mpi/arbil/ui/ImportExportDialog.java
+++ arbil/src/main/java/nl/mpi/arbil/ui/ImportExportDialog.java
@@ -19,6 +19,7 @@ import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 import java.io.File;
+import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URI;
 import java.net.URL;
@@ -815,6 +816,11 @@ public class ImportExportDialog {
 		    metaDataCopyErrors.add(currentRetrievableFile.sourceURI);
 		    appendToTaskOutput("Unable to process the file: " + currentRetrievableFile.sourceURI);
 		    System.out.println("Error getting links from: " + currentRetrievableFile.sourceURI);
+		} catch(IOException ex){
+		    GuiHelper.linorgBugCatcher.logError(currentRetrievableFile.sourceURI.toString(), ex);
+		    totalErrors++;
+		    metaDataCopyErrors.add(currentRetrievableFile.sourceURI);
+		    appendToTaskOutput("Unable to process the file: " + currentRetrievableFile.sourceURI);
 		}
 		totalLoaded++;
 		progressFoundLabel.setText(progressFoundLabelText + (getList.size() + totalLoaded));
