9425830c8781ab3d84a0f998027f944101bab797
Twan Goosen <Twan.Goosen@mpi.nl>
2011-07-07 12:40:29 +0000

    Merged [24367] and [24864] which fix #782

--- src/nl/mpi/arbil/LinorgVersion.java
+++ src/nl/mpi/arbil/LinorgVersion.java
@@ -1,13 +1,13 @@
 package nl.mpi.arbil;
 public class LinorgVersion {
-    public String applicationTitle = "";
-    public String applicationIconName = "";
-    public String currentMajor = "";
-    public String currentMinor = "";
-    public String currentRevision = "";
-    public String lastCommitDate = "";
-    public String compileDate = "";
-    public String fullInfo = "";
-    public String currentVersionFile = "";
+    public String applicationTitle = "Arbil (testing)";
+    public String applicationIconName = "arbil-testing128x128.png";
+    public String currentMajor = "2";
+    public String currentMinor = "2";
+    public String currentRevision = "25151";
+    public String lastCommitDate = "Last Changed Date: 2011-07-07 11:24:00 +0200 (Thu, 07 Jul 2011)";
+    public String compileDate = "Thu Jul  7 11:24:30 CEST 2011";
+    public String fullInfo = "\nPath: src/nl/mpi/arbil\nURL: svn+ssh://svn.mpi.nl/srv/svn/LAT/Arbil/branches/arbil.2.2.x/src/nl/mpi/arbil\nRepository Root: svn+ssh://svn.mpi.nl/srv/svn/LAT\nRepository UUID: d4234c5b-7c33-0410-a85d-91bdcf7faf44\nRevision: 25151\nNode Kind: directory\nSchedule: normal\nLast Changed Author: twagoo\nLast Changed Rev: 25151\nLast Changed Date: 2011-07-07 11:24:00 +0200 (Thu, 07 Jul 2011)\n";
+    public String currentVersionFile = "http://www.mpi.nl/tg/j2se/jnlp/arbil/arbil-testing-current.txt";
 }
         
