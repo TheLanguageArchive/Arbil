cbdf48727c62a3511b5e6c66c7a741c8b5ed329c
Twan Goosen <Twan.Goosen@mpi.nl>
2011-07-26 13:35:37 +0000

    Unmarked ArbilRootNode 'serializable', as it cannot be guaranteed (and actually is not when implemented as member class in AbstractTreeHelper)

--- src/main/java/nl/mpi/arbil/data/ArbilRootNode.java
+++ src/main/java/nl/mpi/arbil/data/ArbilRootNode.java
@@ -1,6 +1,5 @@
 package nl.mpi.arbil.data;
 
-import java.io.Serializable;
 import java.util.Arrays;
 import java.util.Vector;
 import javax.swing.ImageIcon;
@@ -9,7 +8,7 @@ import javax.swing.ImageIcon;
  *
  * @author Twan Goosen <twan.goosen@mpi.nl>
  */
-public abstract class ArbilRootNode implements ArbilNode, Serializable {
+public abstract class ArbilRootNode implements ArbilNode {
 
     private String name;
     private ImageIcon icon;
