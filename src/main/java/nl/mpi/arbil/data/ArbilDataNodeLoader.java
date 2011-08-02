0274befadafd068e3fd80f574bc0854e90dc2bf1
Twan Goosen <Twan.Goosen@mpi.nl>
2011-08-02 09:42:54 +0000

    Renamed AbstractDataNodeLoader to DefaultDataNodeLoader as it is not abstract and should be used in unextended form by arbil-wicket

--- src/main/java/nl/mpi/arbil/data/ArbilDataNodeLoader.java
+++ src/main/java/nl/mpi/arbil/data/ArbilDataNodeLoader.java
@@ -5,7 +5,7 @@ package nl.mpi.arbil.data;
  * Created on : Dec 30, 2008, 3:04:39 PM
  * @author Peter.Withers@mpi.nl 
  */
-public class ArbilDataNodeLoader extends AbstractDataNodeLoader {
+public class ArbilDataNodeLoader extends DefaultDataNodeLoader {
 
     static private ArbilDataNodeLoader singleInstance = null;
     private static LoaderThreadManager threadManager;
