0d571350ba900983962011bfb470b286c20ff182
Twan Goosen <Twan.Goosen@mpi.nl>
2011-08-15 09:52:27 +0000

    Removed dependency on xml-resolver, using ArbilEntityResolver instead. Adding required methods for validation

--- src/main/java/nl/mpi/arbil/data/ArbilEntityResolver.java
+++ src/main/java/nl/mpi/arbil/data/ArbilEntityResolver.java
@@ -1,7 +1,12 @@
 package nl.mpi.arbil.data;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.Reader;
 import java.net.URI;
 import nl.mpi.arbil.userstorage.SessionStorage;
 import nl.mpi.arbil.util.BugCatcher;
@@ -13,13 +18,17 @@ import org.xml.sax.SAXException;
  *  Document   : ArbilEntityResolver
  *  Created on : Sep 16, 2010, 1:36:19 PM
  *  Author     : Peter Withers
+ * 
+ * @author Peter.Withers@mpi.nl
+ * @author Twan.Goosen@mpi.nl
  */
 public class ArbilEntityResolver implements EntityResolver {
 
+    public static final int EXPIRE_CACHE_DAYS = 7;
     private static BugCatcher bugCatcher;
 
     public static void setBugCatcher(BugCatcher bugCatcherInstance) {
-        bugCatcher = bugCatcherInstance;
+	bugCatcher = bugCatcherInstance;
     }
     private static SessionStorage sessionStorage;
 
@@ -29,28 +38,49 @@ public class ArbilEntityResolver implements EntityResolver {
     private URI parentUri;
 
     public ArbilEntityResolver(URI parentUriLocal) {
-        parentUri = parentUriLocal;
+	parentUri = parentUriLocal;
     }
 
     public InputSource resolveEntity(String publicId, String systemId) throws SAXException, IOException {
 //        System.out.println("publicId: " + publicId);
 //        System.out.println("systemId: " + systemId);
-        String targetString;
-        // todo: test with http://www.loc.gov/ndnp/xml/alto-1-2.xsd issue: exception: no protocol: ./xlink.xsd need to resolve local references
-        if (parentUri != null) {
-            URI resolvedSystemIdURL = parentUri.resolve(systemId);
-            targetString = resolvedSystemIdURL.toString();
-        } else {
-            targetString = systemId;
-        }
-        File cachedfile = sessionStorage.updateCache(targetString, 7);
-        if (!cachedfile.exists()) {
-            // todo: pull the file out of the jar
-            bugCatcher.logError(new Exception("dependant xsd not stored in the jar for offline first time use: " + cachedfile));
-        }
-        String cachedfileString = cachedfile.toURI().toString();
+	String targetString;
+	// todo: test with http://www.loc.gov/ndnp/xml/alto-1-2.xsd issue: exception: no protocol: ./xlink.xsd need to resolve local references
+	if (parentUri != null) {
+	    URI resolvedSystemIdURL = parentUri.resolve(systemId);
+	    targetString = resolvedSystemIdURL.toString();
+	} else {
+	    targetString = systemId;
+	}
+	final File cachedfile = sessionStorage.updateCache(targetString, EXPIRE_CACHE_DAYS);
+	if (!cachedfile.exists()) {
+	    // todo: pull the file out of the jar
+	    bugCatcher.logError(new Exception("dependant xsd not stored in the jar for offline first time use: " + cachedfile));
+	}
+	String cachedfileString = cachedfile.toURI().toString();
 //        System.out.println("cachedfileString: " + cachedfileString);
-        return new InputSource(cachedfileString);
-        // to use the default behaviour by returning null
+	return new InputSource(cachedfileString) {
+
+	    @Override
+	    public InputStream getByteStream() {
+		try {
+		    return new FileInputStream(cachedfile);
+		} catch (FileNotFoundException ex) {
+		    bugCatcher.logError(ex);
+		    return null;
+		}
+	    }
+
+	    @Override
+	    public Reader getCharacterStream() {
+		try {
+		    return new FileReader(cachedfile);
+		} catch (FileNotFoundException ex) {
+		    bugCatcher.logError(ex);
+		    return null;
+		}
+	    }
+	};
+	// to use the default behaviour by returning null
     }
 }
