afc0075c7f8801044f0a6b6cb79c64dd2eef6513
Twan Goosen <Twan.Goosen@mpi.nl>
2011-08-15 09:52:27 +0000

    Removed dependency on xml-resolver, using ArbilEntityResolver instead. Adding required methods for validation

--- arbil/src/main/java/nl/mpi/arbil/util/ArbilResourceResolver.java
+++ arbil/src/main/java/nl/mpi/arbil/util/ArbilResourceResolver.java
@@ -1,6 +1,9 @@
 package nl.mpi.arbil.util;
 
-import org.apache.xml.resolver.tools.CatalogResolver;
+import java.net.URI;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import nl.mpi.arbil.data.ArbilEntityResolver;
 import org.w3c.dom.bootstrap.DOMImplementationRegistry;
 import org.w3c.dom.ls.DOMImplementationLS;
 import org.w3c.dom.ls.LSInput;
@@ -12,26 +15,32 @@ import org.xml.sax.InputSource;
  * @author Twan Goosen <twan.goosen@mpi.nl>
  */
 public class ArbilResourceResolver implements LSResourceResolver {
-    private CatalogResolver catRes = new CatalogResolver();
+    //private CatalogResolver catRes = new CatalogResolver();
 
     public LSInput resolveResource(String type, String namespaceURI, String publicId, String systemId, String baseURI) {
-	InputSource resolveEntity = catRes.resolveEntity(publicId, systemId);
-	DOMImplementationLS domImplementation;
 	try {
-	    domImplementation = (DOMImplementationLS) DOMImplementationRegistry.newInstance().getDOMImplementation("LS");
-	} catch (ClassCastException e) {
-	    throw new RuntimeException(e);
-	} catch (ClassNotFoundException e) {
-	    throw new RuntimeException(e);
-	} catch (InstantiationException e) {
-	    throw new RuntimeException(e);
-	} catch (IllegalAccessException e) {
-	    throw new RuntimeException(e);
+	    ArbilEntityResolver resolver = new ArbilEntityResolver(new URI(baseURI));
+	    InputSource resolveEntity = resolver.resolveEntity(publicId,systemId);
+
+	    DOMImplementationLS domImplementation;
+	    try {
+		domImplementation = (DOMImplementationLS) DOMImplementationRegistry.newInstance().getDOMImplementation("LS");
+	    } catch (ClassCastException e) {
+		throw new RuntimeException(e);
+	    } catch (ClassNotFoundException e) {
+		throw new RuntimeException(e);
+	    } catch (InstantiationException e) {
+		throw new RuntimeException(e);
+	    } catch (IllegalAccessException e) {
+		throw new RuntimeException(e);
+	    }
+	    LSInput lsInput = domImplementation.createLSInput();
+	    lsInput.setByteStream(resolveEntity.getByteStream());
+	    lsInput.setCharacterStream(resolveEntity.getCharacterStream());
+	    return lsInput;
+	} catch (Exception ex) {
+	    Logger.getLogger(ArbilResourceResolver.class.getName()).log(Level.SEVERE, null, ex);
 	}
-	LSInput lsInput = domImplementation.createLSInput();
-	lsInput.setByteStream(resolveEntity.getByteStream());
-	lsInput.setCharacterStream(resolveEntity.getCharacterStream());
-	return lsInput;
+	return null;
     }
-    
 }
