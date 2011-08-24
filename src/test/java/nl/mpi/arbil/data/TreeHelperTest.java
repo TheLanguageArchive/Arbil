67ad36257ae6993c3c048ca484b1b7eaf866196a
Peter Withers <Peter.Withers@mpi.nl>
2011-08-24 15:53:23 +0000

    Converted Arbil node from an interface to an abstract class and made more use of it in the tree so that the use of data nodes in the tree is more flexible with respect to the data source used.

--- src/test/java/nl/mpi/arbil/data/TreeHelperTest.java
+++ src/test/java/nl/mpi/arbil/data/TreeHelperTest.java
@@ -27,71 +27,71 @@ public class TreeHelperTest extends ArbilTest {
 
     @Before
     public void setUp() throws Exception {
-	inject();
+        inject();
     }
 
     @Test
     public void testLocationsList() throws Exception {
-	assertEquals(0, getTreeHelper().getLocalCorpusNodes().length);
-	addToLocalTreeFromResource("/nl/mpi/arbil/data/testfiles/\u0131md\u0131test.imdi");
-	assertEquals(1, getTreeHelper().getLocalCorpusNodes().length);
-	assertFalse(getTreeHelper().getLocalCorpusNodes()[0].fileNotFound);
+        assertEquals(0, getTreeHelper().getLocalCorpusNodes().length);
+        addToLocalTreeFromResource("/nl/mpi/arbil/data/testfiles/\u0131md\u0131test.imdi");
+        assertEquals(1, getTreeHelper().getLocalCorpusNodes().length);
+        assertFalse(((ArbilDataNode) getTreeHelper().getLocalCorpusNodes()[0]).fileNotFound);
     }
 
     @Override
     protected SessionStorage newSessionStorage() {
-	return new MockSessionStorage() {
+        return new MockSessionStorage() {
 
-	    @Override
-	    public final String[] loadStringArray(String filename) throws IOException {
-		File currentConfigFile = new File(getStorageDirectory(), filename + ".config");
-		if (currentConfigFile.exists()) {
-		    ArrayList<String> stringArrayList = new ArrayList<String>();
-		    FileInputStream fstream = new FileInputStream(currentConfigFile);
-		    DataInputStream in = new DataInputStream(fstream);
-		    try {
-			BufferedReader br = new BufferedReader(new InputStreamReader(in, "UTF8"));
-			try {
-			    String strLine;
-			    while ((strLine = br.readLine()) != null) {
-				stringArrayList.add(strLine);
-			    }
-			} finally {
-			    br.close();
-			}
-		    } finally {
-			in.close();
-			fstream.close();
-		    }
-		    return stringArrayList.toArray(new String[]{});
-		}
-		return null;
-	    }
+            @Override
+            public final String[] loadStringArray(String filename) throws IOException {
+                File currentConfigFile = new File(getStorageDirectory(), filename + ".config");
+                if (currentConfigFile.exists()) {
+                    ArrayList<String> stringArrayList = new ArrayList<String>();
+                    FileInputStream fstream = new FileInputStream(currentConfigFile);
+                    DataInputStream in = new DataInputStream(fstream);
+                    try {
+                        BufferedReader br = new BufferedReader(new InputStreamReader(in, "UTF8"));
+                        try {
+                            String strLine;
+                            while ((strLine = br.readLine()) != null) {
+                                stringArrayList.add(strLine);
+                            }
+                        } finally {
+                            br.close();
+                        }
+                    } finally {
+                        in.close();
+                        fstream.close();
+                    }
+                    return stringArrayList.toArray(new String[]{});
+                }
+                return null;
+            }
 
-	    @Override
-	    public final void saveStringArray(String filename, String[] storableValue) throws IOException {
-		// save the location list to a text file that admin-users can read and hand edit if they really want to
-		File destinationConfigFile = new File(getStorageDirectory(), filename + ".config");
-		File tempConfigFile = new File(getStorageDirectory(), filename + ".config.tmp");
+            @Override
+            public final void saveStringArray(String filename, String[] storableValue) throws IOException {
+                // save the location list to a text file that admin-users can read and hand edit if they really want to
+                File destinationConfigFile = new File(getStorageDirectory(), filename + ".config");
+                File tempConfigFile = new File(getStorageDirectory(), filename + ".config.tmp");
 
-		BufferedWriter out = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(tempConfigFile), "UTF8"));
-		for (String currentString : storableValue) {
-		    out.write(currentString + "\r\n");
-		}
-		out.close();
-		destinationConfigFile.delete();
-		tempConfigFile.renameTo(destinationConfigFile);
-	    }
+                BufferedWriter out = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(tempConfigFile), "UTF8"));
+                for (String currentString : storableValue) {
+                    out.write(currentString + "\r\n");
+                }
+                out.close();
+                destinationConfigFile.delete();
+                tempConfigFile.renameTo(destinationConfigFile);
+            }
 
-	    @Override
-	    public boolean pathIsInsideCache(File fullTestFile) {
-		return true;
-	    }
+            @Override
+            public boolean pathIsInsideCache(File fullTestFile) {
+                return true;
+            }
 
-	    @Override
-	    public boolean pathIsInFavourites(File fullTestFile) {
-		return false;
-	    }
-	};
+            @Override
+            public boolean pathIsInFavourites(File fullTestFile) {
+                return false;
+            }
+        };
     }
 }
