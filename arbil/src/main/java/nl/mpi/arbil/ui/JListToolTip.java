2137562bbab6bfa7ca0a82604bcf82f4dc203b33
Twan Goosen <Twan.Goosen@mpi.nl>
2011-09-02 14:14:05 +0000

    Removed rendundant storage of schema file in CmdiTemplate

--- arbil/src/main/java/nl/mpi/arbil/ui/JListToolTip.java
+++ arbil/src/main/java/nl/mpi/arbil/ui/JListToolTip.java
@@ -9,7 +9,6 @@ import javax.swing.BoxLayout;
 import javax.swing.Icon;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
-import javax.swing.JTextArea;
 import javax.swing.JToolTip;
 import javax.swing.SwingConstants;
 import javax.swing.ToolTipManager;
