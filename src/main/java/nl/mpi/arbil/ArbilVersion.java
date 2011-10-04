b6b225cf6b927e6e71bcee0e13ff7d12baa90c40
Twan Goosen <Twan.Goosen@mpi.nl>
2011-10-04 14:18:46 +0000

    Merged fix for issues with CMDI profiles with empty description attributes in controllde vocabularies

--- src/main/java/nl/mpi/arbil/ArbilVersion.java
+++ src/main/java/nl/mpi/arbil/ArbilVersion.java
@@ -4,10 +4,10 @@ public class ArbilVersion {
     public String applicationIconName = "arbil-testing128x128.png";
     public String currentMajor = "2";
     public String currentMinor = "3";
-    public String currentRevision = "26545";
-    public String lastCommitDate = "Last Changed Date: 2011-09-27 14:50:26 +0200 (Tue, 27 Sep 2011)";
-    public String compileDate = "Fri Sep 30 11:16:05 CEST 2011";
-    public String fullInfo = "\nPath: src/main/java/nl/mpi/arbil\nURL: svn+ssh://svn.mpi.nl/srv/svn/LAT/Arbil/branches/arbil.2.3.x/src/main/java/nl/mpi/arbil\nRepository Root: svn+ssh://svn.mpi.nl/srv/svn/LAT\nRepository UUID: d4234c5b-7c33-0410-a85d-91bdcf7faf44\nRevision: 26618\nNode Kind: directory\nSchedule: normal\nLast Changed Author: twagoo\nLast Changed Rev: 26545\nLast Changed Date: 2011-09-27 14:50:26 +0200 (Tue, 27 Sep 2011)\n";
+    public String currentRevision = "26628";
+    public String lastCommitDate = "Last Changed Date: 2011-09-30 13:47:33 +0200 (Fri, 30 Sep 2011)";
+    public String compileDate = "Fri Sep 30 13:52:25 CEST 2011";
+    public String fullInfo = "\nPath: src/main/java/nl/mpi/arbil\nURL: svn+ssh://svn.mpi.nl/srv/svn/LAT/Arbil/branches/arbil.2.3.x/src/main/java/nl/mpi/arbil\nRepository Root: svn+ssh://svn.mpi.nl/srv/svn/LAT\nRepository UUID: d4234c5b-7c33-0410-a85d-91bdcf7faf44\nRevision: 26629\nNode Kind: directory\nSchedule: normal\nLast Changed Author: twagoo\nLast Changed Rev: 26628\nLast Changed Date: 2011-09-30 13:47:33 +0200 (Fri, 30 Sep 2011)\n";
     public String currentVersionFile = "http://www.mpi.nl/tg/j2se/jnlp/arbil/arbil-testing-current.txt";
 }
         
