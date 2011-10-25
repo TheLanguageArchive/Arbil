ef30c7995fb2a99d3154a0b6b3e8501d960474d5
Twan Goosen <Twan.Goosen@mpi.nl>
2011-10-25 09:26:31 +0000

    Long Field Editor: get help string in separate thread, because this may take some while in the case of CMDI where it needs to be retrieved from isocat. Refs #595

--- src/main/java/nl/mpi/arbil/clarin/profiles/CmdiTemplate.java
+++ src/main/java/nl/mpi/arbil/clarin/profiles/CmdiTemplate.java
@@ -47,9 +47,7 @@ import org.w3c.dom.Document;
 import org.w3c.dom.NamedNodeMap;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
-import org.xml.sax.Attributes;
 import org.xml.sax.SAXException;
-import org.xml.sax.helpers.DefaultHandler;
 
 /**
  * CmdiTemplate.java
