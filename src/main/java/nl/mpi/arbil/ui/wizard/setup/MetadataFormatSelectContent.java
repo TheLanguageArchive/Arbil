5627409a1ba99d247ad12854e4a93057d90b9d6d
Twan Goosen <Twan.Goosen@mpi.nl>
2011-11-15 12:15:20 +0000

    Setup wizard: - added CmdiProfileSelect and Confirmation contents - check on imdi/cmdi selection (at least one should be selected) on Next - confirmation dialog on cancel - Text content has hyperlink wizard to open external links in browser
     (refs #1026)

--- src/main/java/nl/mpi/arbil/ui/wizard/setup/MetadataFormatSelectContent.java
+++ src/main/java/nl/mpi/arbil/ui/wizard/setup/MetadataFormatSelectContent.java
@@ -1,6 +1,11 @@
 package nl.mpi.arbil.ui.wizard.setup;
 
+import java.awt.BorderLayout;
+import javax.swing.BoxLayout;
 import javax.swing.JCheckBox;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.SwingConstants;
 
 /**
  * ArbilWizard content that lets the user select which MD formats to use: IMDI and/or CMDI
@@ -15,12 +20,19 @@ public class MetadataFormatSelectContent extends TextInstructionWizardContent {
     public MetadataFormatSelectContent(ArbilSetupWizardModel model) {
 	super("/nl/mpi/arbil/resources/html/wizard/MetadataFormatSelect.html");
 	this.model = model;
-
+	
+	JPanel checkBoxPanel = new JPanel();
+	checkBoxPanel.setLayout(new BoxLayout(checkBoxPanel, BoxLayout.PAGE_AXIS));
+	checkBoxPanel.setOpaque(true);
+	
 	imdiCheckBox = new JCheckBox("IDMI");
-	add(imdiCheckBox);
+	imdiCheckBox.setHorizontalAlignment(SwingConstants.LEFT);
+	checkBoxPanel.add(imdiCheckBox);
 	cmdiCheckBox = new JCheckBox("CDMI");
-	add(cmdiCheckBox);
-
+	cmdiCheckBox.setHorizontalAlignment(SwingConstants.LEFT);
+	checkBoxPanel.add(cmdiCheckBox);
+	add(checkBoxPanel, BorderLayout.WEST);
+	
 	refresh();
     }
 
@@ -32,17 +44,28 @@ public class MetadataFormatSelectContent extends TextInstructionWizardContent {
     }
 
     @Override
-    public void onNext() {
+    public boolean beforeNext() {
 	updateModel();
+	if (model.imdi || model.cmdi) {
+	    return true;
+	} else {
+	    JOptionPane.showMessageDialog(this, "Select at least one of the options before continuing", "Select a metadata format", JOptionPane.WARNING_MESSAGE);
+	    return false;
+	}
     }
 
     @Override
-    public void onPrevious() {
+    public boolean beforePrevious() {
 	updateModel();
+	return true;
     }
 
     public Object getNext() {
-	return null;
+	if (model.cmdi) {
+	    return CmdiProfileSelectContent.class;
+	} else {
+	    return ConfirmationContent.class;
+	}
     }
 
     public Object getPrevious() {
--- src/main/java/nl/mpi/arbil/ui/wizard/setup/TextInstructionWizardContent.java
+++ src/main/java/nl/mpi/arbil/ui/wizard/setup/TextInstructionWizardContent.java
@@ -1,19 +1,23 @@
 package nl.mpi.arbil.ui.wizard.setup;
 
+import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.Desktop;
 import java.awt.Insets;
 import java.io.IOException;
-import javax.swing.BoxLayout;
+import java.net.URISyntaxException;
 import javax.swing.JComponent;
 import javax.swing.JPanel;
 import javax.swing.JTextPane;
-import javax.swing.SwingConstants;
+import javax.swing.event.HyperlinkEvent;
+import javax.swing.event.HyperlinkListener;
 import nl.mpi.arbil.ui.GuiHelper;
 import nl.mpi.arbil.ui.wizard.ArbilWizardContent;
 
 /**
- * Abstract Wizard content that has vertical box layout with JTextPane on top
+ * Abstract Wizard content that has vertical box layout with JTextPane on top (BorderLayout.NORTH)
  * Contents of JTextPane come from resource with specified location
+ * @see BorderLayout
  * @author Twan Goosen <twan.goosen@mpi.nl>
  */
 public abstract class TextInstructionWizardContent extends JPanel implements ArbilWizardContent {
@@ -24,31 +28,51 @@ public abstract class TextInstructionWizardContent extends JPanel implements Arb
      */
     public TextInstructionWizardContent(String resourceLocation) {
 	super();
-	setLayout(new BoxLayout(this, BoxLayout.PAGE_AXIS));
+	setLayout(new BorderLayout());
 	setBackground(Color.WHITE);
-	
+
 	JTextPane textPane = new JTextPane();
-	textPane.setBackground(Color.WHITE);
+	textPane.setEditable(false);
+	textPane.addHyperlinkListener(hyperLinkListener);
 	textPane.setMargin(new Insets(5, 10, 5, 10));
+	textPane.setBackground(Color.WHITE);
 	try {
 	    textPane.setPage(JTextPane.class.getResource(resourceLocation));
 	} catch (IOException ex) {
 	    textPane.setText("Error while getting wizard text. Please check the error log.");
 	    GuiHelper.linorgBugCatcher.logError("I/O exception while getting wizard text", ex);
 	}
-	add(textPane);
+	add(textPane, BorderLayout.NORTH);
     }
-    
+
     public JComponent getContent() {
 	return this;
     }
-    
-    public void onNext() {
+
+    public boolean beforeNext() {
+	return true;
     }
-    
-    public void onPrevious() {
+
+    public boolean beforePrevious() {
+	return true;
     }
-    
-    public void refresh(){	
+
+    public void refresh() {
     }
+    private static HyperlinkListener hyperLinkListener = new HyperlinkListener() {
+
+	public void hyperlinkUpdate(HyperlinkEvent e) {
+	    if (e.getEventType() == HyperlinkEvent.EventType.ACTIVATED) {
+		if (Desktop.isDesktopSupported()) {
+		    try {
+			Desktop.getDesktop().browse(e.getURL().toURI());
+		    } catch (IOException ex) {
+			// ignore
+		    } catch (URISyntaxException ex) {
+			// ignore
+		    }
+		}
+	    }
+	}
+    };
 }
