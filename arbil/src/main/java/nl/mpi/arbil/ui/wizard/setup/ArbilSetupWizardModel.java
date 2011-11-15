0e43907f6ff475503686f87bc20f6abe6634c04e
Twan Goosen <Twan.Goosen@mpi.nl>
2011-11-15 15:02:01 +0000

    Encapsulated fields of arbilSetupWizardModel (refs #1026)

--- arbil/src/main/java/nl/mpi/arbil/ui/wizard/setup/ArbilSetupWizardModel.java
+++ arbil/src/main/java/nl/mpi/arbil/ui/wizard/setup/ArbilSetupWizardModel.java
@@ -6,6 +6,34 @@ package nl.mpi.arbil.ui.wizard.setup;
  * @author Twan Goosen <twan.goosen@mpi.nl>
  */
 public class ArbilSetupWizardModel {
-    public boolean imdi;
-    public boolean cmdi;
+    private boolean imdiSelected;
+    private boolean cmdiSelected;
+
+    /**
+     * @return the imdiSelected
+     */
+    public boolean isImdiSelected() {
+	return imdiSelected;
+    }
+
+    /**
+     * @param imdiSelected the imdiSelected to set
+     */
+    public void setImdiSelected(boolean imdiSelected) {
+	this.imdiSelected = imdiSelected;
+    }
+
+    /**
+     * @return the cmdiSelected
+     */
+    public boolean isCmdiSelected() {
+	return cmdiSelected;
+    }
+
+    /**
+     * @param cmdiSelected the cmdiSelected to set
+     */
+    public void setCmdiSelected(boolean cmdiSelected) {
+	this.cmdiSelected = cmdiSelected;
+    }
 }
