51f56beaa5933c8c4fd4e950ff80946db34a482e
Twan Goosen <Twan.Goosen@mpi.nl>
2011-11-30 15:42:21 +0000

    Re-branched arbil 2.3 testing from trunk

--- src/nl/mpi/arbil/LinorgVersion.java
+++ src/nl/mpi/arbil/LinorgVersion.java
@@ -4,10 +4,10 @@ public class LinorgVersion {
     public String applicationIconName = "arbil-testing128x128.png";
     public String currentMajor = "2";
     public String currentMinor = "2";
-    public String currentRevision = "25326";
-    public String lastCommitDate = "Last Changed Date: 2011-07-22 10:26:33 +0200 (Fri, 22 Jul 2011)";
-    public String compileDate = "Fri Jul 22 11:26:50 CEST 2011";
-    public String fullInfo = "\nPath: src/nl/mpi/arbil\nURL: svn+ssh://svn.mpi.nl/srv/svn/LAT/Arbil/branches/arbil.2.2.x/src/nl/mpi/arbil\nRepository Root: svn+ssh://svn.mpi.nl/srv/svn/LAT\nRepository UUID: d4234c5b-7c33-0410-a85d-91bdcf7faf44\nRevision: 25325\nNode Kind: directory\nSchedule: normal\nLast Changed Author: twagoo\nLast Changed Rev: 25325\nLast Changed Date: 2011-07-22 10:26:33 +0200 (Fri, 22 Jul 2011)\n";
+    public String currentRevision = "25329";
+    public String lastCommitDate = "Last Changed Date: 2011-07-22 12:15:46 +0200 (Fri, 22 Jul 2011)";
+    public String compileDate = "Tue Sep 27 13:45:48 CEST 2011";
+    public String fullInfo = "\nPath: src/nl/mpi/arbil\nURL: svn+ssh://svn.mpi.nl/srv/svn/LAT/Arbil/branches/arbil.2.2.x/src/nl/mpi/arbil\nRepository Root: svn+ssh://svn.mpi.nl/srv/svn/LAT\nRepository UUID: d4234c5b-7c33-0410-a85d-91bdcf7faf44\nRevision: 26534\nNode Kind: directory\nSchedule: normal\nLast Changed Author: twagoo\nLast Changed Rev: 25329\nLast Changed Date: 2011-07-22 12:15:46 +0200 (Fri, 22 Jul 2011)\n";
     public String currentVersionFile = "http://www.mpi.nl/tg/j2se/jnlp/arbil/arbil-testing-current.txt";
 }
         
