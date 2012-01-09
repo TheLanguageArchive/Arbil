090ab14eccca0193109114c39ab3d5fd66356142
Twan Goosen <Twan.Goosen@mpi.nl>
2012-01-09 16:22:33 +0000

    Removed fix findbugs branch

--- src/nl/mpi/arbil/LinorgVersion.java
+++ src/nl/mpi/arbil/LinorgVersion.java
@@ -4,10 +4,10 @@ public class LinorgVersion {
     public String applicationIconName = "arbil-testing128x128.png";
     public String currentMajor = "2";
     public String currentMinor = "2";
-    public String currentRevision = "25329";
-    public String lastCommitDate = "Last Changed Date: 2011-07-22 12:15:46 +0200 (Fri, 22 Jul 2011)";
-    public String compileDate = "Tue Sep 27 13:45:48 CEST 2011";
-    public String fullInfo = "\nPath: src/nl/mpi/arbil\nURL: svn+ssh://svn.mpi.nl/srv/svn/LAT/Arbil/branches/arbil.2.2.x/src/nl/mpi/arbil\nRepository Root: svn+ssh://svn.mpi.nl/srv/svn/LAT\nRepository UUID: d4234c5b-7c33-0410-a85d-91bdcf7faf44\nRevision: 26534\nNode Kind: directory\nSchedule: normal\nLast Changed Author: twagoo\nLast Changed Rev: 25329\nLast Changed Date: 2011-07-22 12:15:46 +0200 (Fri, 22 Jul 2011)\n";
+    public String currentRevision = "27765";
+    public String lastCommitDate = "Last Changed Date: 2011-11-30 16:42:21 +0100 (Wed, 30 Nov 2011)";
+    public String compileDate = "Tue Dec  6 10:18:18 CET 2011";
+    public String fullInfo = "\nPath: src/nl/mpi/arbil\nURL: https://svn.mpi.nl/LAT/Arbil/branches/arbil.2.2.x/src/nl/mpi/arbil\nRepository Root: https://svn.mpi.nl/LAT\nRepository UUID: d4234c5b-7c33-0410-a85d-91bdcf7faf44\nRevision: 27822\nNode Kind: directory\nSchedule: normal\nLast Changed Author: twagoo\nLast Changed Rev: 27765\nLast Changed Date: 2011-11-30 16:42:21 +0100 (Wed, 30 Nov 2011)\n";
     public String currentVersionFile = "http://www.mpi.nl/tg/j2se/jnlp/arbil/arbil-testing-current.txt";
 }
         
