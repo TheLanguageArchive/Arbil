6c3b69dbebd6570afc8ee4af153aa57a5050dbce
Twan Goosen <Twan.Goosen@mpi.nl>
2012-01-13 13:04:10 +0000

    Fixed table applet: HTML snipplet with correct dependency jars, build script now includes applet package for compilation

--- src/main/java/nl/mpi/arbil/ui/applet/ArbilTableApplet.java
+++ src/main/java/nl/mpi/arbil/ui/applet/ArbilTableApplet.java
@@ -1,6 +1,7 @@
 package nl.mpi.arbil.ui.applet;
 
 import java.net.URI;
+import nl.mpi.arbil.ArbilDesktopInjector;
 import nl.mpi.arbil.ui.GuiHelper;
 import nl.mpi.arbil.ui.ArbilTable;
 import nl.mpi.arbil.ui.ArbilTableModel;
@@ -17,65 +18,67 @@ public class ArbilTableApplet extends javax.swing.JApplet {
 
     @Override
     public void init() {
-        try {
-            java.awt.EventQueue.invokeAndWait(new Runnable() {
+	try {
+	    java.awt.EventQueue.invokeAndWait(new Runnable() {
 
-                public void run() {
-                    initComponents();
-                    addNodesToTable(getParameter("ImdiFileList"));
-                    addShowOnlyColumnsToTable(getParameter("ShowOnlyColumns"));
-                    addChildNodesToTable(getParameter("ChildNodeColumns"));
-                    addHighlightToTable(getParameter("HighlightText"));
-                }
-            });
-        } catch (Exception ex) {
-            new ArbilBugCatcher().logError(ex);
-        }
+		public void run() {
+		    ArbilDesktopInjector.injectHandlers();
+
+		    initComponents();
+		    addNodesToTable(getParameter("ImdiFileList"));
+		    addShowOnlyColumnsToTable(getParameter("ShowOnlyColumns"));
+		    addChildNodesToTable(getParameter("ChildNodeColumns"));
+		    addHighlightToTable(getParameter("HighlightText"));
+		}
+	    });
+	} catch (Exception ex) {
+	    new ArbilBugCatcher().logError(ex);
+	}
     }
 
     private void addNodesToTable(String nodeURLsString) {
-        if (nodeURLsString != null) {
-            for (String currentUrlString : nodeURLsString.split(",")) {
-                try {
-                    arbilTableModel.addSingleArbilDataNode(ArbilDataNodeLoader.getSingleInstance().getArbilDataNode(rootPane, new URI(currentUrlString)));
-                } catch (Exception ex) {
-                    GuiHelper.linorgBugCatcher.logError(ex);
-                }
-            }
-        }
+	if (nodeURLsString != null) {
+	    for (String currentUrlString : nodeURLsString.split(",")) {
+		try {
+		    arbilTableModel.addSingleArbilDataNode(ArbilDataNodeLoader.getSingleInstance().getArbilDataNode(rootPane, new URI(currentUrlString)));
+		} catch (Exception ex) {
+		    GuiHelper.linorgBugCatcher.logError(ex);
+		}
+	    }
+	}
     }
 
     private void addShowOnlyColumnsToTable(String showColumnsString) {
-        if (showColumnsString != null) {
-            for (String currentshowColumns : showColumnsString.split(",")) {
-                arbilTableModel.getFieldView().addShowOnlyColumn(currentshowColumns);
-            }
-        }
+	if (showColumnsString != null && showColumnsString.trim().length() > 0) {
+	    for (String currentshowColumns : showColumnsString.split(",")) {
+		arbilTableModel.getFieldView().addShowOnlyColumn(currentshowColumns.trim());
+	    }
+	}
     }
 
     private void addChildNodesToTable(String childNodesString) {
-        if (childNodesString != null) {
-            for (String currentChildNode : childNodesString.split(",")) {
-                arbilTableModel.addChildTypeToDisplay(currentChildNode);
-            }
-        }
+	if (childNodesString != null && childNodesString.trim().length() > 0) {
+	    for (String currentChildNode : childNodesString.split(",")) {
+		arbilTableModel.addChildTypeToDisplay(currentChildNode.trim());
+	    }
+	}
     }
 
     private void addHighlightToTable(String highlightableTextString) {
-        if (highlightableTextString != null) {
-            for (String highlightText : highlightableTextString.split(",")) {
-                arbilTableModel.highlightMatchingText(highlightText);
-            }
-        }
+	if (highlightableTextString != null&& highlightableTextString.length() > 0) {
+	    for (String highlightText : highlightableTextString.split(",")) {
+		arbilTableModel.highlightMatchingText(highlightText);
+	    }
+	}
     }
 
     private void initComponents() {
-        arbilTableModel = new ArbilTableModel();
-        ArbilTable arbilTable = new ArbilTable(arbilTableModel, tableTitle);
-        ArbilSplitPanel arbilSplitPanel = new ArbilSplitPanel(arbilTable);
-        arbilTableModel.hideContextMenuAndStatusBar = true;
-        arbilSplitPanel.setSplitDisplay();
-        getContentPane().add(arbilSplitPanel, java.awt.BorderLayout.CENTER);
+	arbilTableModel = new ArbilTableModel();
+	ArbilTable arbilTable = new ArbilTable(arbilTableModel, tableTitle);
+	ArbilSplitPanel arbilSplitPanel = new ArbilSplitPanel(arbilTable);
+	arbilTableModel.hideContextMenuAndStatusBar = true;
+	arbilSplitPanel.setSplitDisplay();
+	getContentPane().add(arbilSplitPanel, java.awt.BorderLayout.CENTER);
     }
     private String tableTitle = "Arbil Table Demo";
     private ArbilTableModel arbilTableModel;
