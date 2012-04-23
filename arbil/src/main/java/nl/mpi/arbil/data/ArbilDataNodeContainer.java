c831fab53fef6d9b8d2ff9ae78d77d59ed50f8f8
Twan Goosen <Twan.Goosen@mpi.nl>
2012-04-23 12:03:15 +0000

    Added isFulllyLoadedNodeRequired() method to dataNodeContainer to indicate whether container requires fully loaded node. Refs #1797

--- arbil/src/main/java/nl/mpi/arbil/data/ArbilDataNodeContainer.java
+++ arbil/src/main/java/nl/mpi/arbil/data/ArbilDataNodeContainer.java
@@ -2,7 +2,6 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package nl.mpi.arbil.data;
 
 /**
@@ -10,21 +9,33 @@ package nl.mpi.arbil.data;
  * @author Twan Goosen <twan.goosen@mpi.nl>
  */
 public interface ArbilDataNodeContainer {
+
     /**
      * Data node is to be removed from the container
+     *
      * @param dataNode Data node that should be removed
      */
-    public void dataNodeRemoved(ArbilNode dataNode);
+    void dataNodeRemoved(ArbilNode dataNode);
+
     /**
      * Data node is clearing its icon
+     *
      * @param dataNode Data node that is clearing its icon
      */
-    public void dataNodeIconCleared(ArbilNode dataNode);
-    
+    void dataNodeIconCleared(ArbilNode dataNode);
+
     /**
      * A new child node has been added to the destination node
+     *
      * @param destination Node to which a node has been added
      * @param newNode The newly added node
      */
-    public void dataNodeChildAdded(ArbilNode destination, ArbilNode newChildNode);
+    void dataNodeChildAdded(ArbilNode destination, ArbilNode newChildNode);
+
+    /**
+     * 
+     * @return whether a {@link ArbilDataNode.LoadingState#LOADED} state is required. If false, this indicates a 
+     * {@link ArbilDataNode.LoadingState#PARTIAL} suffices.
+     */
+    boolean isFulllyLoadedNodeRequired();
 }
