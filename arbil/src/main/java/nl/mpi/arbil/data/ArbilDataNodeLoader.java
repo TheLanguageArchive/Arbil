8f8bda72918bc179cead0d6abb52bf6ad54087ad
Twan Goosen <Twan.Goosen@mpi.nl>
2012-04-25 14:42:52 +0000

    Remodeled datanode service selection in datanodeloader; there is now an abstract method that has to return the service on basis of the URI. All logic for this can be in the implementing class

--- arbil/src/main/java/nl/mpi/arbil/data/ArbilDataNodeLoader.java
+++ arbil/src/main/java/nl/mpi/arbil/data/ArbilDataNodeLoader.java
@@ -1,22 +1,36 @@
 package nl.mpi.arbil.data;
 
+import java.net.URI;
 import nl.mpi.arbil.userstorage.SessionStorage;
 import nl.mpi.arbil.util.MessageDialogHandler;
 import nl.mpi.arbil.util.MimeHashQueue;
 import nl.mpi.arbil.util.TreeHelper;
 
 /**
- * Document   : ArbilDataNodeLoader formerly known as ImdiLoader
+ * Document : ArbilDataNodeLoader formerly known as ImdiLoader
  * Created on : Dec 30, 2008, 3:04:39 PM
- * @author Peter.Withers@mpi.nl 
+ *
+ * @author Peter.Withers@mpi.nl
  * @author Twan Goosen <twan.goosen@mpi.nl>
  */
 public class ArbilDataNodeLoader extends DefaultDataNodeLoader {
 
+    private final ArbilDataNodeService imdiDataNodeService;
+    private final ArbilDataNodeService cmdiDataNodeService;
+
     public ArbilDataNodeLoader(MessageDialogHandler messageDialogHandler, SessionStorage sessionStorage, MimeHashQueue mimeHashQueue, TreeHelper treeHelper) {
 	super(new DataNodeLoaderThreadManager());
-	setImdiDataNodeService(new ImdiDataNodeService(this, messageDialogHandler, sessionStorage, mimeHashQueue, treeHelper));
-	setCmdiDataNodeService(new CmdiDataNodeService(this, messageDialogHandler, sessionStorage, mimeHashQueue, treeHelper));
+	imdiDataNodeService = new ImdiDataNodeService(this, messageDialogHandler, sessionStorage, mimeHashQueue, treeHelper);
+	cmdiDataNodeService = new CmdiDataNodeService(this, messageDialogHandler, sessionStorage, mimeHashQueue, treeHelper);
 	setSchemaCheckLocalFiles(sessionStorage.loadBoolean("schemaCheckLocalFiles", getThreadManager().isSchemaCheckLocalFiles()));
+    }
+
+    @Override
+    protected ArbilDataNodeService getDataNodeServiceForUri(URI uri) {
+	if (ArbilDataNode.isPathCmdi(uri.getPath())) {
+	    return cmdiDataNodeService;
+	} else {
+	    return imdiDataNodeService;
+	}
     }
 }
--- arbil/src/main/java/nl/mpi/arbil/data/DefaultDataNodeLoader.java
+++ arbil/src/main/java/nl/mpi/arbil/data/DefaultDataNodeLoader.java
@@ -11,25 +11,15 @@ import java.util.Vector;
  * @author Peter.Withers@mpi.nl
  * @author Twan Goosen <twan.goosen@mpi.nl>
  */
-public class DefaultDataNodeLoader implements DataNodeLoader {
+public abstract class DefaultDataNodeLoader implements DataNodeLoader {
 
     private Hashtable<String, ArbilDataNode> arbilHashTable = new Hashtable<String, ArbilDataNode>();
     private Vector<ArbilDataNode> nodesNeedingSave = new Vector<ArbilDataNode>();
     private DataNodeLoaderThreadManager threadManager;
-    private ArbilDataNodeService imdiDataNodeService;
-    private ArbilDataNodeService cmdiDataNodeService;
 
     public DefaultDataNodeLoader(DataNodeLoaderThreadManager loaderThreadManager) {
 	System.out.println("ArbilDataNodeLoader init");
 	threadManager = loaderThreadManager;
-    }
-
-    protected final void setImdiDataNodeService(ArbilDataNodeService dataNodeService) {
-	this.imdiDataNodeService = dataNodeService;
-    }
-
-    protected final void setCmdiDataNodeService(ArbilDataNodeService dataNodeService) {
-	this.cmdiDataNodeService = dataNodeService;
     }
 
 //    public ImdiTreeObject isImdiObjectLoaded(String localUrlString) {
@@ -193,12 +183,10 @@ public class DefaultDataNodeLoader implements DataNodeLoader {
     }
 
     public ArbilDataNode createNewDataNode(URI uri) {
-	if (ArbilDataNode.isPathCmdi(uri.toString())) {
-	    return new ArbilDataNode(cmdiDataNodeService, uri);
-	} else {
-	    return new ArbilDataNode(imdiDataNodeService, uri);
-	}
+	return new ArbilDataNode(getDataNodeServiceForUri(uri), uri);
     }
+
+    protected abstract ArbilDataNodeService getDataNodeServiceForUri(URI uri);
 
     /**
      * @return the threadManager
