fc527c696fb1dcd651f29f649f679b03fe511d83
Twan Goosen <Twan.Goosen@mpi.nl>
2012-05-22 09:37:06 +0000

    Fixed TreeContextMenu for Container nodes (see #21)
    (This is in arbil-metadata-api branch, will be merged to trunk after this commit)

--- arbil/src/main/java/nl/mpi/arbil/data/ContainerNode.java
+++ arbil/src/main/java/nl/mpi/arbil/data/ContainerNode.java
@@ -15,179 +15,179 @@ public class ContainerNode extends ArbilNode implements Comparable {
     private ImageIcon imageIcon;
 
     public ContainerNode(String labelString, ImageIcon imageIcon, ArbilNode[] childNodes) {
-        this.childNodes = childNodes;
-        this.labelString = labelString;
-        this.imageIcon = imageIcon;
+	this.childNodes = childNodes;
+	this.labelString = labelString;
+	this.imageIcon = imageIcon;
     }
 
     public int compareTo(Object o) {
-        return labelString.compareTo(o.toString());
+	return labelString.compareTo(o.toString());
     }
 
     public void setChildNodes(ArbilNode[] childNodes) {
-        this.childNodes = childNodes;
+	this.childNodes = childNodes;
     }
 
     @Override
     public boolean equals(Object obj) {
-        if (obj == null) {
-            return false;
-        }
-        if (getClass() != obj.getClass()) {
-            return false;
-        }
-        final ContainerNode other = (ContainerNode) obj;
-        if ((this.labelString == null) ? (other.labelString != null) : !this.labelString.equals(other.labelString)) {
-            return false;
-        }
-        return true;
+	if (obj == null) {
+	    return false;
+	}
+	if (getClass() != obj.getClass()) {
+	    return false;
+	}
+	final ContainerNode other = (ContainerNode) obj;
+	if ((this.labelString == null) ? (other.labelString != null) : !this.labelString.equals(other.labelString)) {
+	    return false;
+	}
+	return true;
     }
 
     @Override
     public int hashCode() {
-        int hash = 17;
+	int hash = 17;
 //        hash = 19 * hash + Arrays.deepHashCode(this.childNodes);
 //        hash = 19 * hash + this.childNodes.hashCode();
-        hash = 19 * hash + (this.labelString != null ? this.labelString.hashCode() : 0);
-        return hash;
+	hash = 19 * hash + (this.labelString != null ? this.labelString.hashCode() : 0);
+	return hash;
     }
 
     @Override
     public String toString() {
-        return labelString + " (" + childNodes.length + ")";
+	return labelString + " (" + childNodes.length + ")";
     }
 
     @Override
     public ArbilNode[] getChildArray() {
-        return childNodes;
+	return childNodes;
     }
 
     @Override
     public ImageIcon getIcon() {
-        return imageIcon;
+	return imageIcon;
     }
 
     @Override
     public void registerContainer(ArbilDataNodeContainer containerToAdd) {
-        for (ArbilNode currentChild : childNodes) {
-            currentChild.registerContainer(containerToAdd);
-        }
+	for (ArbilNode currentChild : childNodes) {
+	    currentChild.registerContainer(containerToAdd);
+	}
     }
 
     @Override
     public void removeContainer(ArbilDataNodeContainer containerToRemove) {
-        for (ArbilNode currentChild : childNodes) {
-            currentChild.removeContainer(containerToRemove);
-        }
+	for (ArbilNode currentChild : childNodes) {
+	    currentChild.removeContainer(containerToRemove);
+	}
     }
 
     @Override
     public ArbilDataNode[] getAllChildren() {
-        throw new UnsupportedOperationException("Not supported yet.");
+	throw new UnsupportedOperationException("Not supported yet.");
     }
 
     @Override
     public void getAllChildren(Vector<ArbilDataNode> allChildren) {
-        throw new UnsupportedOperationException("Not supported yet.");
+	throw new UnsupportedOperationException("Not supported yet.");
     }
 
     @Override
     public int getChildCount() {
-        throw new UnsupportedOperationException("Not supported yet.");
+	throw new UnsupportedOperationException("Not supported yet.");
     }
 
     @Override
     public boolean hasCatalogue() {
-        throw new UnsupportedOperationException("Not supported yet.");
+	throw new UnsupportedOperationException("Not supported yet.");
     }
 
     @Override
     public boolean hasHistory() {
-        throw new UnsupportedOperationException("Not supported yet.");
+	throw new UnsupportedOperationException("Not supported yet.");
     }
 
     @Override
     public boolean hasLocalResource() {
-        throw new UnsupportedOperationException("Not supported yet.");
+	throw new UnsupportedOperationException("Not supported yet.");
     }
 
     @Override
     public boolean hasResource() {
-        throw new UnsupportedOperationException("Not supported yet.");
+	throw new UnsupportedOperationException("Not supported yet.");
     }
 
     @Override
     public boolean isArchivableFile() {
-        throw new UnsupportedOperationException("Not supported yet.");
+	return false;
     }
 
     @Override
     public boolean isCatalogue() {
-        throw new UnsupportedOperationException("Not supported yet.");
+	return false;
     }
 
     @Override
     public boolean isChildNode() {
-        throw new UnsupportedOperationException("Not supported yet.");
+	throw new UnsupportedOperationException("Not supported yet.");
     }
 
     @Override
     public boolean isCmdiMetaDataNode() {
-        throw new UnsupportedOperationException("Not supported yet.");
+	return false;
     }
 
     @Override
     public boolean isCorpus() {
-        throw new UnsupportedOperationException("Not supported yet.");
+	return false;
     }
 
     @Override
     public boolean isDataLoaded() {
-        throw new UnsupportedOperationException("Not supported yet.");
+	throw new UnsupportedOperationException("Not supported yet.");
     }
 
     @Override
     public boolean isDirectory() {
-        throw new UnsupportedOperationException("Not supported yet.");
+	return false;
     }
 
     @Override
     public boolean isEditable() {
-        throw new UnsupportedOperationException("Not supported yet.");
+	return false;
     }
 
     @Override
     public boolean isEmptyMetaNode() {
-        throw new UnsupportedOperationException("Not supported yet.");
+	return false;
     }
 
     @Override
     public boolean isFavorite() {
-        throw new UnsupportedOperationException("Not supported yet.");
+	return false;
     }
 
     @Override
     public boolean isLoading() {
-        throw new UnsupportedOperationException("Not supported yet.");
+	return false;
     }
 
     @Override
     public boolean isLocal() {
-        throw new UnsupportedOperationException("Not supported yet.");
+	throw new UnsupportedOperationException("Not supported yet.");
     }
 
     @Override
     public boolean isMetaDataNode() {
-        throw new UnsupportedOperationException("Not supported yet.");
+	return false;
     }
 
     @Override
     public boolean isResourceSet() {
-        throw new UnsupportedOperationException("Not supported yet.");
+	throw new UnsupportedOperationException("Not supported yet.");
     }
 
     @Override
     public boolean isSession() {
-        throw new UnsupportedOperationException("Not supported yet.");
+	throw new UnsupportedOperationException("Not supported yet.");
     }
 }
