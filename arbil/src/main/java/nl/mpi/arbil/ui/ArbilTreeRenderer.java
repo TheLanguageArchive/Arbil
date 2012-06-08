d21f62c1db55a637cf8203cd568162bf791fb0b1
Peter Withers <Peter.Withers@mpi.nl>
2012-06-08 15:48:30 +0000

    Made changes to address the Swing EDT issues causing Arbil to misbehave at times including the applet version. refs #1974 #1966

--- arbil/src/main/java/nl/mpi/arbil/ui/ArbilTreeRenderer.java
+++ arbil/src/main/java/nl/mpi/arbil/ui/ArbilTreeRenderer.java
@@ -2,52 +2,52 @@ package nl.mpi.arbil.ui;
 
 import java.awt.Color;
 import java.awt.Component;
-import javax.swing.BorderFactory;
 import javax.swing.JLabel;
 import javax.swing.JTree;
-import javax.swing.border.LineBorder;
 import javax.swing.tree.DefaultMutableTreeNode;
 import javax.swing.tree.TreeCellRenderer;
 import nl.mpi.arbil.data.ArbilDataNode;
 import nl.mpi.arbil.data.ArbilNode;
 
 /**
- * Document   : ArbilTreeRenderer
- * Created on : 
+ * Document : ArbilTreeRenderer
+ * Created on :
  * @author Peter.Withers@mpi.nl
  */
-public class ArbilTreeRenderer implements TreeCellRenderer {
-    
+public class ArbilTreeRenderer extends JLabel implements TreeCellRenderer {
+
     public Component getTreeCellRendererComponent(JTree tree, Object value, boolean selected, boolean expanded, boolean leaf, int row, boolean hasFocus) {
-	JLabel returnComponent;
-	DefaultMutableTreeNode node = (DefaultMutableTreeNode) value;
-	if (node.getUserObject() instanceof ArbilNode) {
-	    ArbilNode arbilNode = (ArbilNode) node.getUserObject();
-	    // create the object with parameters so the jvm has a chance to reused objects in memory
-	    returnComponent = new JLabel(arbilNode.toString(), arbilNode.getIcon(), JLabel.LEFT);
-	    
-	    if (arbilNode instanceof ArbilDataNode) {
-		if (((ArbilDataNode) arbilNode).isContainerNode()) {
-		    returnComponent.setForeground(Color.DARK_GRAY);
-		    ((JLabel) returnComponent).setText("<html><u>" + arbilNode.toString() + "</u></html>");
-		}
-		if (/*!sel && */((ArbilDataNode) arbilNode).hasSchemaError) {
-		    returnComponent.setForeground(Color.RED);
-		}
-		if (/*!sel && */((ArbilDataNode) arbilNode).getNeedsSaveToDisk(true)) {
-		    returnComponent.setForeground(Color.BLUE);
-		}
-	    }
-	} else if (node.getUserObject() instanceof JLabel) {
-	    // create the object with parameters so the jvm has a chance to reused objects in memory
-	    returnComponent = new JLabel(((JLabel) node.getUserObject()).getText(), ((JLabel) node.getUserObject()).getIcon(), JLabel.LEFT);
-	} else {
-	    return new JLabel();
-	}
-	if (selected) {
-	    returnComponent.setOpaque(true);
-	    returnComponent.setBackground(tree.getBackground().darker());
-	}
-	return returnComponent;
+        JLabel returnComponent = this;
+        DefaultMutableTreeNode node = (DefaultMutableTreeNode) value;
+        if (node.getUserObject() instanceof ArbilNode) {
+            ArbilNode arbilNode = (ArbilNode) node.getUserObject();
+            // create the object with parameters so the jvm has a chance to reused objects in memory
+//            returnComponent = new JLabel(arbilNode.toString(), arbilNode.getIcon(), JLabel.LEFT);
+            returnComponent.setText(arbilNode.toString());
+            returnComponent.setIcon(arbilNode.getIcon());
+            returnComponent.setHorizontalAlignment(JLabel.LEFT);
+            if (arbilNode instanceof ArbilDataNode) {
+                if (((ArbilDataNode) arbilNode).isContainerNode()) {
+                    returnComponent.setForeground(Color.DARK_GRAY);
+                    ((JLabel) returnComponent).setText("<html><u>" + arbilNode.toString() + "</u></html>");
+                }
+                if (/* !sel && */((ArbilDataNode) arbilNode).hasSchemaError) {
+                    returnComponent.setForeground(Color.RED);
+                }
+                if (/* !sel && */((ArbilDataNode) arbilNode).getNeedsSaveToDisk(true)) {
+                    returnComponent.setForeground(Color.BLUE);
+                }
+            }
+        } else if (node.getUserObject() instanceof JLabel) {
+            // create the object with parameters so the jvm has a chance to reused objects in memory
+            returnComponent = new JLabel(((JLabel) node.getUserObject()).getText(), ((JLabel) node.getUserObject()).getIcon(), JLabel.LEFT);
+        } else {
+            return new JLabel();
+        }
+        if (selected) {
+            returnComponent.setOpaque(true);
+            returnComponent.setBackground(tree.getBackground().darker());
+        }
+        return returnComponent;
     }
 }
