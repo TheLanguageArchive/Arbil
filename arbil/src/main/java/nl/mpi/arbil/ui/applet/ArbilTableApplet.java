76c1b0c0ab5a83a813495266ac7778aa2ee0dee8
Peter Withers <Peter.Withers@mpi.nl>
2012-06-11 12:00:51 +0000

    Made changes to address the Swing EDT issues causing Arbil to misbehave at times including the applet version. Made the ImageBoxRenderer injectable so that alternate image processors can be used. refs #1974 #1966 #1436

--- arbil/src/main/java/nl/mpi/arbil/ui/applet/ArbilTableApplet.java
+++ arbil/src/main/java/nl/mpi/arbil/ui/applet/ArbilTableApplet.java
@@ -2,10 +2,11 @@ package nl.mpi.arbil.ui.applet;
 
 import java.net.URI;
 import nl.mpi.arbil.ArbilDesktopInjector;
+import nl.mpi.arbil.data.DataNodeLoader;
+import nl.mpi.arbil.ui.ArbilSplitPanel;
 import nl.mpi.arbil.ui.ArbilTable;
 import nl.mpi.arbil.ui.ArbilTableModel;
-import nl.mpi.arbil.ui.ArbilSplitPanel;
-import nl.mpi.arbil.data.DataNodeLoader;
+import nl.mpi.arbil.ui.ImageBoxRenderer;
 import nl.mpi.arbil.util.BugCatcherManager;
 
 /*
@@ -19,69 +20,69 @@ public class ArbilTableApplet extends javax.swing.JApplet {
 
     @Override
     public void init() {
-	final ArbilDesktopInjector injector = new ArbilDesktopInjector();
-	// TODO: test if this suffices
-	injector.injectHandlers();
-	dataNodeLoader = injector.getDataNodeLoader();
-	try {
-	    java.awt.EventQueue.invokeAndWait(new Runnable() {
+        final ArbilDesktopInjector injector = new ArbilDesktopInjector();
+        // TODO: test if this suffices
+        injector.injectHandlers();
+        dataNodeLoader = injector.getDataNodeLoader();
+        try {
+            java.awt.EventQueue.invokeAndWait(new Runnable() {
 
-		public void run() {
-		    initComponents();
-		    addNodesToTable(getParameter("ImdiFileList"));
-		    addShowOnlyColumnsToTable(getParameter("ShowOnlyColumns"));
-		    addChildNodesToTable(getParameter("ChildNodeColumns"));
-		    addHighlightToTable(getParameter("HighlightText"));
-		}
-	    });
-	} catch (Exception ex) {
-	    BugCatcherManager.getBugCatcher().logError(ex);
-	}
+                public void run() {
+                    initComponents(injector.getImageBoxRenderer());
+                    addNodesToTable(getParameter("ImdiFileList"));
+                    addShowOnlyColumnsToTable(getParameter("ShowOnlyColumns"));
+                    addChildNodesToTable(getParameter("ChildNodeColumns"));
+                    addHighlightToTable(getParameter("HighlightText"));
+                }
+            });
+        } catch (Exception ex) {
+            BugCatcherManager.getBugCatcher().logError(ex);
+        }
     }
 
     private void addNodesToTable(String nodeURLsString) {
-	if (nodeURLsString != null) {
-	    for (String currentUrlString : nodeURLsString.split(",")) {
-		try {
-		    arbilTableModel.addSingleArbilDataNode(dataNodeLoader.getArbilDataNode(rootPane, new URI(currentUrlString)));
-		} catch (Exception ex) {
-		    BugCatcherManager.getBugCatcher().logError(ex);
-		}
-	    }
-	}
+        if (nodeURLsString != null) {
+            for (String currentUrlString : nodeURLsString.split(",")) {
+                try {
+                    arbilTableModel.addSingleArbilDataNode(dataNodeLoader.getArbilDataNode(rootPane, new URI(currentUrlString)));
+                } catch (Exception ex) {
+                    BugCatcherManager.getBugCatcher().logError(ex);
+                }
+            }
+        }
     }
 
     private void addShowOnlyColumnsToTable(String showColumnsString) {
-	if (showColumnsString != null && showColumnsString.trim().length() > 0) {
-	    for (String currentshowColumns : showColumnsString.split(",")) {
-		arbilTableModel.getFieldView().addShowOnlyColumn(currentshowColumns.trim());
-	    }
-	}
+        if (showColumnsString != null && showColumnsString.trim().length() > 0) {
+            for (String currentshowColumns : showColumnsString.split(",")) {
+                arbilTableModel.getFieldView().addShowOnlyColumn(currentshowColumns.trim());
+            }
+        }
     }
 
     private void addChildNodesToTable(String childNodesString) {
-	if (childNodesString != null && childNodesString.trim().length() > 0) {
-	    for (String currentChildNode : childNodesString.split(",")) {
-		arbilTableModel.addChildTypeToDisplay(currentChildNode.trim());
-	    }
-	}
+        if (childNodesString != null && childNodesString.trim().length() > 0) {
+            for (String currentChildNode : childNodesString.split(",")) {
+                arbilTableModel.addChildTypeToDisplay(currentChildNode.trim());
+            }
+        }
     }
 
     private void addHighlightToTable(String highlightableTextString) {
-	if (highlightableTextString != null && highlightableTextString.length() > 0) {
-	    for (String highlightText : highlightableTextString.split(",")) {
-		arbilTableModel.highlightMatchingText(highlightText);
-	    }
-	}
+        if (highlightableTextString != null && highlightableTextString.length() > 0) {
+            for (String highlightText : highlightableTextString.split(",")) {
+                arbilTableModel.highlightMatchingText(highlightText);
+            }
+        }
     }
 
-    private void initComponents() {
-	arbilTableModel = new ArbilTableModel();
-	ArbilTable arbilTable = new ArbilTable(arbilTableModel, tableTitle);
-	ArbilSplitPanel arbilSplitPanel = new ArbilSplitPanel(arbilTable);
-	arbilTableModel.hideContextMenuAndStatusBar = true;
-	arbilSplitPanel.setSplitDisplay();
-	getContentPane().add(arbilSplitPanel, java.awt.BorderLayout.CENTER);
+    private void initComponents(ImageBoxRenderer imageBoxRenderer) {
+        arbilTableModel = new ArbilTableModel(imageBoxRenderer);
+        ArbilTable arbilTable = new ArbilTable(arbilTableModel, tableTitle);
+        ArbilSplitPanel arbilSplitPanel = new ArbilSplitPanel(arbilTable);
+        arbilTableModel.hideContextMenuAndStatusBar = true;
+        arbilSplitPanel.setSplitDisplay();
+        getContentPane().add(arbilSplitPanel, java.awt.BorderLayout.CENTER);
     }
     private String tableTitle = "Arbil Table Demo";
     private ArbilTableModel arbilTableModel;
