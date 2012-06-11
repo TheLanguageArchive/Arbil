2a86b47ca07c0c4830ac06a2ce769657333cdefd
Peter Withers <Peter.Withers@mpi.nl>
2012-06-11 15:30:48 +0000

    Made changes to address the Swing EDT issues causing Arbil to misbehave at times including the applet version. Made the ImageBoxRenderer injectable so that alternate image processors can be used. Addressed performance issues with the tree and table renderer components by overriding: validate, invalidate, revalidate, repaint and firePropertyChange. refs #1974 #1966 #1436

--- arbil/src/main/java/nl/mpi/arbil/ui/ArbilTableCellRenderer.java
+++ arbil/src/main/java/nl/mpi/arbil/ui/ArbilTableCellRenderer.java
@@ -4,8 +4,6 @@
  */
 package nl.mpi.arbil.ui;
 
-import nl.mpi.arbil.data.ArbilField;
-import nl.mpi.arbil.data.ArbilDataNode;
 import java.awt.Color;
 import java.awt.Component;
 import java.awt.FontMetrics;
@@ -13,6 +11,8 @@ import javax.swing.Icon;
 import javax.swing.JTable;
 import javax.swing.table.DefaultTableCellRenderer;
 import nl.mpi.arbil.ArbilIcons;
+import nl.mpi.arbil.data.ArbilDataNode;
+import nl.mpi.arbil.data.ArbilField;
 import nl.mpi.arbil.data.ArbilTableCell;
 
 /**
@@ -21,139 +21,93 @@ import nl.mpi.arbil.data.ArbilTableCell;
  */
 public class ArbilTableCellRenderer extends DefaultTableCellRenderer {
 
-    private ArbilTableCell cellObject;
-    boolean isCellSelected = false;
-
-    public int getRequiredWidth(FontMetrics fontMetrics) {
-	String currentCellString = getText();
-	// Calculate width of text
-	int width = fontMetrics.stringWidth(currentCellString);
-	// ArbilField might have an icon
-	if (getCellContent() instanceof ArbilField) {
-	    Icon icon = ArbilIcons.getSingleInstance().getIconForField((ArbilField) getCellContent());
-	    // If there's an icon, add its width
-	    if (icon != null) {
-		width += icon.getIconWidth();
-	    }
-	}
-	return width;
+    public int getRequiredWidth(FontMetrics fontMetrics, ArbilTableCell cellObject) {
+        Object cellContent = getCellContent(cellObject);
+        String currentCellString = getText();
+        // Calculate width of text
+        int width = fontMetrics.stringWidth(currentCellString);
+        // ArbilField might have an icon
+        if (cellContent instanceof ArbilField) {
+            Icon icon = ArbilIcons.getSingleInstance().getIconForField((ArbilField) cellContent);
+            // If there's an icon, add its width
+            if (icon != null) {
+                width += icon.getIconWidth();
+            }
+        }
+        return width;
     }
 
     @Override
     public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus, int row, int column) {
-	// TODO: this might be a better place to set the backgound and text colours
-	isCellSelected = isSelected;
-	Component component = super.getTableCellRendererComponent(table, value, isSelected, hasFocus, row, column);
-
-	// ArbilField may have to be decorated with an icon
-	if (getCellContent() instanceof ArbilField) {
-	    Icon icon = ArbilIcons.getSingleInstance().getIconForField((ArbilField) getCellContent());
-	    if (icon != null) {
-		// An icon exists for this field, so wrap component with icon panel
-		return new ArbilIconCellPanel(component, icon);
-	    }
-	}
-	return component;
+        // TODO: this might be a better place to set the backgound and text colours
+//        Component component = super.getTableCellRendererComponent(table, value, isSelected, hasFocus, row, column);
+        ArbilTableCell cellObject = ((ArbilTableCell) value);
+        Object cellContent = getCellContent(cellObject);
+        // ArbilField may have to be decorated with an icon
+        if (cellContent instanceof ArbilField) {
+            Icon icon = ArbilIcons.getSingleInstance().getIconForField((ArbilField) cellContent);
+            if (icon != null) {
+                // An icon exists for this field, so wrap component with icon panel
+                return new ArbilIconCellPanel(this, icon);
+            }
+        }
+        setColours(cellContent, isSelected);
+        if (cellObject == null) {
+            setText("");
+        } else {
+            setText(cellObject.toString());
+        }
+        setIcon(getIcon(cellContent));
+        return this;
     }
 
     public ArbilTableCellRenderer() {
     }
 
-    // this is used when getting a string value for the object
-    public ArbilTableCellRenderer(ArbilTableCell cellObject) {
-	setValue(cellObject);
+    protected final void setColours(Object cellContent, boolean isSelected) {
+        if (cellContent instanceof ArbilField[]) {
+            // Multi-valued field
+            int greyTone = 150;
+            super.setForeground(new Color(greyTone, greyTone, greyTone));
+        }
+        if (!isSelected) {
+            if (cellContent instanceof ArbilFieldPlaceHolder || cellContent instanceof String && "".equals(cellContent)) {
+                // Field does not exist in node OR childs column and node has no children of this type
+                super.setBackground(new Color(230, 230, 230)/* Color.lightGray */);
+            } else if (cellContent instanceof ArbilField && ((ArbilField) cellContent).fieldNeedsSaveToDisk()) {
+                // Value has changed since last save
+                super.setForeground(Color.blue);
+            }
+        }
+        if (cellContent instanceof ArbilField && ((ArbilField) cellContent).isRequiredField() && ((ArbilField) cellContent).getFieldValue().length() == 0) {
+            super.setForeground(Color.RED);
+        } else if (cellContent instanceof ArbilField && !((ArbilField) cellContent).fieldValueValidates()) {
+            super.setForeground(Color.RED);
+        }
     }
 
-    @Override
-    protected final void setValue(Object value) {
-	super.setValue(value);
-	cellObject = ((ArbilTableCell) value);
-	if (getCellContent() instanceof ArbilField[]) {
-	    // Multi-valued field
-	    int greyTone = 150;
-	    super.setForeground(new Color(greyTone, greyTone, greyTone));
-	}
-	if (!isCellSelected) {
-	    if (getCellContent() instanceof ArbilFieldPlaceHolder || getCellContent() instanceof String && "".equals(getCellContent())) {
-		// Field does not exist in node OR childs column and node has no children of this type
-		super.setBackground(new Color(230, 230, 230)/*Color.lightGray*/);
-	    } else if (getCellContent() instanceof ArbilField && ((ArbilField) getCellContent()).fieldNeedsSaveToDisk()) {
-		// Value has changed since last save
-		super.setForeground(Color.blue);
-	    }
-	}
-    }
-
-    @Override
-    public Icon getIcon() {
-	if (getCellContent() instanceof ArbilDataNode) {
-	    return (((ArbilDataNode) getCellContent()).getIcon());
-	} else if (getCellContent() instanceof ArbilDataNode[]) {
-	    return (ArbilIcons.getSingleInstance().getIconForNode((ArbilDataNode[]) getCellContent()));
-	} else if (getCellContent() instanceof ArbilField[]) {
-	    return null;
+    public Icon getIcon(Object cellContent) {
+        if (cellContent instanceof ArbilDataNode) {
+            return (((ArbilDataNode) cellContent).getIcon());
+        } else if (cellContent instanceof ArbilDataNode[]) {
+            return (ArbilIcons.getSingleInstance().getIconForNode((ArbilDataNode[]) cellContent));
+        } else if (cellContent instanceof ArbilField[]) {
+            return null;
 //        } else if (cellObject instanceof ArbilField) {
 //            return ArbilIcons.getSingleInstance().getIconForVocabulary((ArbilField) cellObject);
-	} else {
-	    return (null);
-	}
-    }
-
-    @Override
-    public String getText() {
-	if (cellObject == null) {
-	    return super.toString();
-	} else {
-	    if (getCellContent() instanceof ArbilField && ((ArbilField) getCellContent()).isRequiredField() && ((ArbilField) getCellContent()).getFieldValue().length() == 0) {
-		super.setForeground(Color.RED);
-	    } else if (getCellContent() instanceof ArbilField && !((ArbilField) getCellContent()).fieldValueValidates()) {
-		super.setForeground(Color.RED);
-	    }
-	    return cellObject.toString();
-	}
-//	
-//	if (getCellObject() instanceof ArbilDataNode) {
-//	    return (((ArbilDataNode) getCellObject()).toString());
-//	} else if (getCellObject() instanceof ArbilDataNode[]) {
-//	    String cellText = "";
-//	    Arrays.sort((ArbilDataNode[]) getCellObject(), new Comparator() {
-//
-//		public int compare(Object o1, Object o2) {
-//		    String value1 = o1.toString();
-//		    String value2 = o2.toString();
-//		    return value1.compareToIgnoreCase(value2);
-//		}
-//	    });
-//	    boolean hasAddedValues = false;
-//	    for (ArbilDataNode currentArbilDataNode : (ArbilDataNode[]) getCellObject()) {
-//		cellText = cellText + "[" + currentArbilDataNode.toString() + "],";
-//		hasAddedValues = true;
-//	    }
-//	    if (hasAddedValues) {
-//		cellText = cellText.substring(0, cellText.length() - 1);
-//	    }
-//	    return (cellText);
-//	} else if (getCellObject() instanceof ArbilField[]) {
-//	    return "<multiple values>";
-//	} else if (getCellObject() instanceof ArbilField && ((ArbilField) getCellObject()).isRequiredField() && ((ArbilField) getCellObject()).toString().length() == 0) {
-//	    super.setForeground(Color.RED);
-//	    return "<required field>";
-//	} else if (getCellObject() instanceof ArbilField && !((ArbilField) getCellObject()).fieldValueValidates()) {
-//	    super.setForeground(Color.RED);
-//	    return super.getText();
-//	} else {
-//	    return super.getText();
-//	}
+        } else {
+            return (null);
+        }
     }
 
     /**
      * @return the cellObject
      */
-    private Object getCellContent() {
-	if (cellObject == null) {
-	    return null;
-	} else {
-	    return cellObject.getContent();
-	}
+    private Object getCellContent(ArbilTableCell cellObject) {
+        if (cellObject == null) {
+            return null;
+        } else {
+            return cellObject.getContent();
+        }
     }
 }
