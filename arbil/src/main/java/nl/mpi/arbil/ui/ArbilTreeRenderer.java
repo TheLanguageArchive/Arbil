23e0c6bacbea9a4f4382d098c9f9bc67c850f31b
Peter Withers <Peter.Withers@mpi.nl>
2012-06-12 15:11:24 +0000

    Made changes to address the Swing EDT issues causing Arbil to misbehave at times including the applet version. Made the ImageBoxRenderer injectable so that alternate image processors can be used. Addressed performance issues with the tree and table renderer components by overriding: validate, invalidate, revalidate, repaint and firePropertyChange. Modified the Arbil tree sorting and adding of nodes so that it runs inside the EDT, this was causing many issues such as flickering and icons/text being in the wrong place. refs #1974 #1966 #1436 #1980

--- arbil/src/main/java/nl/mpi/arbil/ui/ArbilTreeRenderer.java
+++ arbil/src/main/java/nl/mpi/arbil/ui/ArbilTreeRenderer.java
@@ -2,6 +2,7 @@ package nl.mpi.arbil.ui;
 
 import java.awt.Color;
 import java.awt.Component;
+import javax.swing.Icon;
 import javax.swing.JLabel;
 import javax.swing.JTree;
 import javax.swing.tree.DefaultMutableTreeNode;
@@ -19,42 +20,42 @@ public class ArbilTreeRenderer extends DefaultTreeCellRenderer {
 
     @Override
     synchronized public Component getTreeCellRendererComponent(JTree tree, Object value, boolean selected, boolean expanded, boolean leaf, int row, boolean hasFocus) {
-        JLabel returnComponent = this;
-        returnComponent.setForeground(tree.getForeground());
-        returnComponent.setBackground(tree.getBackground());
-        returnComponent.setIcon(null); // setting the icon to null to avoid a null pointer later in the set icon where it tries to testthe icon width/hight
-        returnComponent.setText(null);
+        DefaultTreeCellRenderer returnComponent = this;
+        returnComponent.setTextNonSelectionColor(tree.getForeground());
+        returnComponent.setBackgroundNonSelectionColor(tree.getBackground());
+//        returnComponent.setIcon(null); // setting the icon to null to avoid a null pointer later in the set icon where it tries to testthe icon width/hight
+//        returnComponent.setText(null);
+        String valueString = "";
+        Icon nodeIcon = null;
         DefaultMutableTreeNode node = (DefaultMutableTreeNode) value;
         if (node.getUserObject() instanceof ArbilNode) {
             ArbilNode arbilNode = (ArbilNode) node.getUserObject();
             // create the object with parameters so the jvm has a chance to reused objects in memory
 //            returnComponent = new JLabel(arbilNode.toString(), arbilNode.getIcon(), JLabel.LEFT);
-            returnComponent.setText(arbilNode.toString());
-            returnComponent.setIcon(arbilNode.getIcon());
+            valueString = (arbilNode.toString());
+            nodeIcon = (arbilNode.getIcon());
             returnComponent.setHorizontalAlignment(JLabel.LEFT);
             if (arbilNode instanceof ArbilDataNode) {
                 if (((ArbilDataNode) arbilNode).isContainerNode()) {
-                    returnComponent.setForeground(Color.DARK_GRAY);
-                    ((JLabel) returnComponent).setText("<html><u>" + arbilNode.toString() + "</u></html>");
-                }
-                if (/* !sel && */((ArbilDataNode) arbilNode).hasSchemaError) {
-                    returnComponent.setForeground(Color.RED);
+                    returnComponent.setTextNonSelectionColor(Color.DARK_GRAY);
+                    valueString = ("<u>" + arbilNode.toString() + "</u>");
                 }
                 if (/* !sel && */((ArbilDataNode) arbilNode).getNeedsSaveToDisk(true)) {
-                    returnComponent.setForeground(Color.BLUE);
+                    returnComponent.setTextNonSelectionColor(Color.BLUE);
+                }
+                if (/* !sel && */((ArbilDataNode) arbilNode).hasSchemaError) {
+                    returnComponent.setTextNonSelectionColor(Color.RED);
                 }
             }
         } else if (node.getUserObject() instanceof JLabel) {
             // create the object with parameters so the jvm has a chance to reused objects in memory
-            returnComponent.setText(((JLabel) node.getUserObject()).getText());
-            returnComponent.setIcon(((JLabel) node.getUserObject()).getIcon());
+            valueString = (((JLabel) node.getUserObject()).getText());
+            nodeIcon = ((JLabel) node.getUserObject()).getIcon();
         }
-        if (selected) {
-            returnComponent.setOpaque(true);
-            returnComponent.setBackground(tree.getBackground().darker());
-        } else {
-            returnComponent.setOpaque(false);
-        }
-        return returnComponent;
+//        returnComponent.setBackgroundSelectionColor(tree.getBackground().darker());
+        returnComponent.setClosedIcon(nodeIcon);
+        returnComponent.setOpenIcon(nodeIcon);
+        returnComponent.setLeafIcon(nodeIcon);
+        return super.getTreeCellRendererComponent(tree, "<html>" + valueString + "</html>", selected, expanded, leaf, row, hasFocus);
     }
 }
