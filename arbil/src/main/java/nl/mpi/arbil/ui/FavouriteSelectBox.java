f02fef812ba04f40f58e7bb867a3a218edafc0c6
Peter Withers <Peter.Withers@mpi.nl>
2012-08-01 11:54:37 +0000

    Added a file select to add resources in bulk, with the option to create a metadata file per resource file. refs #2209 Added option to automatically create corpus structure from directory structure (folders + files) refs #2198 Add the node icon to the add favourites menu. refs #2210 Related to and maybe resolves: refs #12 Add multiple Hopefully this will address the issue of new resources generates too many windows. refs #42

---
+++ arbil/src/main/java/nl/mpi/arbil/ui/FavouriteSelectBox.java
@@ -0,0 +1,56 @@
+package nl.mpi.arbil.ui;
+
+import java.awt.BorderLayout;
+import java.awt.Component;
+import javax.swing.BorderFactory;
+import javax.swing.BoxLayout;
+import javax.swing.DefaultListCellRenderer;
+import javax.swing.JCheckBox;
+import javax.swing.JLabel;
+import javax.swing.JList;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import nl.mpi.arbil.data.ArbilDataNode;
+import nl.mpi.arbil.templates.ArbilFavourites;
+
+/**
+ * Document : FavouriteSelectBox
+ * Created on : May 10, 2012, 5:28:11 PM
+ * Author : Peter Withers
+ */
+public class FavouriteSelectBox extends JPanel {
+
+    final ArbilDataNode targetNode;
+    final JCheckBox metadataFilePerResourceCheckBox;
+    final JCheckBox copyDirectoryStructureCheckBox;
+
+    public FavouriteSelectBox(ArbilDataNode targetNode) {
+        this.targetNode = targetNode;
+        this.setLayout(new BorderLayout());
+        this.setBorder(BorderFactory.createTitledBorder("Suitable Favourites"));
+        final JList favouriteList = new JList(ArbilFavourites.getSingleInstance().listFavouritesFor(targetNode));
+        favouriteList.setCellRenderer(new DefaultListCellRenderer() {
+
+            @Override
+            public Component getListCellRendererComponent(JList list, Object value, int index, boolean isSelected, boolean cellHasFocus) {
+                final JLabel listCellRendererComponent = (JLabel) super.getListCellRendererComponent(list, value, index, isSelected, cellHasFocus);
+                String labelString = value.toString();
+                if (labelString.length() > 50) {
+                    labelString = labelString.substring(0, 48) + "...";
+                }
+                listCellRendererComponent.setText(labelString);
+                listCellRendererComponent.setIcon(((ArbilDataNode) value).getIcon());
+                return listCellRendererComponent;
+            }
+        });
+        this.add(new JScrollPane(favouriteList), BorderLayout.CENTER);
+//        this.add(new JLabel(targetNode.toString(), targetNode.getIcon(), JLabel.LEFT), BorderLayout.PAGE_END);
+        JPanel optionsPanel = new JPanel();
+        optionsPanel.setLayout(new BoxLayout(optionsPanel, BoxLayout.PAGE_AXIS));
+        metadataFilePerResourceCheckBox = new JCheckBox("Metadata for each resource", false);
+        optionsPanel.add(metadataFilePerResourceCheckBox);
+        copyDirectoryStructureCheckBox = new JCheckBox("Copy directory structure", false);
+        optionsPanel.add(copyDirectoryStructureCheckBox);
+        this.add(optionsPanel, BorderLayout.PAGE_END);
+    }
+}
