cb270081d4d8f087f63d9556842ef54e23ec5a04
Peter Withers <Peter.Withers@mpi.nl>
2012-09-12 11:53:48 +0000

    Added the first search and faceted tree plugin into Arbil. Added the plugins menu to Arbil. Added an Arbil Table to the faceted tree test panel. refs #2272 refs #2233

--- arbil-wicket/src/main/java/nl/mpi/arbil/wicket/ArbilWicketDataNodeLoaderProxy.java
+++ arbil-wicket/src/main/java/nl/mpi/arbil/wicket/ArbilWicketDataNodeLoaderProxy.java
@@ -3,85 +3,96 @@ package nl.mpi.arbil.wicket;
 import java.net.URI;
 import nl.mpi.arbil.data.ArbilDataNode;
 import nl.mpi.arbil.data.DataNodeLoader;
+import nl.mpi.arbil.plugin.PluginArbilDataNode;
 
 /**
- * Proxy for the DataNodeLoader that is contained in the session that makes
- * the request. To be injected into Arbil core classes.
+ * Proxy for the DataNodeLoader that is contained in the session that makes the
+ * request. To be injected into Arbil core classes.
+ *
  * @author Twan Goosen <twan.goosen@mpi.nl>
  */
-public class ArbilWicketDataNodeLoaderProxy implements DataNodeLoader{
+public class ArbilWicketDataNodeLoaderProxy implements DataNodeLoader {
 
     private DataNodeLoader getDataNodeLoader() {
-	// Data node loader is retrieved from the session - each session has its own.
-	return ArbilWicketSession.get().getDataNodeLoader();
+        // Data node loader is retrieved from the session - each session has its own.
+        return ArbilWicketSession.get().getDataNodeLoader();
     }
-    
+
     public void addNodeNeedingSave(ArbilDataNode nodeToSave) {
-	getDataNodeLoader().addNodeNeedingSave(nodeToSave);
+        getDataNodeLoader().addNodeNeedingSave(nodeToSave);
     }
 
     public ArbilDataNode getArbilDataNode(Object registeringObject, URI localUri) {
-	return getDataNodeLoader().getArbilDataNode(registeringObject, localUri);
+        return getDataNodeLoader().getArbilDataNode(registeringObject, localUri);
     }
 
     public ArbilDataNode getArbilDataNodeOnlyIfLoaded(URI arbilUri) {
-	return getDataNodeLoader().getArbilDataNodeOnlyIfLoaded(arbilUri);
+        return getDataNodeLoader().getArbilDataNodeOnlyIfLoaded(arbilUri);
     }
 
     public ArbilDataNode getArbilDataNodeWithoutLoading(URI localUri) {
-	return getDataNodeLoader().getArbilDataNodeWithoutLoading(localUri);
+        return getDataNodeLoader().getArbilDataNodeWithoutLoading(localUri);
+    }
+
+    /**
+     * this is a transitional method and will be replaced when the time comes
+     *
+     * @return the ArbilDataNode that was obtained via getArbilDataNode and cast
+     * to PluginArbilDataNode
+     */
+    public PluginArbilDataNode getPluginArbilDataNode(Object registeringObject, URI localUri) {
+        return (PluginArbilDataNode) getArbilDataNode(registeringObject, localUri);
     }
 
     public ArbilDataNode[] getNodesNeedSave() {
-	return getDataNodeLoader().getNodesNeedSave();
+        return getDataNodeLoader().getNodesNeedSave();
     }
 
     public boolean isSchemaCheckLocalFiles() {
-	return getDataNodeLoader().isSchemaCheckLocalFiles();
+        return getDataNodeLoader().isSchemaCheckLocalFiles();
     }
 
     public boolean nodesNeedSave() {
-	return getDataNodeLoader().nodesNeedSave();
+        return getDataNodeLoader().nodesNeedSave();
     }
 
     public void removeNodesNeedingSave(ArbilDataNode savedNode) {
-	getDataNodeLoader().removeNodesNeedingSave(savedNode);
+        getDataNodeLoader().removeNodesNeedingSave(savedNode);
     }
 
     public void requestReload(ArbilDataNode currentDataNode) {
-	getDataNodeLoader().requestReload(currentDataNode);
+        getDataNodeLoader().requestReload(currentDataNode);
     }
 
     public void requestReloadAllNodes() {
-	getDataNodeLoader().requestReloadAllNodes();
+        getDataNodeLoader().requestReloadAllNodes();
     }
 
     public void requestReloadOnlyIfLoaded(URI arbilUri) {
-	getDataNodeLoader().requestReloadOnlyIfLoaded(arbilUri);
+        getDataNodeLoader().requestReloadOnlyIfLoaded(arbilUri);
     }
 
     public void saveNodesNeedingSave(boolean updateIcons) {
-	getDataNodeLoader().saveNodesNeedingSave(updateIcons);
+        getDataNodeLoader().saveNodesNeedingSave(updateIcons);
     }
 
     public void setSchemaCheckLocalFiles(boolean schemaCheckLocalFiles) {
-	getDataNodeLoader().setSchemaCheckLocalFiles(schemaCheckLocalFiles);
+        getDataNodeLoader().setSchemaCheckLocalFiles(schemaCheckLocalFiles);
     }
 
     public void startLoaderThreads() {
-	getDataNodeLoader().startLoaderThreads();
+        getDataNodeLoader().startLoaderThreads();
     }
 
     public void stopLoaderThreads() {
-	getDataNodeLoader().stopLoaderThreads();
+        getDataNodeLoader().stopLoaderThreads();
     }
 
     public ArbilDataNode createNewDataNode(URI uri) {
-	return getDataNodeLoader().createNewDataNode(uri);
+        return getDataNodeLoader().createNewDataNode(uri);
     }
 
     public void requestShallowReload(ArbilDataNode adn) {
-	getDataNodeLoader().requestShallowReload(adn);
+        getDataNodeLoader().requestShallowReload(adn);
     }
-    
 }
