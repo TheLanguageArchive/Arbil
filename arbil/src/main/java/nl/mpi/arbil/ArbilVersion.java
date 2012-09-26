7d50567daef2cfc1d8508542975b743b194412b9
Peter Withers <Peter.Withers@mpi.nl>
2012-09-26 09:56:14 +0000

    Added a unit test that restricts the risk of human error in the maven build process and matching the correct build number. refs #2339

--- arbil/src/main/java/nl/mpi/arbil/ArbilVersion.java
+++ arbil/src/main/java/nl/mpi/arbil/ArbilVersion.java
@@ -7,19 +7,20 @@ import nl.mpi.arbil.util.ApplicationVersion;
 public class ArbilVersion extends ApplicationVersion {
 
     public ArbilVersion() {
-	Properties properties = new Properties();
-	try {
-	    properties.load(getClass().getResourceAsStream("/version.properties"));
-	    applicationTitle = properties.getProperty("application.title");
-	    applicationIconName = properties.getProperty("application.iconName");
-	    currentMajor = properties.getProperty("application.majorVersion");
-	    currentMinor = properties.getProperty("application.minorVersion");
-	    currentRevision = properties.getProperty("application.revision");
-	    lastCommitDate = properties.getProperty("application.lastCommitDate");
-	    compileDate = properties.getProperty("application.compileDate");
-	    currentVersionFile = properties.getProperty("application.currentVersionFile");
-	} catch (IOException ex) {
-	    System.err.println("Version properties could not be read!");
-	}
+        Properties properties = new Properties();
+        try {
+            properties.load(getClass().getResourceAsStream("/version.properties"));
+            applicationTitle = properties.getProperty("application.title");
+            applicationIconName = properties.getProperty("application.iconName");
+            currentMajor = properties.getProperty("application.majorVersion");
+            currentMinor = properties.getProperty("application.minorVersion");
+            currentRevision = properties.getProperty("application.revision");
+            lastCommitDate = properties.getProperty("application.lastCommitDate");
+            compileDate = properties.getProperty("application.compileDate");
+            currentVersionFile = properties.getProperty("application.currentVersionFile");
+            artifactVersion = properties.getProperty("application.projectVersion");
+        } catch (IOException ex) {
+            System.err.println("Version properties could not be read!");
+        }
     }
 }
