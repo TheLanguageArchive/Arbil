57147969ba2146e43eded8e69dbbc1c85a0c82aa
Twan Goosen <Twan.Goosen@mpi.nl>
2012-09-28 14:38:02 +0000

    Arbil desktop is now depending on the arbil-help project. All the help resources have been removed from the desktop project.

--- arbil/src/main/java/nl/mpi/arbil/ui/ArbilHelp.java
+++ arbil/src/main/java/nl/mpi/arbil/ui/ArbilHelp.java
@@ -3,6 +3,7 @@ package nl.mpi.arbil.ui;
 import java.io.IOException;
 import java.net.URL;
 import java.util.Arrays;
+import nl.mpi.arbil.help.ArbilHelpResources;
 import org.xml.sax.SAXException;
 
 /**
@@ -10,14 +11,15 @@ import org.xml.sax.SAXException;
  */
 public class ArbilHelp extends HelpViewerPanel {
 
+    // IMDI resource set
     public final static String IMDI_HELPSET = "IMDI";
-    public static final String IMDI_URL_PATTERN = "arbil-imdi";
+    public static final String IMDI_URL_PATTERN = ArbilHelpResources.IMDI_URL_PATTERN;
+    private final static HelpResourceSet IMDI_HELP_SET = new HelpResourceSet(IMDI_HELPSET, ArbilHelpResources.class, ArbilHelpResources.IMDI_HELP_RESOURCE_BASE, ArbilHelpResources.IMDI_HELP_INDEX_XML);
+    // CMDI resource set
     public final static String CMDI_HELPSET = "CMDI";
-    public static final String CMDI_URL_PATTERN = "arbil-cmdi";
-    private final static String IMDI_HELP_RESOURCE_BASE = "/nl/mpi/arbil/resources/html/help/arbil-imdi/";
-    private final static HelpResourceSet IMDI_HELP_SET = new HelpResourceSet(IMDI_HELPSET, ArbilHelp.class, IMDI_HELP_RESOURCE_BASE, IMDI_HELP_RESOURCE_BASE + "arbil-imdi.xml");
-    private final static String CMDI_HELP_RESOURCE_BASE = "/nl/mpi/arbil/resources/html/help/arbil-cmdi/";
-    private final static HelpResourceSet CMDI_HELP_SET = new HelpResourceSet(CMDI_HELPSET, ArbilHelp.class, CMDI_HELP_RESOURCE_BASE, CMDI_HELP_RESOURCE_BASE + "arbil-cmdi.xml");
+    public static final String CMDI_URL_PATTERN = ArbilHelpResources.CMDI_URL_PATTERN;
+    private final static HelpResourceSet CMDI_HELP_SET = new HelpResourceSet(CMDI_HELPSET, ArbilHelpResources.class, ArbilHelpResources.CMDI_HELP_RESOURCE_BASE, ArbilHelpResources.CMDI_HELP_INDEX_XML);
+    // Singleton
     private static ArbilHelp singleInstance = null;
 
     public static synchronized ArbilHelp getArbilHelpInstance() throws IOException, SAXException {
