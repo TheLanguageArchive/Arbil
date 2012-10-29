87fe1e1ab28b5c9b023772975d4fa4dd3bcc88c1
Twan Goosen <Twan.Goosen@mpi.nl>
2012-10-29 12:55:26 +0000

    Added 'close' action to find/replace panel, and create key binding between this and ESC. Also made separate internal objects for action and focus listeners (class no longer implements these itself) Refs #2168

--- arbil/src/main/java/nl/mpi/arbil/ui/FindReplacePanel.java
+++ arbil/src/main/java/nl/mpi/arbil/ui/FindReplacePanel.java
@@ -8,54 +8,60 @@
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
  */
 package nl.mpi.arbil.ui;
 
-import nl.mpi.arbil.data.ArbilField;
 import java.awt.Color;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.FocusEvent;
 import java.awt.event.FocusListener;
+import java.awt.event.KeyEvent;
+import javax.swing.AbstractAction;
+import javax.swing.Action;
 import javax.swing.JButton;
 import javax.swing.JCheckBox;
+import javax.swing.JComponent;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
+import javax.swing.KeyStroke;
+import nl.mpi.arbil.data.ArbilField;
 
 /**
- *  Document   : FindReplacePanel
- *  Created on : Jun 21, 2010, 10:13:22 AM
- *  Author     : Peter Withers
+ * Document : FindReplacePanel
+ * Created on : Jun 21, 2010, 10:13:22 AM
+ * Author : Peter Withers
  */
-public class FindReplacePanel extends JPanel implements ActionListener, FocusListener {
+public class FindReplacePanel extends JPanel {
 
-    String defaultFindText = "<enter find text>";
-    String defaultReplaceText = "<enter replacement text>";
-    ArbilSplitPanel splitPanel;
-    JButton closeButton;
-    JTextField seachField;
-    JTextField replaceField;
-    JCheckBox useJavaRegExCheckBox;
-    JCheckBox replaceAllCheckBox;
-    JButton searchButton;
-    JButton replaceButton;
+    private final static String defaultFindText = "<enter find text>";
+    private final static String defaultReplaceText = "<enter replacement text>";
+    private final ArbilSplitPanel splitPanel;
+    private final JButton closeButton;
+    private final JTextField searchField;
+    private final JTextField replaceField;
+    private final JCheckBox useJavaRegExCheckBox;
+    //private final JCheckBox replaceAllCheckBox;
+    private final JButton searchButton;
+    private final JButton replaceButton;
 
-    public FindReplacePanel(ArbilSplitPanel splitPanelLocal) {	
+    public FindReplacePanel(ArbilSplitPanel splitPanelLocal) {
 	splitPanel = splitPanelLocal;
-	seachField = new JTextField(defaultFindText);
+	searchField = new JTextField(defaultFindText);
 	replaceField = new JTextField(defaultReplaceText);
-	seachField.setName(defaultFindText);
+	searchField.setName(defaultFindText);
 	replaceField.setName(defaultReplaceText);
-	seachField.setForeground(Color.gray);
+	searchField.setForeground(Color.gray);
 	replaceField.setForeground(Color.gray);
 	useJavaRegExCheckBox = new JCheckBox("Regular Expression");
-	replaceAllCheckBox = new JCheckBox("Replace All");
+	//replaceAllCheckBox = new JCheckBox("Replace All");
 	useJavaRegExCheckBox.setToolTipText("Use Java Style Regular Expressions (eg: "
 		+ ". any "
 		+ "\\d digit "
@@ -66,18 +72,18 @@ public class FindReplacePanel extends JPanel implements ActionListener, FocusLis
 		+ "^ line start "
 		+ "$ line end)");
 
+	closeButton = new JButton(closeAction);
 	searchButton = new JButton("Find Next");
 	replaceButton = new JButton("Replace Selected");
-	closeButton = new JButton("Close");
-	seachField.addActionListener(this);
-	replaceField.addActionListener(this);
+	searchField.addActionListener(findActionListener);
+	replaceField.addActionListener(findActionListener);
 //        useJavaRegExCheckBox.addActionListener(this);
-	searchButton.addActionListener(this);
-	replaceButton.addActionListener(this);
-	closeButton.addActionListener(this);
-	seachField.addFocusListener(this);
-	replaceField.addFocusListener(this);
-	this.add(seachField);
+	searchButton.addActionListener(findActionListener);
+	replaceButton.addActionListener(findActionListener);
+	searchField.addFocusListener(findReplaceFocusListener);
+	replaceField.addFocusListener(findReplaceFocusListener);
+	this.add(new JLabel("Find:"));
+	this.add(searchField);
 	this.add(replaceField);
 //        this.add(useJavaRegExCheckBox);
 //        this.add(replaceAllCheckBox);
@@ -85,96 +91,107 @@ public class FindReplacePanel extends JPanel implements ActionListener, FocusLis
 	this.add(replaceButton);
 	this.add(closeButton);
 
+	getActionMap().put("close", closeAction);
+	getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW).put(KeyStroke.getKeyStroke(KeyEvent.VK_ESCAPE, 0), "close");
     }
-
-    public void actionPerformed(ActionEvent actionEvent) {
-	System.out.println(actionEvent.toString());
-	if (actionEvent.getSource().equals(closeButton)) {
+    final private Action closeAction = new AbstractAction("Close") {
+	public void actionPerformed(ActionEvent e) {
 	    splitPanel.showSearchPane();
-	    return;
 	}
-	String searchString = "";
-	if (!seachField.getText().equals(defaultFindText)) {
-	    searchString = seachField.getText();
-	}
-	String replaceString = null;
-	if (actionEvent.getSource().equals(replaceButton)) {
-	    replaceString = "";
-	    if (!replaceField.getText().equals(defaultReplaceText)) {
-		replaceString = replaceField.getText();
+    };
+    /**
+     * Action listener for search and replace buttons and fields
+     */
+    final private ActionListener findActionListener = new ActionListener() {
+	public void actionPerformed(ActionEvent actionEvent) {
+	    System.out.println(actionEvent.toString());
+	    String searchString = "";
+	    if (!searchField.getText().equals(defaultFindText)) {
+		searchString = searchField.getText();
 	    }
-	}
-
-	if (actionEvent.getSource().equals(replaceButton)) {
-	    for (ArbilField currentField : splitPanel.arbilTable.getSelectedFields()) {
-		if (currentField.getParentDataNode().isEditable() && currentField.getFieldValue().contains(searchString)) {
-		    String currentFieldString = currentField.getFieldValue();
-		    if (currentFieldString.contains(searchString)) {
-			currentFieldString = currentFieldString.replace(searchString, replaceString);
-			currentField.setFieldValue(currentFieldString, true, false);
-		    }
+	    String replaceString = null;
+	    if (actionEvent.getSource().equals(replaceButton)) {
+		replaceString = "";
+		if (!replaceField.getText().equals(defaultReplaceText)) {
+		    replaceString = replaceField.getText();
 		}
 	    }
-	    // in the case where a user has selected a number of rows to replace in we do not want to clear their selection
-	    return;
-	}
 
-	boolean replaceAll = (actionEvent.getSource().equals(replaceButton) && replaceAllCheckBox.isSelected());
-
-	int startColumn = splitPanel.arbilTable.getSelectedColumn();
-	int startRow = splitPanel.arbilTable.getSelectedRow();
-	boolean firstTime = true;
-	if (startColumn < 0) {
-	    firstTime = false;
-	    startColumn = 0;
-	}
-	if (startRow < 0) {
-	    firstTime = false;
-	    startRow = 0;
-	}
-	for (int currentRow = startRow; currentRow < splitPanel.arbilTable.getRowCount(); currentRow++) {
-	    for (int currentColumn = startColumn; currentColumn < splitPanel.arbilTable.getColumnCount(); currentColumn++) {
-		if (firstTime) {
-		    firstTime = false;
-		} else {
-		    Object currentCellValue = splitPanel.arbilTable.getTableCellContentAt(currentRow, currentColumn);
-		    if (currentCellValue instanceof ArbilField) {
-			currentCellValue = new ArbilField[]{(ArbilField) currentCellValue};
+	    if (actionEvent.getSource().equals(replaceButton)) {
+		for (ArbilField currentField : splitPanel.arbilTable.getSelectedFields()) {
+		    if (currentField.getParentDataNode().isEditable() && currentField.getFieldValue().contains(searchString)) {
+			String currentFieldString = currentField.getFieldValue();
+			if (currentFieldString.contains(searchString)) {
+			    currentFieldString = currentFieldString.replace(searchString, replaceString);
+			    currentField.setFieldValue(currentFieldString, true, false);
+			}
 		    }
-		    if (currentCellValue instanceof ArbilField[]) {
-			for (ArbilField currentField : (ArbilField[]) currentCellValue) {
-			    if (currentField.getFieldValue().contains(searchString)) {
-				splitPanel.arbilTable.setCellSelectionEnabled(true);
-				splitPanel.arbilTable.changeSelection(currentRow, currentColumn, false, false);
+		}
+		// in the case where a user has selected a number of rows to replace in we do not want to clear their selection
+		return;
+	    }
+
+	    //boolean replaceAll = (actionEvent.getSource().equals(replaceButton) && replaceAllCheckBox.isSelected());
+
+	    int startColumn = splitPanel.arbilTable.getSelectedColumn();
+	    int startRow = splitPanel.arbilTable.getSelectedRow();
+	    boolean firstTime = true;
+	    if (startColumn < 0) {
+		firstTime = false;
+		startColumn = 0;
+	    }
+	    if (startRow < 0) {
+		firstTime = false;
+		startRow = 0;
+	    }
+	    for (int currentRow = startRow; currentRow < splitPanel.arbilTable.getRowCount(); currentRow++) {
+		for (int currentColumn = startColumn; currentColumn < splitPanel.arbilTable.getColumnCount(); currentColumn++) {
+		    if (firstTime) {
+			firstTime = false;
+		    } else {
+			Object currentCellValue = splitPanel.arbilTable.getTableCellContentAt(currentRow, currentColumn);
+			if (currentCellValue instanceof ArbilField) {
+			    currentCellValue = new ArbilField[]{(ArbilField) currentCellValue};
+			}
+			if (currentCellValue instanceof ArbilField[]) {
+			    for (ArbilField currentField : (ArbilField[]) currentCellValue) {
+				if (currentField.getFieldValue().contains(searchString)) {
+				    splitPanel.arbilTable.setCellSelectionEnabled(true);
+				    splitPanel.arbilTable.changeSelection(currentRow, currentColumn, false, false);
 //                                if (!replaceAll) {
-				return;
+				    return;
 //                                }
+				}
 			    }
 			}
 		    }
 		}
+		startColumn = 0;
 	    }
-	    startColumn = 0;
 	}
-    }
+    };
+    /**
+     * Focus listener for search and replace fields
+     */
+    final private FocusListener findReplaceFocusListener = new FocusListener() {
+	public void focusGained(FocusEvent e) {
+	    JTextField currentTextField = (JTextField) e.getSource();
+	    if (currentTextField.getText().equals(defaultFindText) || currentTextField.getText().equals(defaultReplaceText)) {
+		currentTextField.setText("");
+	    }
+	    currentTextField.setForeground(Color.black);
+	}
 
-    public void focusGained(FocusEvent e) {
-	JTextField currentTextField = (JTextField) e.getSource();
-	if (currentTextField.getText().equals(defaultFindText) || currentTextField.getText().equals(defaultReplaceText)) {
-	    currentTextField.setText("");
+	public void focusLost(FocusEvent e) {
+	    JTextField currentTextField = (JTextField) e.getSource();
+	    if (currentTextField.getText().length() < 1) {// || currentTextField.getText().equals(currentTextField.getName())) {
+		currentTextField.setText(currentTextField.getName());
+		currentTextField.setForeground(Color.gray);
+	    }
 	}
-	currentTextField.setForeground(Color.black);
-    }
+    };
 
-    public void focusLost(FocusEvent e) {
-	JTextField currentTextField = (JTextField) e.getSource();
-	if (currentTextField.getText().length() < 1) {// || currentTextField.getText().equals(currentTextField.getName())) {
-	    currentTextField.setText(currentTextField.getName());
-	    currentTextField.setForeground(Color.gray);
-	}
-    }
-    
-    public void requestFocusOnSearchField(){
-	seachField.requestFocus();
+    public void requestFocusOnSearchField() {
+	searchField.requestFocus();
     }
 }
