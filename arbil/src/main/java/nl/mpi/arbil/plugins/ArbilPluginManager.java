7e0b22fc1088d16b1b2629cf0eacd75f9cd16222
Twan Goosen <Twan.Goosen@mpi.nl>
2013-02-04 15:15:00 +0000

    Minor change to retrieval of plugins from directories: use set to prevent duplicates
    Refs #2725

--- arbil/src/main/java/nl/mpi/arbil/plugins/ArbilPluginManager.java
+++ arbil/src/main/java/nl/mpi/arbil/plugins/ArbilPluginManager.java
@@ -23,8 +23,10 @@ import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 import nl.mpi.arbil.plugin.ActivatablePlugin;
 import nl.mpi.arbil.plugin.ArbilWindowPlugin;
 import nl.mpi.arbil.plugin.PluginArbilDataNodeLoader;
@@ -98,13 +100,15 @@ public class ArbilPluginManager implements PluginManager {
     }
 
     public List<URL> getPluginsFromDirectoriesAndPluginsList() {
-	final List<URL> plugins = getPluginsFromDirectories();
-	plugins.addAll(getPluginsFromPluginList());
-	return plugins;
+	// Using a set to strip out duplicates
+	final Collection<URL> pluginsSet = new HashSet<URL>();
+	pluginsSet.addAll(getPluginsFromDirectories());
+	pluginsSet.addAll(getPluginsFromPluginList());
+	return new ArrayList<URL>(pluginsSet);
     }
 
-    public List<URL> getPluginsFromDirectories() {
-	final List<URL> pluginURLs = new ArrayList<URL>();
+    public Collection<URL> getPluginsFromDirectories() {
+	final Collection<URL> pluginURLs = new HashSet<URL>();
 
 	final FileFilter jarFileFilter = new FileFilter() {
 	    public boolean accept(File pathname) {
@@ -140,8 +144,8 @@ public class ArbilPluginManager implements PluginManager {
 	return pluginURLs;
     }
 
-    public List<URL> getPluginsFromPluginList() {
-	List<URL> pluginUlrs = new ArrayList<URL>();
+    public Collection<URL> getPluginsFromPluginList() {
+	final Collection<URL> pluginUlrs = new HashSet<URL>();
 	String errorMessages = "";
 	try {
 	    final String[] pluginStringArray = arbilSessionStorage.loadStringArray("PluginList");
