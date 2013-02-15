cd3271b0efa4dd3adfc6b28b89ee1e5ca16dcbfd
Peter Withers <Peter.Withers@mpi.nl>
2013-02-15 15:22:05 +0000

    Added a history field abstract class into commons so that the undo buffer can work while the the core interfaces remain agnostic.

--- arbil/src/main/java/nl/mpi/arbil/data/ArbilField.java
+++ arbil/src/main/java/nl/mpi/arbil/data/ArbilField.java
@@ -1,19 +1,19 @@
 /**
  * Copyright (C) 2013 Max Planck Institute for Psycholinguistics
  *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License
- * as published by the Free Software Foundation; either version 2
- * of the License, or (at your option) any later version.
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 2 of the License, or (at your option) any later
+ * version.
  *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
  *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ * You should have received a copy of the GNU General Public License along with
+ * this program; if not, write to the Free Software Foundation, Inc., 59 Temple
+ * Place - Suite 330, Boston, MA 02111-1307, USA.
  */
 package nl.mpi.arbil.data;
 
@@ -26,14 +26,14 @@ import java.util.Map;
 import nl.mpi.arbil.data.metadatafile.MetadataReader;
 import nl.mpi.arbil.userstorage.SessionStorage;
 import nl.mpi.arbilcommons.journal.ArbilJournal;
-import nl.mpi.flap.model.DataField;
+import nl.mpi.arbilcommons.model.HistoryField;
 
 /**
  * Document : ArbilField Created on : Wed Dec 03 13:29:30 CET 2008
  *
  * @author Peter.Withers@mpi.nl
  */
-public class ArbilField extends DataField implements Serializable {
+public class ArbilField extends HistoryField implements Serializable {
 
     private transient ArbilDataNode parentDataNode;
     private URI parentDataNodeURI;
