34bcd1e4d6dd895c0212318d8ec14db28f61b26e
Twan Goosen <Twan.Goosen@mpi.nl>
2013-09-11 13:13:11 +0000

    ArbilMain: moved activaiton of localisation selector out of constructor into call from run() method Refs #2828

--- arbil/src/main/java/nl/mpi/arbil/ArbilMain.java
+++ arbil/src/main/java/nl/mpi/arbil/ArbilMain.java
@@ -68,157 +68,161 @@ public class ArbilMain extends javax.swing.JFrame {
      * @param args the command line arguments
      */
     public static void main(String args[]) {
-        final ArbilVersion arbilVersion = new ArbilVersion();
-        final ArbilLogConfigurer logConfigurer = new ArbilLogConfigurer(arbilVersion, "arbil-log-");
-        // See if a logging configuration has been specified manually
-        if (System.getProperty("java.util.logging.config.file") == null) {
-            // No logging configured, use built in initial logging properties
-            logConfigurer.configureLoggingFromResource(ArbilMain.class, "/logging-initial.properties");
-        }
+	final ArbilVersion arbilVersion = new ArbilVersion();
+	final ArbilLogConfigurer logConfigurer = new ArbilLogConfigurer(arbilVersion, "arbil-log-");
+	// See if a logging configuration has been specified manually
+	if (System.getProperty("java.util.logging.config.file") == null) {
+	    // No logging configured, use built in initial logging properties
+	    logConfigurer.configureLoggingFromResource(ArbilMain.class, "/logging-initial.properties");
+	}
 
-        logger.info("Starting Arbil");
+	logger.info("Starting Arbil");
 
-        System.setProperty("sun.swing.enableImprovedDragGesture", "true");
-        System.setProperty("apple.awt.graphics.UseQuartz", "true");
-        System.setProperty("apple.laf.useScreenMenuBar", "true");
-        java.awt.EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                final ApplicationVersionManager versionManager = new ApplicationVersionManager(arbilVersion);
-                try {
-                    new ArbilMain(versionManager, logConfigurer).run();
-                } catch (Exception ex) {
-                    BugCatcherManager.getBugCatcher().logError(ex);
-                }
-            }
-        });
+	System.setProperty("sun.swing.enableImprovedDragGesture", "true");
+	System.setProperty("apple.awt.graphics.UseQuartz", "true");
+	System.setProperty("apple.laf.useScreenMenuBar", "true");
+	java.awt.EventQueue.invokeLater(new Runnable() {
+	    public void run() {
+		final ApplicationVersionManager versionManager = new ApplicationVersionManager(arbilVersion);
+		try {
+		    new ArbilMain(versionManager, logConfigurer).run();
+		} catch (Exception ex) {
+		    BugCatcherManager.getBugCatcher().logError(ex);
+		}
+	    }
+	});
     }
 
     public ArbilMain(ApplicationVersionManager versionManager, ArbilLogConfigurer logConfigurer) {
-        this.versionManager = versionManager;
-        this.logConfigurer = logConfigurer;
+	this.versionManager = versionManager;
+	this.logConfigurer = logConfigurer;
 
-        final ArbilDesktopInjector injector = new ArbilDesktopInjector();
-        injector.injectHandlers(versionManager, logConfigurer);
+	final ArbilDesktopInjector injector = new ArbilDesktopInjector();
+	injector.injectHandlers(versionManager, logConfigurer);
 
-        this.treeHelper = injector.getTreeHelper();
-        this.treeController = injector.getTreeController();
-        this.windowManager = injector.getWindowManager();
-        this.mimeHashQueue = injector.getMimeHashQueue();
-        this.tableController = injector.getTableController();
-        this.sessionStorage = injector.getSessionStorage();
-        final String availableLanguages = ResourceBundle.getBundle("nl/mpi/arbil/localisation/AvailableLanguages").getString("LANGUAGE CODES");
-        final LocalisationSelector localisationSelector = new LocalisationSelector(sessionStorage, availableLanguages.split(","));
-        if (!localisationSelector.hasSavedLocal()) {
-            final String please_select_your_preferred_language = menus.getString("PLEASE SELECT YOUR PREFERRED LANGUAGE");
-            final String language_Selection = menus.getString("LANGUAGE SELECTION");
-            final String system_Default = menus.getString("SYSTEM DEFAULT");
-            localisationSelector.askUser(null, ArbilIcons.getSingleInstance().linorgIcon, please_select_your_preferred_language, language_Selection, system_Default);
-        }
-        localisationSelector.setLanguageFromSaved();
+	this.treeHelper = injector.getTreeHelper();
+	this.treeController = injector.getTreeController();
+	this.windowManager = injector.getWindowManager();
+	this.mimeHashQueue = injector.getMimeHashQueue();
+	this.tableController = injector.getTableController();
+	this.sessionStorage = injector.getSessionStorage();
     }
 
     public void run() {
-        initApplication();
-        initUI();
-        checkFirstRun();
+	initApplication();
+	initUI();
+	checkFirstRun();
+	checkLocalisation();
     }
 
     private void initApplication() {
 
-        try {
-            java.net.Authenticator.setDefault(new AuthenticatorStub(windowManager));
-        } catch (SecurityException sEx) {
-            BugCatcherManager.getBugCatcher().logError("Failed to set custom Authenticator. Default authentication dialogs may appear.", sEx);
-        }
+	try {
+	    java.net.Authenticator.setDefault(new AuthenticatorStub(windowManager));
+	} catch (SecurityException sEx) {
+	    BugCatcherManager.getBugCatcher().logError("Failed to set custom Authenticator. Default authentication dialogs may appear.", sEx);
+	}
 
-        treeHelper.init();
-        mimeHashQueue.init();
+	treeHelper.init();
+	mimeHashQueue.init();
     }
 
     private void initUI() {
-        this.addWindowListener(new WindowAdapter() {
-            @Override
-            public void windowClosing(WindowEvent e) {
-                arbilMenuBar.performCleanExit();
-                //super.windowClosing(e);
-            }
-        });
+	this.addWindowListener(new WindowAdapter() {
+	    @Override
+	    public void windowClosing(WindowEvent e) {
+		arbilMenuBar.performCleanExit();
+		//super.windowClosing(e);
+	    }
+	});
 
-        initComponents();
-        windowManager.addTaskListener(statusBar);
-        PreviewSplitPanel previewSplitPanel = new PreviewSplitPanel(windowManager, tableController);
-        mainSplitPane.setRightComponent(previewSplitPanel);
+	initComponents();
+	windowManager.addTaskListener(statusBar);
+	PreviewSplitPanel previewSplitPanel = new PreviewSplitPanel(windowManager, tableController);
+	mainSplitPane.setRightComponent(previewSplitPanel);
 
-        final ArbilDragDrop dragDrop = new ArbilDragDrop(sessionStorage, treeHelper, windowManager, windowManager, tableController);
-        windowManager.setDragDrop(dragDrop);
+	final ArbilDragDrop dragDrop = new ArbilDragDrop(sessionStorage, treeHelper, windowManager, windowManager, tableController);
+	windowManager.setDragDrop(dragDrop);
 
-        final ArbilTreePanels arbilTreePanels = new ArbilTreePanels(treeHelper, treeController, previewSplitPanel, dragDrop);
-        mainSplitPane.setLeftComponent(arbilTreePanels);
-        arbilMenuBar = new ArbilMenuBar(previewSplitPanel, null, logConfigurer);
-        setJMenuBar(arbilMenuBar);
+	final ArbilTreePanels arbilTreePanels = new ArbilTreePanels(treeHelper, treeController, previewSplitPanel, dragDrop);
+	mainSplitPane.setLeftComponent(arbilTreePanels);
+	arbilMenuBar = new ArbilMenuBar(previewSplitPanel, null, logConfigurer);
+	setJMenuBar(arbilMenuBar);
 
-        mainSplitPane.setDividerLocation(0.25);
+	mainSplitPane.setDividerLocation(0.25);
 
-        windowManager.loadGuiState(this, statusBar);
-        setTitle(versionManager.getApplicationVersion().applicationTitle + " " + versionManager.getApplicationVersion().compileDate);
-        setIconImage(ArbilIcons.getSingleInstance().linorgIcon.getImage());
-        // load the templates and populate the templates menu
-        setVisible(true);
+	windowManager.loadGuiState(this, statusBar);
+	setTitle(versionManager.getApplicationVersion().applicationTitle + " " + versionManager.getApplicationVersion().compileDate);
+	setIconImage(ArbilIcons.getSingleInstance().linorgIcon.getImage());
+	// load the templates and populate the templates menu
+	setVisible(true);
 
-        if (arbilMenuBar.checkNewVersionAtStartCheckBoxMenuItem.isSelected()) {
-            versionManager.checkForUpdate();
-        }
+	if (arbilMenuBar.checkNewVersionAtStartCheckBoxMenuItem.isSelected()) {
+	    versionManager.checkForUpdate();
+	}
 
-        initMacHandlers();
+	initMacHandlers();
     }
 
     private void initMacHandlers() {
-        final MacAdapter macAdapter = new MacAdapter() {
-            @Override
-            protected boolean performApplicationExit() {
-                return arbilMenuBar.performCleanExit();
-            }
+	final MacAdapter macAdapter = new MacAdapter() {
+	    @Override
+	    protected boolean performApplicationExit() {
+		return arbilMenuBar.performCleanExit();
+	    }
 
-            @Override
-            protected void performApplicationAbout() {
-                windowManager.openAboutPage();
-            }
-        };
+	    @Override
+	    protected void performApplicationAbout() {
+		windowManager.openAboutPage();
+	    }
+	};
 
-        try {
-            if (macAdapter.initMacApplicationHandlers()) {
-                // Successfully set handlers, now remove redundant options from menu bar
-                arbilMenuBar.setMacOsMenu(true);
-            }
-        } catch (MacAdapterException exception) {
-            System.err.println("Could not configure MacOS application handlers");
-            if (exception != null) {
-                System.err.println(exception);
-            }
+	try {
+	    if (macAdapter.initMacApplicationHandlers()) {
+		// Successfully set handlers, now remove redundant options from menu bar
+		arbilMenuBar.setMacOsMenu(true);
+	    }
+	} catch (MacAdapterException exception) {
+	    System.err.println("Could not configure MacOS application handlers");
+	    if (exception != null) {
+		System.err.println(exception);
+	    }
 
-        }
+	}
     }
 
     private void checkFirstRun() {
-        windowManager.showSetupWizardIfFirstRun();
-        windowManager.openIntroductionPage();
+	windowManager.showSetupWizardIfFirstRun();
+	windowManager.openIntroductionPage();
+    }
+
+    private void checkLocalisation() {
+	final String availableLanguages = ResourceBundle.getBundle("nl/mpi/arbil/localisation/AvailableLanguages").getString("LANGUAGE CODES");
+	final LocalisationSelector localisationSelector = new LocalisationSelector(sessionStorage, availableLanguages.split(","));
+	if (!localisationSelector.hasSavedLocal()) {
+	    final String please_select_your_preferred_language = menus.getString("PLEASE SELECT YOUR PREFERRED LANGUAGE");
+	    final String language_Selection = menus.getString("LANGUAGE SELECTION");
+	    final String system_Default = menus.getString("SYSTEM DEFAULT");
+	    localisationSelector.askUser(null, ArbilIcons.getSingleInstance().linorgIcon, please_select_your_preferred_language, language_Selection, system_Default);
+	}
+	localisationSelector.setLanguageFromSaved();
     }
 
     private void initComponents() {
 
-        mainSplitPane = new javax.swing.JSplitPane();
-        statusBar = new ArbilTaskStatusBar();
+	mainSplitPane = new javax.swing.JSplitPane();
+	statusBar = new ArbilTaskStatusBar();
 
-        setDefaultCloseOperation(javax.swing.WindowConstants.DO_NOTHING_ON_CLOSE);
-        setTitle("Arbil");
+	setDefaultCloseOperation(javax.swing.WindowConstants.DO_NOTHING_ON_CLOSE);
+	setTitle("Arbil");
 
-        mainSplitPane.setDividerLocation(100);
-        mainSplitPane.setDividerSize(5);
-        mainSplitPane.setName("mainSplitPane");
-        getContentPane().setLayout(new BorderLayout());
-        getContentPane().add(mainSplitPane, java.awt.BorderLayout.CENTER);
-        getContentPane().add(statusBar, BorderLayout.SOUTH);
+	mainSplitPane.setDividerLocation(100);
+	mainSplitPane.setDividerSize(5);
+	mainSplitPane.setName("mainSplitPane");
+	getContentPane().setLayout(new BorderLayout());
+	getContentPane().add(mainSplitPane, java.awt.BorderLayout.CENTER);
+	getContentPane().add(statusBar, BorderLayout.SOUTH);
 
-        pack();
+	pack();
     }
 }
