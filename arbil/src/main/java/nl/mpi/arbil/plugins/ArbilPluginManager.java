9ee41e501802344c932ac1ba624f7cc3992d353e
Peter Withers <Peter.Withers@mpi.nl>
2013-10-30 14:40:06 +0000

    Updated and added license headers where necessary.

--- arbil/src/main/java/nl/mpi/arbil/plugins/ArbilPluginManager.java
+++ arbil/src/main/java/nl/mpi/arbil/plugins/ArbilPluginManager.java
@@ -1,10 +1,10 @@
 /**
- * Copyright (C) 2013 Max Planck Institute for Psycholinguistics
+ * Copyright (C) 2013 The Language Archive, Max Planck Institute for Psycholinguistics
  *
- * This program is free software; you can redistribute it and/or modify it under
- * the terms of the GNU General Public License as published by the Free Software
- * Foundation; either version 2 of the License, or (at your option) any later
- * version.
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
