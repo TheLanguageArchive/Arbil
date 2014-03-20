e9751b3b62f33c8b221de8460b13301cd856bb5e
Peter Withers <Peter.Withers@mpi.nl>
2014-03-20 14:45:49 +0000

    Merged the recent changes in documentation. Added a bash script to make it clear how to update the documentation from its home.

---
+++ arbil-help/src/main/docbook/svn-merge-documentation.sh
@@ -0,0 +1,5 @@
+# The intention of this script is to make it clear what and how to merge the latest documatation. This must be run from the same directory as this script.
+svn merge ^/documentation/manuals/arbil/images/ images/
+svn merge ^/documentation/manuals/arbil/arbil-cmdi.xml arbil-cmdi.xml 
+svn merge ^/documentation/manuals/arbil/arbil-imdi.xml arbil-imdi.xml 
+
