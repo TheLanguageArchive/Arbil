0f6ddc585f6ac31d746badbe4f2694052d6e78ea
Peter Withers <Peter.Withers@mpi.nl>
2014-06-05 13:00:32 +0000

    Added more data provided by the YAMS CS REST service so that YAMS GWT can utilise the output.

--- arbil/src/main/java/nl/mpi/arbil/data/ArbilDataNodeType.java
+++ arbil/src/main/java/nl/mpi/arbil/data/ArbilDataNodeType.java
@@ -1,19 +1,20 @@
 /**
- * Copyright (C) 2013 The Language Archive, Max Planck Institute for Psycholinguistics
+ * Copyright (C) 2013 The Language Archive, Max Planck Institute for
+ * Psycholinguistics
  *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License
- * as published by the Free Software Foundation; either version 2
- * of the License, or (at your option) any later version.
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 2 of the License, or (at your option) any later
+ * version.
  *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
  *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ * You should have received a copy of the GNU General Public License along with
+ * this program; if not, write to the Free Software Foundation, Inc., 59 Temple
+ * Place - Suite 330, Boston, MA 02111-1307, USA.
  */
 package nl.mpi.arbil.data;
 
@@ -37,7 +38,6 @@ public class ArbilDataNodeType extends DataNodeType {
 //        childnode,
 //        directory
 //    }
-
     static public DataNodeType getTypeForNode(ArbilDataNode arbilDataNode) {
         final DataNodeType dataNodeType = new DataNodeType();
 //        if (arbilDataNode.isCmdiMetaDataNode()) {
@@ -66,7 +66,7 @@ public class ArbilDataNodeType extends DataNodeType {
 ////        rootnode
 //        }
 //        final CmdiTemplate cmdiTemplate = (CmdiTemplate) template;
-        dataNodeType.setName(arbilDataNode.getNodeTemplate().getTemplateName());
+        dataNodeType.setID(arbilDataNode.getNodeTemplate().getTemplateName());
         return dataNodeType;
     }
 }
