0fbe1559775468e9e2c38c36eeb9e0d7aeafac1d
Peter Withers <Peter.Withers@mpi.nl>
2014-09-11 08:26:49 +0000

    Removed UI related code from the Arbil metadata reader/writer.

--- metadata-reader-writer/src/main/java/nl/mpi/arbil/data/ArbilRootNode.java
+++ metadata-reader-writer/src/main/java/nl/mpi/arbil/data/ArbilRootNode.java
@@ -1,19 +1,20 @@
 /**
- * Copyright (C) 2013 The Language Archive, Max Planck Institute for Psycholinguistics
+ * Copyright (C) 2013 The Language Archive, Max Planck Institute for
+ * Psycholinguistics
  *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License
- * as published by the Free Software Foundation; either version 2
- * of the License, or (at your option) any later version.
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 2 of the License, or (at your option) any later
+ * version.
  *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
  *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ * You should have received a copy of the GNU General Public License along with
+ * this program; if not, write to the Free Software Foundation, Inc., 59 Temple
+ * Place - Suite 330, Boston, MA 02111-1307, USA.
  */
 package nl.mpi.arbil.data;
 
@@ -21,7 +22,10 @@ import java.net.URI;
 import java.util.Arrays;
 import java.util.List;
 import javax.swing.ImageIcon;
+import nl.mpi.flap.model.DataNodeLink;
+import nl.mpi.flap.model.DataNodePermissions;
 import nl.mpi.flap.model.FieldGroup;
+import nl.mpi.flap.model.ModelException;
 import nl.mpi.flap.model.PluginDataNode;
 
 /**
@@ -152,6 +156,26 @@ public abstract class ArbilRootNode extends ArbilNode {
 
     public boolean isDataLoaded() {
         return true;
+    }
+
+    public String getURI() throws ModelException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public String getArchiveHandle() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public DataNodePermissions getPermissions() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public Integer getLinkCount() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public List<DataNodeLink> getChildIds() throws ModelException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
     @Override
