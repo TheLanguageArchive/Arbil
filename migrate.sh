723a6e1feefdf629481752419fb93b2bf20d32e0
Peter Withers <peter.withers@mpi.nl>
2014-09-25 17:55:23 +0200

    Imported the Linorg history and added the import script.

---
+++ migrate.sh
@@ -0,0 +1,8 @@
+# this file was used to import the SVN history into git.
+svn2git https://svn.mpi.nl/LAT --username petwit --authors ../users.txt --notags --nobranches --trunk trunk/src/java/mpi/linorg;
+#git remote add origin https://github.com/TheLanguageArchive/LINORG.git;
+#git push --all -u;
+# please note that new clones taken from GitHub will not have the svn information required to 
+# pull new changes from svn to git. So it is recommended to keep the original that you pushed
+# to GitHub if you wish to updates from SVN. If this is not possible you can always pull whole
+# lot from SVN again with this script into a fresh Git repostitory.
