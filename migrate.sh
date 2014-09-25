8aa8851b6af28b0b229caaf21633545a3a0335a6
PeterWithers <peter-gthb@bambooradical.com>
2014-09-25 18:09:55 +0200

    Update migrate.sh

--- migrate.sh
+++ migrate.sh
@@ -1,7 +1,9 @@
 # this file was used to import the SVN history into git.
 svn2git https://svn.mpi.nl/LAT --username petwit --authors ../users.txt --notags --nobranches --trunk trunk/src/java/mpi/linorg;
-#git remote add origin https://github.com/TheLanguageArchive/LINORG.git;
+git remote add origin https://github.com/TheLanguageArchive/Arbil.git;
 #git push --all -u;
+# push the local master into the existing Arbil repository as a new branch "linorg"
+git push origin master:linorg
 # please note that new clones taken from GitHub will not have the svn information required to 
 # pull new changes from svn to git. So it is recommended to keep the original that you pushed
 # to GitHub if you wish to updates from SVN. If this is not possible you can always pull whole
